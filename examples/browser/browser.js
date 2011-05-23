62e874a3b935bcdea253c3f148bc900f2a27c8de
Adam Beynon <adam@adambeynon.com>
2011-05-23 17:09:19 +0100

    Update example to use new block generation

--- examples/browser/browser.js
+++ examples/browser/browser.js
@@ -10,29 +10,29 @@ title = document.title;
 self.$m.$puts(self, ("The document title is '" + (__b = title).$m.to_s(__b) + "'"));
 
 
-(__a = [1, 2, 3, 4, 5], __b = [__a], ($block.p = (__c = function(self, a) {var $args = arguments, $meth = $args.callee, $len = $args.length; var __a;if (a === undefined) { a = nil; }
+(($B.p = function(self, a) {var $A = arguments, $L = $A.length; var __a;if (a === undefined) { a = nil; }
   return self.$m.$puts(self, a);
-}, __c.$arity = 0, __c.$meth = null, __c)).$self = self, ($block.f = __a.$m.each).apply(__a, __b));
+}).$proc = [self], $B.f = (__a = [1, 2, 3, 4, 5]).$m.each)(__a);
 
 
 $class(self, nil, 'ClassA', function(self) {
-  return $def(self, 'method_missing', function(self, method_id, args) { var __a, __b, __c;var $args = arguments, $meth = $args.callee, $len = $args.length;if ($len < 2) { $ac(1, $len - 1); }args = [].slice.call($args, 2);
+  return $def(self, 'method_missing', function(self, method_id, args) { var __a, __b, __c;var $A = arguments, $M = $A.callee, $L = $A.length;if ($L < 2) { $ac(1, $L - 1); }args = [].slice.call($A, 2);
     return self.$m.$puts(self, ("Tried to call '" + (__b = method_id).$m.to_s(__b) + "' with: " + (__b = args.$m.inspect(args)).$m.to_s(__b)));
   }, 0);
 }, 0);
 
-self['@cls'] = (__b = rb_vm_cg(self, 'ClassA')).$m['new'](__b);
-(__b = self['@cls']).$m.try_this(__b);
-(__b = self['@cls']).$m['do_assign='](__b, "this won't work");
-(__b = self['@cls']).$m['[]='](__b, 'neither', $symbol('will_this'));
-(__b = self['@cls']).$m['is_this_true?'](__b);
-(__b = self['@cls']).$m['no_its_not!'](__b);
-(__b = self['@cls']).$m['+'](__b, 'something to add');
+self['@cls'] = (__a = rb_vm_cg(self, 'ClassA')).$m['new'](__a);
+(__a = self['@cls']).$m.try_this(__a);
+(__a = self['@cls']).$m['do_assign='](__a, "this won't work");
+(__a = self['@cls']).$m['[]='](__a, 'neither', $symbol('will_this'));
+(__a = self['@cls']).$m['is_this_true?'](__a);
+(__a = self['@cls']).$m['no_its_not!'](__a);
+(__a = self['@cls']).$m['+'](__a, 'something to add');
 
 
-self.$m.$puts(self, (__a = 1).$m['+'](__a, 2));
-self.$m.$puts(self, (__a = [1, 2, 3, 4]).$m['[]'](__a, 0));
-self.$m.$puts(self, (__a = [1, 2, 3, 4]).$m['[]'](__a, -2));
+self.$m.$puts(self, (__b = 1).$m['+'](__b, 2));
+self.$m.$puts(self, (__b = [1, 2, 3, 4]).$m['[]'](__b, 0));
+self.$m.$puts(self, (__b = [1, 2, 3, 4]).$m['[]'](__b, -2));
 
 
 $class(self, rb_vm_cg(self, 'Exception'), 'CustomBrowserException', function(self) {  return nil;}, 0);
@@ -44,7 +44,7 @@ try {
     self.$m.$puts(self, e.$m.inspect(e));}
 };
 
-var nil, $ac, $super, $break, $class, $def, $symbol, $range, $hash, $block, Qtrue, Qfalse;var __a, __b, title, __c, e;
-function $$init() {nil = $runtime.Qnil, $ac = $runtime.ac, $super = $runtime.S, $break = $runtime.B, $class = $runtime.dc, $def = $runtime.dm, $symbol = $runtime.Y, $range = $runtime.G, $hash = $runtime.H, $block = $runtime.P, Qtrue = $runtime.Qtrue, Qfalse = $runtime.Qfalse;$runtime.mm(['puts', 'to_s', 'each', 'inspect', 'new', 'try_this', 'do_assign=', '[]=', 'is_this_true?', 'no_its_not!', '+', '[]', 'raise']);if (self['@cls'] == undefined) { self['@cls'] = nil; }}
+var nil, $ac, $super, $break, $class, $def, $symbol, $range, $hash, $B, Qtrue, Qfalse;var __a, __b, title, e;
+function $$init() {nil = $runtime.Qnil, $ac = $runtime.ac, $super = $runtime.S, $break = $runtime.B, $class = $runtime.dc, $def = $runtime.dm, $symbol = $runtime.Y, $range = $runtime.G, $hash = $runtime.H, $B = $runtime.P, Qtrue = $runtime.Qtrue, Qfalse = $runtime.Qfalse;$runtime.mm(['puts', 'to_s', 'each', 'inspect', 'new', 'try_this', 'do_assign=', '[]=', 'is_this_true?', 'no_its_not!', '+', '[]', 'raise']);if (self['@cls'] == undefined) { self['@cls'] = nil; }}
  });
 opal.require('browser');
