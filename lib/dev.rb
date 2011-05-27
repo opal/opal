cb47b78e7ffc15360d633cbac30b468491bfa21e
Adam Beynon <adam@adambeynon.com>
2011-05-27 12:19:57 +0100

    In browser loading of script tags

--- lib/dev.rb
+++ lib/dev.rb
@@ -1,33 +1,68 @@
 require 'opal/ruby/parser'
 
-`var Dp = opal.dev = {};
+module Opal
 
-Dp.parse = opal.compile = function(source, options) {
+  def self.compile(source)
+    "'some-ruby-content'"
+  end
+
+  def self.run_ruby_content(source, filename = "(opal)")
+    js = compile source
+    puts js
+    `var exec = new Function('$runtime', 'self', '__FILE__', js);
+    exec($runtime, $runtime.top, filename);`
+    nil
+  end
+
+  # Load the ruby code at the remote url, parse and run it. This is typically
+  # used when loading a script tag of type text/ruby. The filename given in the
+  # tag is used as the actual filename
+  #
+  # @param [String] filename
+  def self.run_remote_content(filename)
+    `var xhr;
+
+    if (window.ActiveXObject)
+      xhr = new window.ActiveXObject('Microsoft.XMLHTTP');
+    else
+      xhr = new XMLHttpRequest();
+
+    xhr.open('GET', filename, true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4) {
+        if (xhr.status == 0 || xhr.status == 200) {
+          #{ run_ruby_content `xhr.responseText`, filename };
+        } else {
+          #{ raise "LoadError: Cannot load: #{filename}" };
+        }
+      }
+    };
+    xhr.send(null);`
+    nil
+  end
+
+  def self.run_script_tags
+
+  end
+end
+
+`opal.compile = function(source, options) {
   console.log("need to compile some code");
-  return "";
-};
+  return #{ Opal.compile `source` };
+};`
 
-function runRubyContent(source, options) {
-  if (!options) options = {};
-  if (!options.filename) options.filename = '(script-tag)';
-  var js = Dp.parse(source, options);
-  console.log(js);
-  var exec = new Function('$runtime', 'self', '__FILE__', js);
+`if (typeof window !== 'undefined') {
+  var runner = function() { #{ Opal.run_script_tags }; };
 
-  if (typeof opal.runtime === 'undefined') {
-    throw new Error("Cannot run ruby content without opal runtime");
+  if (window.addEventListener) {
+    window.addEventListener('DOMContentLoaded', runner, false);
+  } else {
+    window.attachEvent('onload', runner);
   }
-  exec(opal.runtime, opal.runtime.top, options.filename);
-};
+}`
 
-/**
-  Load the ruby code at the remote url, parse and run it. This is typically
-  used when loading a script tag of type text/ruby. The filename given in the
-  tag is used as the actual filename
 
-  @param {String} filename
-*/
-function runRemoteContent(filename) {
+`function runRemoteContent(filename) {
   var xhr;
 
   if (window.ActiveXObject)
