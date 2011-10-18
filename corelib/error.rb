02fd7e285289181888c5bef11dcc2c1fc0eb4d05
meh <meh@paranoici.org>
2011-10-18 14:41:36 +0200

    corelib/error: cleanup and giggles

--- corelib/error.rb
+++ corelib/error.rb
@@ -1,21 +1,31 @@
 class Exception
-  def initialize (message = "")
+  def initialize(message = '')
     @message = message
+  end
+
+  def ==(*)
+    raise NotImplementedError, 'Exception#== not yet implemented'
+  end
+
+  def backtrace
+    []
+  end
+
+  def exception(*)
+    raise NotImplementedError, 'Exception#exception not yet implemented'
+  end
+
+  def inspect
+    `"#<" + self.$k.__classid__ + ": '" + #{message} + "'>"`
   end
 
   def message
     @message
   end
 
+  def set_backtrace(*)
+    raise NotImplementedError, 'Exception#set_backtrace not yet implemented'
+  end
+
   alias_method :to_s, :message
-
-  def inspect
-    `"#<" + self.$k.__classid__ + ": '" + #{message} + "'>"`
-    "#<#{self.class}: #{message}>"
-  end
-
-  def backtrace
-    []
-  end
 end
-
