6723f7659f14da23008359ab1c0ae3d8586ec20c
meh <meh@paranoici.org>
2011-10-18 14:59:43 +0200

    corelib/object: cleanup and giggles

---
+++ corelib/object.rb
@@ -0,0 +1,15 @@
+class Object
+  def methods
+    `self.$k.$methods`
+  end
+
+  alias_method :private_methods, :methods
+
+  alias_method :protected_methods, :methods
+
+  alias_method :public_methods, :methods
+
+  def singleton_methods
+    raise NotImplementedError, 'Object#singleton_methods not yet implemented'
+  end
+end
