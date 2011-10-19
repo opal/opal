97af4c905636a10f1016720195b9803393b2f4de
meh <meh@paranoici.org>
2011-10-19 17:57:56 +0200

    corelib/native_object: cleanup and giggles

--- corelib/native_object.rb
+++ corelib/native_object.rb
@@ -1,26 +1,14 @@
-##
-# +NativeObject+ is a class used to represent native javascript objects
-# to opal. Whenever a message is sent to a native js object, then it will
-# act like an instance of this class. If a method is not found, then the
-# method name will be used to look up a raw property on the receiver, or
-# even set a property value.
-#
-# It is not possible to create new instances of this class. +.new+ simply
-# returns an empty javascript object, which you can interact with using
-# the methods listed in this class.
-
 class NativeObject < BasicObject
-
   def self.new
     `{}`
   end
 
-  def [] key
+  def [](key)
     `self.hasOwnProperty(key) ? self[key] : null`
   end
 
-  def []= key, val
-    `self[key] = val`
+  def []=(key, value)
+    `self[key] = value`
   end
 
   ##
@@ -30,19 +18,20 @@ class NativeObject < BasicObject
   #
   #     $document.include? :some_random_property  # => false
   #     $document.include? :body                  # => true
-
-  def include? member
+  def include?(member)
     `self.hasOwnProperty(member)`
   end
 
   def to_hash
     hash = {}
 
-    `for (var name in self) {
-      if (self.hasOwnProperty(name)) {
-        #{hash[`name`] = `self[name]`};
+    `
+      for (var name in self) {
+        if (self.hasOwnProperty(name)) {
+          #{hash[`name`] = `self[name]`};
+        }
       }
-    }`
+    `
 
     hash
   end
@@ -52,7 +41,7 @@ class NativeObject < BasicObject
   end
 
   def inspect
-    `return "#<NativeObject: " + self.toString() + "#>";`
+    "#<NativeObject: #{self}>"
   end
 end
 
