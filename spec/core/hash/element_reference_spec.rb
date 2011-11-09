e62096a4a208b4798436eb22a32013407fbd5313
Adam Beynon <adam@adambeynon.com>
2011-11-09 18:32:49 +0000

    Hash specs

---
+++ spec/core/hash/element_reference_spec.rb
@@ -0,0 +1,16 @@
+require File.expand_path('../../../spec_helper', __FILE__)
+
+describe "Hash#[]" do
+  it "returns the value for the key" do
+    obj = Object.new
+    h = {1 => 2, 3 => 4, "foo" => "bar", obj => obj, [] => "baz"}
+    h[1].should == 2
+    h[3].should == 4
+    h["foo"].should == "bar"
+    h[obj].should == obj
+  end
+
+  it "returns nil as default default value" do
+    {0 => 0}[5].should == nil
+  end
+end
