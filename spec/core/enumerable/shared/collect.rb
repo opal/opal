337e08551ae3d0e1b2ae8d769595c7d5c59120e2
Adam Beynon <adam@adambeynon.com>
2011-12-01 16:01:14 +0000

    Enumerable#collect specs

---
+++ spec/core/enumerable/shared/collect.rb
@@ -0,0 +1,12 @@
+describe "Enumerable#collect" do
+  before :each do
+    ScratchPad.record []
+  end
+
+  it "returns a new array with the results of passing each element to block" do
+    entries = [0, 1, 3, 4, 5, 6]
+    numerous = EnumerableSpecs::Numerous.new(*entries)
+    numerous.collect { |i| i % 2 }.should == [0, 1, 1, 0, 1, 0]
+    numerous.collect { |i| i }.should == entries
+  end
+end
