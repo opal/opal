7c8d1aa397f95ee8e08ea1733e4f42322c3848c7
Adam Beynon <adam@adambeynon.com>
2011-12-04 10:15:23 +0000

    Complete numeric reimplementation.

--- corelib/numeric.js
+++ corelib/numeric.js
@@ -53,6 +53,139 @@ function num_cmp(other) {
   return self < other ? -1 : (self > other ? 1 : 0);
 }
 
+function num_mod(other) {
+  return this % other;
+}
+
+function num_and(other) {
+  return this & other;
+}
+
+function num_or(other) {
+  return this | other;
+}
+
+function num_tild(other) {
+  return ~this;
+}
+
+function num_xor(other) {
+  return this ^ other;
+}
+
+function num_lshft(count) {
+  return this << count;
+}
+
+function num_rshft(count) {
+  return this >> count;
+}
+
+function num_abs() {
+  return Math.abs(self);
+}
+
+function num_hash() {
+  return this.$f + '_' + this;
+}
+
+function num_even_p() {
+  return this % 2 === 0;
+}
+
+function num_odd_p() {
+  return this % 2 !== 0;
+}
+
+function num_succ() {
+  return this + 1;
+}
+
+function num_pred() {
+  return this - 1;
+}
+
+function num_upto(finish) {
+  var iterator = num_upto.proc;
+  if (!iterator) return this.m$enum_for("upto", finish);
+
+  var context = iterator.$S;
+  num_upto.proc = 0;
+
+  for (var i = this; i <= finish; i++) {
+    if (iterator.call(context, i) === breaker) return breaker.$v;
+  }
+  return this;
+}
+
+function num_downto(finish) {
+  var iterator = num_downto.proc;
+  if (!iterator) return this.m$enum_for("downto", finish);
+
+  var context = iterator.$S;
+  num_downto.proc = 0;
+
+  for (var i = this; i >= finish; i--) {
+    if (iterator.call(context, i) === breaker) return breaker.$v;
+  }
+  return this;
+}
+
+function num_times() {
+  var iterator = num_times.proc;
+  if (!iterator) return this.m$enum_for("times");
+
+  var context = iterator.$S;
+  num_times.proc = 0;
+
+  for (var i = 0; i <= this; i++) {
+    if (iterator.call(context, i) === breaker) return breaker.$v;
+  }
+  return this;
+}
+
+function num_zero_p() {
+  return this == 0;
+}
+
+function num_nonzero_p() {
+  return this == 0 ? nil : self;
+}
+
+function num_ceil() {
+  return Math.ceil(this);
+}
+
+function num_floor() {
+  return Math.floor(this);
+}
+
+function num_int_p() {
+  return this % 1 === 0;
+}
+
+function num_to_s() {
+  return this.toString();
+}
+
+function num_to_i() {
+  return parseInt(this);
+}
+
+function num_to_f() {
+  return parseFloat(this);
+}
+
+function int_eqq(other) {
+  if (typeof other !== 'number') return false;
+  return other % 1 === 0;
+}
+
+function flo_eqq(other) {
+  if (typeof other !== 'number') return false;
+  return other % 1 !== 0;
+}
+
 function init_numeric() {
   RubyNumeric = rb_bridge_class(Number, T_OBJECT | T_NUMBER, 'Numeric');
 
@@ -69,6 +202,40 @@ function init_numeric() {
     'm$le$': num_le,
     'm$gt$': num_gt,
     'm$ge$': num_ge,
-    'm$cmp$': num_cmp
+    'm$cmp$': num_cmp,
+    'm$mod$': num_mod,
+    'm$modulo': num_mod,
+    'm$and$': num_and,
+    'm$or$': num_or,
+    'm$tild$': num_tild,
+    'm$xor$': num_xor,
+    'm$lshft$': num_lshft,
+    'm$rshft$': num_rshft,
+    'm$abs': num_abs,
+    'm$magnitude': num_abs,
+    'm$hash': num_hash,
+    'm$even$p': num_even_p,
+    'm$odd$p': num_odd_p,
+    'm$next': num_succ,
+    'm$succ': num_succ,
+    'm$pred': num_pred,
+    'm$upto': num_upto,
+    'm$downto': num_downto,
+    'm$times': num_times,
+    'm$zero$p': num_zero_p,
+    'm$nonzero$p': num_nonzero_p,
+    'm$ceil': num_ceil,
+    'm$floor': num_floor,
+    'm$integer$p': num_int_p,
+    'm$to_s': num_to_s,
+    'm$inspect': num_to_s,
+    'm$to_i': num_to_i,
+    'm$to_f': num_to_f
   });
+
+  RubyInteger = define_class(rb_cObject, 'Integer', RubyNumeric);
+  define_singleton_method(RubyInteger, 'm$eqq$', int_eqq);
+
+  RubyFloat = define_class(rb_cObject, 'Float', RubyNumeric);
+  define_singleton_method(RubyFloat, 'm$eqq$', flo_eqq);
 }
