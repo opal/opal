e8c511901ac13c257df68fbf5507f6a4603b9066
Adam Beynon <adam@adambeynon.com>
2011-12-08 12:27:13 +0000

    Clean up class.rb and reflect passing block to methods

--- corelib/class.rb
+++ corelib/class.rb
@@ -1,17 +1,14 @@
 class Class
   def self.new(sup = Object, &block)
-    cls = `boot_class(sup)`
-    `cls.__classid__ = "AnonClass";`
-    `rb_make_metaclass(cls, sup.$k);`
-    `cls.$parent = sup;`
+    `
+      var klass = boot_class(sup);
+      klass.__classid__ = "AnonClass";
+      rb_make_metaclass(klass, sup.$k);
+      klass.$parent = sup;
+      #{sup.inherited `klass`};
 
-    sup.inherited cls
-
-    if block_given?
-      `return block.call(cls);`
-    else
-      cls
-    end
+      return block !== nil ? block.call(klass, null) : klass;
+    `
   end
 
   def allocate
@@ -31,26 +28,11 @@ class Class
   def superclass
     `
       var sup = self.$s;
-
       if (!sup) {
-        if (self === rb_cObject) {
-          return nil;
-        }
-
-        rb_raise(rb_eRuntimeError, "uninitialized class");
+        if (self === rb_cObject) return nil;
+        rb_raise(RubyRuntimeError, 'uninitialized class');
       }
-
       return sup;
-    `
-  end
-
-  def from_native(obj)
-    `
-      obj.$id = rb_hash_yield++;
-      obj.$k  = self;
-      obj.$m  = self.$m_tbl;
-      obj.$f  = T_OBJECT
-      return obj;
     `
   end
 end
