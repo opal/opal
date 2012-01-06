f8f5dace8e741d9ecb641c94c47e1dfd02999a1e
Adam Beynon <adam@adambeynon.com>
2012-01-06 19:01:20 +0000

    Added 'init' command

--- lib/opal/command.rb
+++ lib/opal/command.rb
@@ -8,6 +8,11 @@ module Opal
 
     def initialize(args)
       options = {}
+      if ARGV.first == 'init'
+        options[:init] = true
+        ARGV.shift
+      end
+
       OptionParser.new do |opts|
         opts.on('-c', '--compile', 'Compile ruby') do |c|
           options[:compile] = c
@@ -21,23 +26,46 @@ module Opal
           options[:join] = j || ''
         end
 
+        opts.on('-d', '--debug', 'Debug mode') do |d|
+          options[:debug] = true
+        end
+
         opts.on_tail("-v", "--version", "Show version") do
           puts Opal::VERSION
           exit
         end
       end.parse!
 
-      if options[:compile]
-        Compiler.new(ARGV, options).compile
+      if options[:init]
+        init options
+      elsif options[:compile]
+        build options
       else
-        if ARGV.empty?
-          Context.new.start_repl
-        elsif File.exists? ARGV.first
-          Context.runner ARGV.first
-        else
-          puts "#{ARGV.first} does not exist"
-        end
+        run options
       end
+    end
+
+    def run(options)
+      if ARGV.empty?
+        Context.new.start_repl
+      elsif File.exists? ARGV.first
+        Context.runner ARGV.first
+      else
+        puts "#{ARGV.first} does not exist"
+      end
+    end
+
+    def init(options)
+      out   = options[:output]
+      src   = options[:debug] ? Opal.runtime_debug_code : Opal.runtime_code
+      out ||= (options[:debug] ? 'opal.debug.js' : 'opal.js')
+
+      File.open(out, 'w+') { |o| o.write src }
+      puts "Wrote Opal to #{out}#{options[:debug] && ' (debug)'}"
+    end
+
+    def build(options)
+      Builder.new(ARGV, options).build
     end
   end # Command
 end
