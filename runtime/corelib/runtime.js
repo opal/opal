b3b5f839ba0876895011ab747800c40020270e44
Adam Beynon <adam@adambeynon.com>
2012-01-10 18:17:28 +0000

    Move String#inspect implementation back into string.rb

--- runtime/corelib/runtime.js
+++ runtime/corelib/runtime.js
@@ -176,30 +176,6 @@ var define_method = opal.defn = function(klass, id, body) {
   return nil;
 }
 
-function string_inspect(self) {
-  /* borrowed from json2.js, see file for license */
-  var cx        = /[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,
-      escapable = /[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,
-      meta      = {
-        '\b': '\\b',
-        '\t': '\\t',
-        '\n': '\\n',
-        '\f': '\\f',
-        '\r': '\\r',
-        '"' : '\\"',
-        '\\': '\\\\'
-      };
-
-  escapable.lastIndex = 0;
-
-  return escapable.test(self) ? '"' + self.replace(escapable, function(a) {
-    var c = meta[a];
-
-    return typeof c === 'string' ? c :
-      '\\u' + ('0000' + a.charCodeAt(0).toString(16)).slice(-4);
-  }) + '"' : '"' + self + '"';
-};
-
 // Fake yielder used when no block given
 opal.no_proc = function() {
   raise(RubyLocalJumpError, "no block given");
--- runtime/corelib/string.rb
+++ runtime/corelib/string.rb
@@ -106,7 +106,27 @@ class String
   end
 
   def inspect
-    `string_inspect(this)`
+    %x{
+      var escapable = /[\\\\\\"\\x00-\\x1f\\x7f-\\x9f\\u00ad\\u0600-\\u0604\\u070f\\u17b4\\u17b5\\u200c-\\u200f\\u2028-\\u202f\\u2060-\\u206f\\ufeff\\ufff0-\\uffff]/g,
+          meta      = {
+            '\\b': '\\\\b',
+            '\\t': '\\\\t',
+            '\\n': '\\\\n',
+            '\\f': '\\\\f',
+            '\\r': '\\\\r',
+            '"' : '\\\\"',
+            '\\\\': '\\\\\\\\'
+          };
+
+      escapable.lastIndex = 0;
+
+      return escapable.test(this) ? '"' + this.replace(escapable, function(a) {
+        var c = meta[a];
+
+        return typeof c === 'string' ? c :
+          '\\\\u' + ('0000' + a.charCodeAt(0).toString(16)).slice(-4);
+      }) + '"' : '"' + this + '"';
+  }
   end
 
   def intern
