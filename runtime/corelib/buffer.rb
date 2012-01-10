87e5114f8b83b16e4918611d8df1d3eb46baf54e
meh <meh@paranoici.org>
2012-01-10 06:27:26 +0100

    corelib/buffer: implement faster view access

--- runtime/corelib/buffer.rb
+++ runtime/corelib/buffer.rb
@@ -7,7 +7,6 @@ class Buffer
     end}#{bits}"
   end
 
-  end
   class Array
     def self.for(bits, type)
       `#{Buffer.name_for bits, type}Array`
@@ -83,6 +82,30 @@ class Buffer
     end
 
     alias []= set
+
+    def get_int8(offset, little = false); `#@native.getInt8(offset, little)`; end
+    def set_int8(offset, value, little = false); `#@native.setInt8(offset, value, little)`; end
+
+    def get_uint8(offset, little = false); `#@native.getUint8(offset, little)`; end
+    def set_uint8(offset, value, little = false); `#@native.setUint8(offset, value, little)`; end
+
+    def get_int16(offset, little = false); `#@native.getInt16(offset, little)`; end
+    def set_int16(offset, value, little = false); `#@native.setInt16(offset, value, little)`; end
+
+    def get_uint16(offset, little = false); `#@native.getUint16(offset, little)`; end
+    def set_uint16(offset, value, little = false); `#@native.setUint16(offset, value, little)`; end
+
+    def get_int32(offset, little = false); `#@native.getInt32(offset, little)`; end
+    def set_int32(offset, value, little = false); `#@native.setInt32(offset, value, little)`; end
+
+    def get_uint32(offset, little = false); `#@native.getUint32(offset, little)`; end
+    def set_uint32(offset, value, little = false); `#@native.setUint32(offset, value, little)`; end
+
+    def get_float32(offset, little = false); `#@native.getFloat32(offset, little)`; end
+    def set_float32(offset, value, little = false); `#@native.setFloat32(offset, value, little)`; end
+
+    def get_float64(offset, little = false); `#@native.getFloat64(offset, little)`; end
+    def set_float64(offset, value, little = false); `#@native.setFloat64(offset, value, little)`; end
   end
 
   include Native
