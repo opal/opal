cfc9fd9657f68879fe9e414f043368054dbde95e
meh <meh@paranoici.org>
2012-01-19 14:25:20 +0100

    core/range: implement some missing methods

--- core/range.rb
+++ core/range.rb
@@ -11,19 +11,7 @@
 }
 
 class Range
-  def begin
-    `this.begin`
-  end
-
-  def end
-    `this.end`
-  end
-
-  alias first begin
-  alias min begin
-
-  alias last end
-  alias max end
+  include Enumerable
 
   def initialize(min, max, exclude = false)
     @begin   = min
@@ -31,13 +19,84 @@ class Range
     @exclude = exclude
   end
 
+  def ==(other)
+    return false unless Range === other
+
+    exclude_end? == other.exclude_end? && `this.begin` == other.begin && `this.end` == other.end
+  end
+
   # FIXME: currently hardcoded to assume range holds numerics
   def ===(obj)
     `return obj >= this.begin && obj <= this.end`
   end
 
+  def begin
+    `this.begin`
+  end
+
+  def cover?(value)
+    `this.begin` <= value && value <= (exclude_end? ? `this.end` - 1 : `this.end`)
+  end
+
+  def each
+    return enum_for :each unless block_given?
+
+    current = min
+
+    while current != max
+      yield current
+
+      current = current.succ
+    end
+
+    yield current unless exclude_end?
+
+    self
+  end
+
+  def end
+    `this.end`
+  end
+
+  def eql?(other)
+    return false unless Range === other
+
+    exclude_end? == other.exclude_end? && `this.begin`.eql?(other.begin) && `this.end`.eql?(other.end)
+  end
+
   def exclude_end?
     `this.exclude`
+  end
+
+  # FIXME: currently hardcoded to assume range holds numerics
+  def include?(val)
+    `return obj >= this.begin && obj <= this.end`
+  end
+
+  def max
+    if block_given?
+      # I actually don't get what this should do
+      raise NotImplementedError
+    else
+      `this.end`
+    end
+  end
+
+  def min
+    if block_given?
+      # I actually don't get what this should do
+      raise NotImplementedError
+    else
+      `this.begin`
+    end
+  end
+
+  alias member? include?
+
+  def step(n = 1)
+    return enum_for :step, n unless block_given?
+
+    raise NotImplementedError
   end
 
   def to_s
