fae0ea30876d58baa6c4e2b459db87d653178b73
Adam Beynon <adam@adambeynon.com>
2012-05-17 11:20:55 +0100

    Added Document.ready? method for observing ready state
    The Document.ready? method is useful for registering blocks of code
    to be run once the document becomes ready. This adds the default
    listening event for browsers that support DOMContentReady.
    
    This method does not currently work with browsers not supporting
    this event (IE < 10), but will be added at a later stage.
    
    No tests added for this method as a manual will need to be added
    in future.

--- browser/document.rb
+++ browser/document.rb
@@ -36,4 +36,65 @@ module Document
       return this.head;
     }
   end
+
+  %x{
+    var loaded = false, callbacks = [];
+
+    var trigger = function() {
+      if (loaded) return;
+      loaded = true;
+
+      for (var i = 0, length = callbacks.length; i < length; i++) {
+        var callback = callbacks[i];
+        callback.call(callback._s);
+      }
+    };
+
+    if (document.addEventListener) {
+      document.addEventListener('DOMContentLoaded', trigger, false);
+    }
+    else {
+      console.log("No Document.ready? in this browser");
+    }
+  }
+
+  # Used to both register blocks that should be once the Document is
+  # ready, as well as returning a boolean to indicate the ready state.
+  #
+  # Multiple blocks can be passed to this method which will then be run
+  # in order once the Document becomes ready. If no block is given then
+  # the ready state is just returned.
+  #
+  # The ready state is an indication of whether the document is ready
+  # for manipulation. Commonly, until the document is ready, there is
+  # no guarantee that the {body} element is created.
+  #
+  # This method will also try and perform in a cross browser way to
+  # eliminate any differences between browsers and their ready state.
+  #
+  # @example
+  #
+  #     Document.ready? do
+  #       puts "Document is ready and loaded!"
+  #     end
+  #
+  #     Document.ready?     # => false
+  #     # page load...
+  #     Document.ready?     # => true
+  #
+  # @return [true, false] document ready state  
+  def self.ready?(&block)
+    %x{
+      if (block && block !== nil) {
+        if (loaded) {
+          block.call(block._s);
+        }
+        else {
+          callbacks.push(block);
+        }
+      }
+
+      return loaded;
+    }
+  end
 end
