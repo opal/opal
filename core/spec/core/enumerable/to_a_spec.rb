d73f1c3108015c313886ed03192887cad900456c
Adam Beynon <adam@adambeynon.com>
2012-05-23 12:21:03 +0100

    Add missing to_a and take specs from enumerable

---
+++ core/spec/core/enumerable/to_a_spec.rb
@@ -0,0 +1,6 @@
+describe "Enumerable#to_a" do
+  it "returns an array containing the elements" do
+    numerous = EnumerableSpecs::Numerous.new(1, nil, 'a', 2, false, true)
+    numerous.to_a.should == [1, nil, "a", 2, false, true]
+  end
+end
