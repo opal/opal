88906b34e78bc7a3cf9f2daecb086099264f9e31
Adam Beynon <adam@adambeynon.com>
2012-05-24 19:52:49 +0100

    Add more ._methods specs for modules

--- test/opal/runtime/_methods_spec.rb
+++ test/opal/runtime/_methods_spec.rb
@@ -14,6 +14,20 @@ class RuntimeMethodsSpec3
   def kapow; end
 end
 
+module RuntimeMethodsSpec4
+  def ding; end
+end
+
+module RuntimeMethodsSpec4
+  def dong; end
+end
+
+class RuntimeMethodsSpec5
+  include RuntimeMethodsSpec4
+
+  def thor; end
+end
+
 describe "Class._methods private array" do
   it "should store a list of all defined methods on classes as jsid's" do
     `#{RuntimeMethodsSpec}._methods`.should == []
@@ -22,5 +36,13 @@ describe "Class._methods private array" do
 
   it "correctly adds methods when reopening classes" do
     `#{RuntimeMethodsSpec3}._methods`.should == ['$woosh', '$kapow']
+  end
+
+  it "should store methods for modules" do
+    `#{RuntimeMethodsSpec4}._methods`.should == ['$ding', '$dong']
+  end
+
+  it "should not include methods from included modules" do
+    `#{RuntimeMethodsSpec5}._methods`.should == ['$thor']
   end
 end
