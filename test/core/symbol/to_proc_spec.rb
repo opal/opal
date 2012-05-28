b9eb94fca086ec678b3520409e5d869417897668
Adam Beynon <adam@adambeynon.com>
2012-05-28 10:39:47 +0100

    Add test for Symbol#to_proc ensuring arg is correctly passed

--- test/core/symbol/to_proc_spec.rb
+++ test/core/symbol/to_proc_spec.rb
@@ -3,4 +3,10 @@ describe "Symbol#to_proc" do
     proc = :to_s.to_proc
     proc.should be_kind_of(Proc)
   end
+
+  it "sends self to arguments passed when calling #call on the proc" do
+    obj = Object.new
+    def obj.to_s; "Received #to_s"; end
+    :to_s.to_proc.call(obj).should == "Received #to_s"
+  end
 end
