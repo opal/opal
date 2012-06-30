53d95695c6960f2d89f0e5e7b3b241391f735309
Elia Schito <elia@schito.me>
2012-06-30 16:45:14 +0200

    Fix dummy app path

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ require 'opal-rails'
 # Configure Rails Environment
 ENV['RAILS_ENV'] = 'test'
 
-require File.expand_path('../dummy/config/environment.rb',  __FILE__)
+require File.expand_path('../test_app/config/environment.rb',  __FILE__)
 require 'rspec/rails'
 require 'capybara/rspec'
 
