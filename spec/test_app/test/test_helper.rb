dcc0b388a7560c397f906f42d979f99f69ca4dce
Elia Schito <elia@schito.me>
2012-06-30 16:02:25 +0200

    Add test app

---
+++ spec/test_app/test/test_helper.rb
@@ -0,0 +1,7 @@
+ENV["RAILS_ENV"] = "test"
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
+
+class ActiveSupport::TestCase
+  # Add more helper methods to be used by all tests here...
+end
