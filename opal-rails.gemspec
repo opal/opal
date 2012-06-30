2bf319da89b211c2629cc18adf3205eef0e8bd2a
Elia Schito <elia@schito.me>
2012-06-30 16:40:11 +0200

    Add rspec-rails

--- opal-rails.gemspec
+++ opal-rails.gemspec
@@ -23,4 +23,5 @@ Gem::Specification.new do |s|
   s.add_runtime_dependency 'sprockets', '~> 2.1'
   
   s.add_development_dependency 'rspec', '~> 2.4'
+  s.add_development_dependency 'rspec-rails', '~> 2.4'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,24 @@
 $:.unshift File.expand_path('../../lib')
 require 'opal-rails'
+
+
+# Configure Rails Environment
+ENV['RAILS_ENV'] = 'test'
+
+require File.expand_path('../dummy/config/environment.rb',  __FILE__)
+require 'rspec/rails'
+require 'capybara/rspec'
+
+Rails.backtrace_cleaner.remove_silencers!
+
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+RSpec.configure do |config|
+  # config.mock_with :mocha
+  config.mock_with :rspec
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.use_transactional_fixtures = true
+end
