fab2304155eaeec1d594e0f8a669049025ce8601
Elia Schito <elia@schito.me>
2012-07-01 00:21:33 +0200

    Make it available in test env too

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  match '/opal_spec' => 'opal_spec#run' if Rails.env.development?
+  match '/opal_spec' => 'opal_spec#run' if %w[test development].include? Rails.env
 end
