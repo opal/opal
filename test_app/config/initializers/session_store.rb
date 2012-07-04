8fbe13bf203444527516d75bf570a20530abcbbc
Elia Schito <elia@schito.me>
2012-07-04 19:07:06 +0200

    Use 1.8 hash syntax in the test_app

--- test_app/config/initializers/session_store.rb
+++ test_app/config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-TestApp::Application.config.session_store :cookie_store, key: '_test_app_session'
+TestApp::Application.config.session_store :cookie_store, :key => '_test_app_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
--- test_app/config/initializers/wrap_parameters.rb
+++ test_app/config/initializers/wrap_parameters.rb
@@ -5,6 +5,6 @@
 
 # Enable parameter wrapping for JSON. You can disable this by setting :format to an empty array.
 ActiveSupport.on_load(:action_controller) do
-  wrap_parameters format: [:json]
+  wrap_parameters :format => [:json]
 end
 
