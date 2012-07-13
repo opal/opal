84e7f6598a447b266335e555afde293838d58eab
Elia Schito <elia@schito.me>
2012-07-13 22:48:22 +0200

    Stop eager loading from requiring assets/**.rb files

--- lib/opal/rails/engine.rb
+++ lib/opal/rails/engine.rb
@@ -4,6 +4,10 @@ module Opal
   module Rails
     class Engine < ::Rails::Engine
       config.app_generators.javascript_engine :opal
+
+      # Cache eager_load_paths now, otherwise the assets dir is added
+      # and its .rb files are eagerly loaded.
+      config.eager_load_paths
     end
   end
 end
