0a7d926ab27263809498132d003eaa689a0e68d6
Adam Beynon <adam.beynon@gmail.com>
2012-09-12 21:21:42 +0100

    Use opal_spec_runner for running phantom specs

--- Rakefile
+++ Rakefile
@@ -12,14 +12,7 @@ end
 
 desc "Run tests"
 task :test do
-  src = %w(build/opal.js build/opal-spec.js build/opal-parser.js build/specs.js)
-  out = 'build/phantom_runner.js'
-
-  File.open(out, 'w+') do |o|
-    src.each { |s| o.write File.read(s) }
-  end
-
-  sh "phantomjs build/phantom_runner.js"
+  sh "phantomjs vendor/opal_spec_runner.js spec/index.html"
 end
 
 task :default => :test
---
+++ vendor/opal_spec_runner.js
@@ -0,0 +1,43 @@
+var args = phantom.args;
+
+if (args.length !== 1) {
+  console.log("Usage: " + phantom.scriptName + " <URL>");
+  phantom.exit(1);
+}
+
+var page = require('webpage').create();
+
+page.onConsoleMessage = function(msg) {
+  console.log(msg);
+};
+
+page.onInitialized = function() {
+  page.evaluate(function () {
+    window.OPAL_SPEC_PHANTOM = true;
+  });
+};
+
+page.open(args[0], function(status) {
+  if (status !== 'success') {
+    console.error("Cannot load: " + args[0]);
+    phantom.exit(1);
+  } else {
+    var timeout = parseInt(args[1] || 60000, 10);
+    var start = Date.now();
+    var interval = setInterval(function() {
+      if (Date.now() > start + timeout) {
+        console.error("Specs timed out");
+        phantom.exit(124);
+      } else {
+        var code = page.evaluate(function() {
+          return window.OPAL_SPEC_CODE;
+        });
+
+        if (code === 0 || code === 1) {
+          clearInterval(interval);
+          phantom.exit(code);
+        }
+      }
+    }, 500);
+  }
+});
