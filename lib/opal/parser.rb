6ca8ae7f1005848c6a03467f09992ff3f08dcf71
Adam Beynon <adam.beynon@gmail.com>
2012-09-18 11:47:55 +0100

    Revert removal of comments commit

--- lib/opal/parser.rb
+++ lib/opal/parser.rb
@@ -29,7 +29,6 @@ module Opal
     def parse(source, file = '(file)')
       @grammar  = Grammar.new
       @requires = []
-      @comments = false
       @file     = file
       @line     = 1
       @indent   = ''
@@ -699,7 +698,7 @@ module Opal
       spacer  = "\n#{@indent}#{INDENT}"
       cls     = "function #{name}() {};"
       boot    = "#{name} = __klass(__base, __super, #{name.inspect}, #{name});"
-      comment = "#{spacer}// line #{ sexp.line }, #{ @file }, class #{ name }" if @comment
+      comment = "#{spacer}// line #{ sexp.line }, #{ @file }, class #{ name }"
 
       "(function(__base, __super){#{comment}#{spacer}#{cls}#{spacer}#{boot}\n#{code}\n#{@indent}})(#{base}, #{sup})"
     end
@@ -753,7 +752,7 @@ module Opal
       spacer  = "\n#{@indent}#{INDENT}"
       cls     = "function #{name}() {};"
       boot    = "#{name} = __module(__base, #{name.inspect}, #{name});"
-      comment = "#{spacer}// line #{ sexp.line }, #{ @file }, module #{ name }" if @comment
+      comment = "#{spacer}// line #{ sexp.line }, #{ @file }, module #{ name }"
 
       "(function(__base){#{comment}#{spacer}#{cls}#{spacer}#{boot}\n#{code}\n#{@indent}})(#{base})"
     end
@@ -874,7 +873,6 @@ module Opal
       end
 
       comment += "\n#{@indent}"
-      comment = nil unless @comments
 
       if recvr
         if smethod
