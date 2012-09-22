301534333cf062a96c6b941a93c17b5db511b6a8
Adam Beynon <adam.beynon@gmail.com>
2012-09-22 10:38:59 +0100

    Update Array#- (minus) specs to reflect rubyspec tests

--- spec/core/array/minus_spec.rb
+++ spec/core/array/minus_spec.rb
@@ -1,13 +1,23 @@
 describe "Array#-" do
-  it "subtracts two arrays" do
-    ([ 1, 2, 3 ] - [ 3, 4, 5 ] ).should == [1, 2]
-    ([ 1, 2, 3 ] - []).should == [1, 2, 3]
-    ([] - [ 1, 2, 3 ]).should == []
-    ([] - []).should == []
+  it "creates an array minus any items from other array" do
+    ([] - [1, 2, 4]).should == []
+    ([1, 2, 4] - []).should == [1, 2, 4]
+    ([1, 2, 3, 4, 5] - [1, 2, 4]).should == [3, 5]
   end
 
-  it "can subtracts an array from itself" do
-    ary = [1, 2, 3]
-    (ary - ary).should == []
+  it "removes multiple items on the lhs equal to one on the rhs" do
+    ([1, 1, 2, 2, 3, 3, 4, 5] - [1, 2, 4]).should == [3, 3, 5]
+  end
+
+  it "is not destructive" do
+    a = [1, 2, 3]
+    a - []
+    a.should == [1, 2, 3]
+    a - [1]
+    a.should == [1, 2, 3]
+    a - [1, 2, 3]
+    a.should == [1, 2, 3]
+    a - [:a, :b, :c]
+    a.should == [1, 2, 3]
   end
 end
