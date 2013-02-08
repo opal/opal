d95cba9b92850617f431a6f31c0a5c4af59db438
Adam Beynon <adam.beynon@gmail.com>
2013-02-08 14:10:52 +0000

    Array#shuffle may return an array in same order

--- spec/core/array/shuffle_spec.rb
+++ spec/core/array/shuffle_spec.rb
@@ -1,11 +1,11 @@
 describe "Array#shuffle" do
   it "returns an array mixed up" do
     ary = [1, 2, 3, 4, 5]
-    [1, 2, 3, 4, 5].shuffle.should_not == ary
+    [1, 2, 3, 4, 5].shuffle.size.should eq(ary.size)
   end
 
   it "returns an array mixed up with UTF-8 characters" do
     ary = ["á", "é", "à", "ñÑ", "¥ØŁØ"]
-    ["á", "é", "à", "ñÑ", "¥ØŁØ"].shuffle.should_not == ary
+    ["á", "é", "à", "ñÑ", "¥ØŁØ"].shuffle.size.should eq(ary.size)
   end
 end
