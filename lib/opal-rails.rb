d861d2605c3c01d971c5ab5e04503eba824fe5aa
Elia Schito <elia@schito.me>
2013-02-09 17:40:28 +0100

    Explicitly load dependencies

--- lib/opal-rails.rb
+++ lib/opal-rails.rb
@@ -1,5 +1,10 @@
 require 'opal'
+require 'opal/jquery'
+require 'opal/spec'
+
 require 'opal/rails/engine'
 require 'opal/rails/template_handler'
 require 'opal/rails/version'
 require 'opal/rails/haml_filter' if defined?(Haml)
+
+require 'jquery-rails'
