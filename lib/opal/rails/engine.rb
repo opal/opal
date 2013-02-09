206a86934b5d37f65f2e7c988d00d9d441e18a43
Elia Schito <elia@schito.me>
2013-02-09 17:42:44 +0100

    Add Opal paths to the sprocket enviironment

--- lib/opal/rails/engine.rb
+++ lib/opal/rails/engine.rb
@@ -8,6 +8,17 @@ module Opal
       # Cache eager_load_paths now, otherwise the assets dir is added
       # and its .rb files are eagerly loaded.
       config.eager_load_paths
+
+      initializer 'opal.asset_paths', :after => 'sprockets.environment', :group => :all do |app|
+        app.config.before_initialize do
+          app.config.eager_load_paths = app.config.eager_load_paths.dup - Dir["#{app.root}/app/{assets,views}"]
+        end
+
+        Opal.paths.each do |path|
+          app.assets.append_path path
+        end
+      end
+
     end
   end
 end
