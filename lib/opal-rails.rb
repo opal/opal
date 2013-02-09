00bceaba979ce775652a6fcbf58f3cf8d953ea29
Elia Schito <elia@schito.me>
2013-02-09 17:36:44 +0100

    Tilt processor is now part of Opal

--- lib/opal-rails.rb
+++ lib/opal-rails.rb
@@ -1,6 +1,5 @@
 require 'opal'
 require 'opal/rails/engine'
 require 'opal/rails/template_handler'
-require 'opal/rails/processor'
 require 'opal/rails/version'
 require 'opal/rails/haml_filter' if defined?(Haml)
--- spec/opal/processor_spec.rb
+++ spec/opal/processor_spec.rb
@@ -1,22 +1,22 @@
 require 'spec_helper'
 
-describe Opal::Rails::Processor do
+describe Opal::Processor do
 
   it "is registered for '.opal' files" do
-    Tilt['test.opal'].should eq(Opal::Rails::Processor)
+    Tilt['test.opal'].should eq(Opal::Processor)
   end
 
   it "is registered for '.rb' files" do
-    Tilt['test.rb'].should eq(Opal::Rails::Processor)
+    Tilt['test.rb'].should eq(Opal::Processor)
   end
 
   it "compiles and evaluates the template on #render" do
-    template = Opal::Rails::Processor.new { |t| "puts 'Hello, World!'\n" }
+    template = Opal::Processor.new { |t| "puts 'Hello, World!'\n" }
     template.render.should include('self.$puts("Hello, World!")')
   end
 
   it "can be rendered more than once" do
-    template = Opal::Rails::Processor.new { |t| "puts 'Hello, World!'\n" }
+    template = Opal::Processor.new { |t| "puts 'Hello, World!'\n" }
     3.times { template.render.should include('self.$puts("Hello, World!")') }
   end
 end
