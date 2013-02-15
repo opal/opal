29e199857c7c3d42de920e9c0e89980aedd4376f
Adam Beynon <adam.beynon@gmail.com>
2013-02-15 14:42:59 +0000

    Add core template.rb for the basis of template libraries for opal

---
+++ lib/assets/javascripts/opal.rb
@@ -0,0 +1,36 @@
+require 'opal/runtime'
+require 'opal/class'
+require 'opal/basic_object'
+require 'opal/kernel'
+require 'opal/nil_class'
+require 'opal/boolean'
+require 'opal/error'
+require 'opal/regexp'
+require 'opal/comparable'
+require 'opal/enumerable'
+require 'opal/array'
+require 'opal/hash'
+require 'opal/string'
+require 'opal/numeric'
+require 'opal/proc'
+require 'opal/range'
+require 'opal/time'
+require 'opal/date'
+require 'opal/json'
+
+# regexp matches
+$~ = nil
+
+# split lines
+$/ = "\n"
+
+RUBY_ENGINE   = 'opal'
+RUBY_PLATFORM = 'opal'
+
+def to_s
+  'main'
+end
+
+def include(mod)
+  Object.include mod
+end
