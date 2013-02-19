d83fcdf50e71137493907320cbf24900ac582534
Adam Beynon <adam.beynon@gmail.com>
2013-02-19 19:04:28 +0000

    Keep track of subclasses of a class through a _inherited private property

---
+++ spec/core_ext/class/_inherited_spec.rb
@@ -0,0 +1,32 @@
+require "spec_helper"
+
+class Class
+  def get_inherited_classes
+    `#{self}._inherited`
+  end
+end
+
+module ClassInheritedSpecs
+  class None
+  end
+
+  class A
+  end
+
+  class B < A
+  end
+
+  class C < A
+  end
+
+  class D < C
+  end
+end
+
+describe "Class '_inherited' variable" do
+  it "contains an array of all subclasses of class" do
+    ClassInheritedSpecs::None.get_inherited_classes.should eq([])
+    ClassInheritedSpecs::A.get_inherited_classes.should eq([ClassInheritedSpecs::B, ClassInheritedSpecs::C])
+    ClassInheritedSpecs::C.get_inherited_classes.should eq([ClassInheritedSpecs::D])
+  end
+end
