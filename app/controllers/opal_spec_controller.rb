8021507a89806a4e74e87c84393359808358f5de
Elia Schito <elia@schito.me>
2013-02-22 12:26:41 +0100

    Accept globs as spec pattern & run all by default

--- app/controllers/opal_spec_controller.rb
+++ app/controllers/opal_spec_controller.rb
@@ -1,13 +1,32 @@
 class OpalSpecController < ActionController::Base
+  helper_method :spec_files
+
   def run
   end
-  
-  
+
+
   private
-  
+
   def spec_files
-    @spec_files ||= (params[:files] || 'spec').split(':')
+    @spec_files ||= some_spec_files || all_spec_files
   end
-  
-  helper_method :spec_files
+
+  def specs_param
+    params[:pattern]
+  end
+
+  def some_spec_files
+    return if specs_param.blank?
+    specs_param.split(':').map { |path| spec_files_for_glob(path) }.flatten
+  end
+
+  def all_spec_files
+    spec_files_for_glob '**'
+  end
+
+  def spec_files_for_glob glob = '**'
+    Dir[Rails.root.join("{app,lib}/assets/javascripts/spec/#{glob}{,_spec.js.{rb,opal}}")].map do |path|
+      path.split('assets/javascripts/spec/').last
+    end.uniq
+  end
 end
--- spec/integration/in_browser_specs_spec.rb
+++ spec/integration/in_browser_specs_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'In-browser specs runner' do
   it 'runs all specs', :js do
     visit '/opal_spec'
-    page.should have_content('Running: spec')
+    page.should have_content('Running: example_spec.js.rb')
     page.should have_content('1 examples, 0 failures')
   end
 end
