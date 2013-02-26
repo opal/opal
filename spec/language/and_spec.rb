04dd82e565212db1754bb95d674955ba85a5ffab
Adam Beynon <adam.beynon@gmail.com>
2013-02-26 17:50:30 +0000

    Get all 'and' specs passing

--- spec/language/and_spec.rb
+++ spec/language/and_spec.rb
@@ -1,23 +1,27 @@
+require File.expand_path('../../spec_helper', __FILE__)
+
 describe "The '&&' statement" do
+
   it "short-circuits evaluation at the first condition to be false" do
     x = nil
     true && false && x = 1
     x.should be_nil
   end
 
-  it "evaluates to the first condition not to be true" do
+  it "evalutes to the first condition not to be true" do
     ("yes" && 1 && nil && true).should == nil
     ("yes" && 1 && false && true).should == false
   end
 
-  it "evaluates to the last condition if all are true" do
+  it "evalutes to the last condition if all are true" do
     ("yes" && 1).should == 1
     (1 && "yes").should == "yes"
   end
 
   it "evaluates the full set of chained conditions during assignment" do
-    x = y = nil
+    x, y = nil
     x = 1 && y = 2
+    # "1 && y = 2" is evaluated and then assigned to x
     x.should == 2
   end
 
@@ -26,6 +30,7 @@ describe "The '&&' statement" do
     (true && ()).should be_nil
     (() && ()).should be_nil
   end
+
 end
 
 describe "The 'and' statement" do
@@ -35,19 +40,20 @@ describe "The 'and' statement" do
     x.should be_nil
   end
 
-  it "evaluates to the first condition not to be true" do
+  it "evalutes to the first condition not to be true" do
     ("yes" and 1 and nil and true).should == nil
     ("yes" and 1 and false and true).should == false
   end
 
-  it "evaluates to the last condition if all are true" do
+  it "evalutes to the last condition if all are true" do
     ("yes" and 1).should == 1
     (1 and "yes").should == "yes"
   end
 
   it "when used in assignment, evaluates and assigns expressions individually" do
-    x = y = nil
+    x, y = nil
     x = 1 and y = 2
+    # evaluates (x=1) and (y=2)
     x.should == 1
   end
 
@@ -56,4 +62,5 @@ describe "The 'and' statement" do
     (true and ()).should be_nil
     (() and ()).should be_nil
   end
+
 end
