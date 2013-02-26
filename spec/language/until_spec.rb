4cfa29c4ac8f633f9339645c6cb796ace2fefee2
Adam Beynon <adam.beynon@gmail.com>
2013-02-26 18:15:51 +0000

    Update until specs to rubyspecs

--- spec/language/until_spec.rb
+++ spec/language/until_spec.rb
@@ -1,3 +1,14 @@
+require File.expand_path('../../spec_helper', __FILE__)
+
+# until bool-expr [do]
+#   body
+# end
+#
+# begin
+#   body
+# end until bool-expr
+#
+# expr until bool-expr
 describe "The until expression" do
   it "runs while the expression is false" do
     i = 0
@@ -91,6 +102,18 @@ describe "The until expression" do
     end
     a.should == [1, 2, 4]
   end
+
+  pending "restarts the current iteration without reevaluating condition with redo" do
+    a = []
+    i = 0
+    j = 0
+    until (i+=1)>=3
+      a << i
+      j+=1
+      redo if j<3
+    end
+    a.should == [1, 1, 1, 2]
+  end
 end
 
 describe "The until modifier" do
@@ -104,10 +127,10 @@ describe "The until modifier" do
     a = []
     i = 0
     a << i until (i+=1) >= 3
-    a.should == [1,2]
+    a.should == [1, 2]
   end
 
-  it "does not run the preceding statement if the condition is true" do
+  it "does not run preceding statement if the condition is true" do
     i = 0
     i += 1 until true
     i.should == 0
@@ -131,5 +154,81 @@ describe "The until modifier" do
     j = 0
     ((i+=1) == 3 ? next : j+=i) until i > 10
     j.should == 63
+  end
+
+  pending "restarts the current iteration without reevaluating condition with redo" do
+    i = 0
+    j = 0
+    (i+=1) == 4 ? redo : j+=i until (i+=1) > 10
+    j.should == 34
+  end
+end
+
+describe "The until modifier with begin .. end block" do
+  pending "runs block while the expression is false" do
+    i = 0
+    begin
+      i += 1
+    end until i > 9
+
+    i.should == 10
+  end
+
+  pending "stops running block if interrupted by break" do
+    i = 0
+    begin
+      i += 1
+      break if i > 5
+    end until i > 9
+
+    i.should == 6
+  end
+
+  pending "returns value passed to break if interrupted by break" do
+    (begin; break 123; end until false).should == 123
+  end
+
+  pending "returns nil if interrupted by break with no arguments" do
+    (begin; break; end until false).should == nil
+  end
+
+  pending "runs block at least once (even if the expression is true)" do
+    i = 0
+    begin
+      i += 1
+    end until true
+
+    i.should == 1
+  end
+
+  pending "evaluates condition after block execution" do
+    a = []
+    i = 0
+    begin
+      a << i
+    end until (i+=1)>=5
+    a.should == [0, 1, 2, 3, 4]
+  end
+
+  pending "skips to end of body with next" do
+    a = []
+    i = 0
+    begin
+      next if i==3
+      a << i
+    end until (i+=1)>=5
+    a.should == [0, 1, 2, 4]
+  end
+
+  pending "restart the current iteration without reevaluting condition with redo" do
+    a = []
+    i = 0
+    j = 0
+    begin
+      a << i
+      j+=1
+      redo if j<3
+    end until (i+=1)>=3
+    a.should == [0, 0, 0, 1, 2]
   end
 end
