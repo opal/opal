c1af84d96dc99fcd39ecfc9724e7e90dae9c6a6d
Adam Beynon <adam.beynon@gmail.com>
2013-02-27 09:10:30 +0000

    Add proc specs from rubyspec

--- spec/language/proc_spec.rb
+++ spec/language/proc_spec.rb
@@ -1,37 +1,265 @@
-class ProcSpec
-  define_method :defined_method do
-    self
+require File.expand_path('../../spec_helper', __FILE__)
+
+describe "A Proc" do
+  it "captures locals from the surrounding scope" do
+    var = 1
+    lambda { var }.call.should == 1
   end
 
-  def call_proc
-    get_proc.call
+  ruby_version_is ""..."1.9" do
+    pending "overwrites a captured local when used as an argument" do
+      var = 1
+      lambda { |var| var }.call(2).should == 2
+      var.should == 2
+    end
   end
 
-  def get_proc
-    proc { self }
-  end
-end
-
-describe "Procs" do
-  before do
-    @obj = ProcSpec.new
+  ruby_version_is "1.9" do
+    it "does not capture a local when an argument has the same name" do
+      var = 1
+      lambda { |var| var }.call(2).should == 2
+      var.should == 1
+    end
   end
 
-  it "should use the 'self' value of wrapping scope" do
-    @obj.call_proc.should eq(@obj)
+  describe "taking zero arguments" do
+    before :each do
+      @l = lambda { 1 }
+    end
+
+    it "does not raise an exception if no values are passed" do
+      @l.call.should == 1
+    end
+
+    ruby_version_is ""..."1.9" do
+      it "does not raise an exception if a value is passed" do
+        @l.call(0).should == 1
+      end
+    end
+
+    ruby_version_is "1.9" do
+      pending "raises an ArgumentErro if a value is passed" do
+        lambda { @l.call(0) }.should raise_error(ArgumentError)
+      end
+    end
   end
 
-  it "instance_eval correctly sets proc self value" do
-    arr = []
-    arr.instance_eval(&@obj.get_proc).should eq(arr)
+  describe "taking || arguments" do
+    before :each do
+      @l = lambda { || 1 }
+    end
+
+    it "does not raise an exception when passed no values" do
+      @l.call.should == 1
+    end
+
+    pending "raises an ArgumentError if a value is passed" do
+      lambda { @l.call(0) }.should raise_error(ArgumentError)
+    end
   end
 
-  it "procs used by define_method use correct self value" do
-    @obj.defined_method.should eq(@obj)
+  describe "taking |a| arguments" do
+    before :each do
+      @l = lambda { |a| a }
+    end
+
+    pending "assigns the value passed to the argument" do
+      @l.call(2).should == 2
+    end
+
+    pending "does not destructure a single Array value" do
+      @l.call([1, 2]).should == [1, 2]
+    end
+
+    pending "does not call #to_ary to convert a single passed object to an Array" do
+      obj = mock("block yield to_ary")
+      obj.should_not_receive(:to_ary)
+
+      @l.call(obj).should equal(obj)
+    end
+
+    ruby_version_is ""..."1.9" do
+      it "assigns nil to the argument if no value is passed" do
+        @l.call.should be_nil
+      end
+
+      pending "assigns all the values passed to the argument as an Array" do
+        @l.call(1, 2).should == [1, 2]
+      end
+    end
+
+    ruby_version_is "1.9" do
+      pending "raises an ArgumentError if no value is passed" do
+        lambda { @l.call }.should raise_error(ArgumentError)
+      end
+    end
   end
 
-  it "procs used by define_singleton_method use correct self value" do
-    @obj.define_singleton_method(:bar) { self }
-    @obj.bar.should eq(@obj)
+  describe "taking |a, b| arguments" do
+    before :each do
+      @l = lambda { |a, b| [a, b] }
+    end
+
+    pending "raises an ArgumentError if passed no values" do
+      lambda { @l.call }.should raise_error(ArgumentError)
+    end
+
+    pending "raises an ArgumentError if passed one value" do
+      lambda { @l.call(0) }.should raise_error(ArgumentError)
+    end
+
+    it "assigns the values passed to the arguments" do
+      @l.call(1, 2).should == [1, 2]
+    end
+
+    pending "does not call #to_ary to convert a single passed object to an Array" do
+      obj = mock("proc call to_ary")
+      obj.should_not_receive(:to_ary)
+
+      lambda { @l.call(obj) }.should raise_error(ArgumentError)
+    end
+  end
+
+  describe "taking |a, *b| arguments" do
+    before :each do
+      @l = lambda { |a, *b| [a, b] }
+    end
+
+    pending "raises an ArgumentError if passed no values" do
+      lambda { @l.call }.should raise_error(ArgumentError)
+    end
+
+    pending "does not destructure a single Array value yielded" do
+      @l.call([1, 2, 3]).should == [[1, 2, 3], []]
+    end
+
+    pending "assigns all passed values after the first to the rest argument" do
+        @l.call(1, 2, 3).should == [1, [2, 3]]
+    end
+
+    pending "does not call #to_ary to convert a single passed object to an Array" do
+      obj = mock("block yield to_ary")
+      obj.should_not_receive(:to_ary)
+
+      @l.call(obj).should == [obj, []]
+    end
+  end
+
+  describe "taking |*| arguments" do
+    before :each do
+      # @l = lambda { |*| 1 }
+    end
+
+    pending "does not raise an exception when passed no values" do
+      @l.call.should == 1
+    end
+
+    pending "does not raise an exception when passed multiple values" do
+      @l.call(2, 3, 4).should == 1
+    end
+
+    pending "does not call #to_ary to convert a single passed object to an Array" do
+      obj = mock("block yield to_ary")
+      obj.should_not_receive(:to_ary)
+
+      @l.call(obj).should == 1
+    end
+  end
+
+  describe "taking |*a| arguments" do
+    before :each do
+      @l = lambda { |*a| a }
+    end
+
+    pending "assigns [] to the argument when passed no values" do
+      @l.call.should == []
+    end
+
+    pending "assigns the argument an Array wrapping one passed value" do
+      @l.call(1).should == [1]
+    end
+
+    pending "assigns the argument an Array wrapping all values passed" do
+      @l.call(1, 2, 3).should == [1, 2, 3]
+    end
+
+    pending "does not call #to_ary to convert a single passed object to an Array" do
+      obj = mock("block yield to_ary")
+      obj.should_not_receive(:to_ary)
+
+      @l.call(obj).should == [obj]
+    end
+  end
+
+  describe "taking |a, | arguments" do
+    before :each do
+      # @l = lambda { |a, | a }
+    end
+
+    pending "raises an ArgumentError when passed no values" do
+      lambda { @l.call }.should raise_error(ArgumentError)
+    end
+
+    pending "raises an ArgumentError when passed more than one value" do
+      lambda { @l.call(1, 2) }.should raise_error(ArgumentError)
+    end
+
+    pending "assigns the argument the value passed" do
+      @l.call(1).should == 1
+    end
+
+    pending "does not destructure when passed a single Array" do
+      @l.call([1,2]).should == [1, 2]
+    end
+
+    pending "does not call #to_ary to convert a single passed object to an Array" do
+      obj = mock("block yield to_ary")
+      obj.should_not_receive(:to_ary)
+
+      @l.call(obj).should == obj
+    end
+  end
+
+  describe "taking |(a, b)| arguments" do
+    before :each do
+      # @l = lambda { |(a, b)| [a, b] }
+    end
+
+    pending "raises an ArgumentError when passed no values" do
+      lambda { @l.call }.should raise_error(ArgumentError)
+    end
+
+    ruby_version_is ""..."1.9" do
+      pending "raises an ArgumentError when passed a single Array" do
+        lambda { @l.call([1, 2]) }.should raise_error(ArgumentError)
+      end
+
+      pending "raises an ArgumentError when passed a single object" do
+        obj = mock("block yield to_ary")
+        obj.should_not_receive(:to_ary)
+
+        lambda { @l.call(obj) }.should raise_error(ArgumentError)
+      end
+    end
+
+    ruby_version_is "1.9" do
+      pending "destructures a single Array value yielded" do
+        @l.call([1, 2]).should == [1, 2]
+      end
+
+      pending "calls #to_ary to convert a single passed object to an Array" do
+        obj = mock("block yield to_ary")
+        obj.should_receive(:to_ary).and_return([1, 2])
+
+        @l.call(obj).should == [1, 2]
+      end
+
+      pending "raises an TypeError if #to_ary does not return an Array" do
+        obj = mock("block yield to_ary invalid")
+        obj.should_receive(:to_ary).and_return(1)
+
+        lambda { @l.call(obj) }.should raise_error(TypeError)
+      end
+    end
   end
 end
