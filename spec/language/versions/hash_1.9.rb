7daab71a56cba093c367b7d7997528b0d2e3cc0f
Adam Beynon <adam.beynon@gmail.com>
2013-02-27 12:09:35 +0000

    Add hash 1.9 specs from rubyspec

---
+++ spec/language/versions/hash_1.9.rb
@@ -0,0 +1,18 @@
+describe "Hash literal" do
+  describe "new-style hash syntax" do
+    it "constructs a new hash with the given elements" do
+      {foo: 123}.should == {:foo => 123}
+      {rbx: :cool, specs: 'fail_sometimes'}.should == {:rbx => :cool, :specs => 'fail_sometimes'}
+    end
+
+    it "ignores a hanging comma" do
+      {foo: 123,}.should == {:foo => 123}
+      {rbx: :cool, specs: 'fail_sometimes',}.should == {:rbx => :cool, :specs => 'fail_sometimes'}
+    end
+
+    it "can mix and match syntax styles" do
+      {rbx: :cool, :specs => 'fail_sometimes'}.should == {:rbx => :cool, :specs => 'fail_sometimes'}
+      {'rbx' => :cool, specs: 'fail_sometimes'}.should == {'rbx' => :cool, :specs => 'fail_sometimes'}
+    end
+  end
+end
