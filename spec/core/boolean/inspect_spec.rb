ec60900a38765a1b6320ce1925e6075bdc5f1dde
Adam Beynon <adam.beynon@gmail.com>
2013-02-28 14:30:47 +0000

    Specs for true, false

--- spec/core/boolean/inspect_spec.rb
+++
@@ -1,9 +0,0 @@
-describe "Boolean#inspect" do
-  it "when false returns the string 'false'" do
-    false.inspect.should == "false"
-  end
-
-  it "when true returns the string 'true'" do
-    true.inspect.should == "true"
-  end
-end
--- spec/core/boolean/to_s_spec.rb
+++
@@ -1,9 +0,0 @@
-describe "Boolean#to_s" do
-  it "when true returns the string 'false'" do
-    false.to_s.should == "false"
-  end
-
-  it "when false returns the string 'true'" do
-    true.to_s.should == "true"
-  end
-end
