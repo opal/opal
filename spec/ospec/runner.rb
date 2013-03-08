cfe33b2d2885a29034b73c3017de43bed93b343d
Adam Beynon <adam.beynon@gmail.com>
2013-03-08 15:07:09 +0000

    cleanup opal specific mspec runners to closer match mspec commands

--- spec/ospec/runner.rb
+++ spec/ospec/runner.rb
@@ -1,6 +1,37 @@
 require 'ospec/phantom'
 require 'ospec/filter'
 
+# stdlib
+require 'opal/date'
+require 'opal/enumerator'
+
+ENV = {}
+
+module Kernel
+  def opal_eval(str)
+    code = Opal::Parser.new.parse str
+    `eval(#{code})`
+  end
+
+  def opal_parse(str, file='(string)')
+    Opal::Grammar.new.parse str, file
+  end
+
+  def opal_eval_compiled(javascript)
+    `eval(javascript)`
+  end
+
+  def eval(str)
+    opal_eval str
+  end
+end
+
+module Kernel
+  # FIXME: remove
+  def ruby_version_is(*); end
+  def pending(*); end
+end
+
 module MSpec
   def self.opal_runner
     @env = Object.new
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,35 +1,27 @@
-require 'opal'
-require 'opal-parser'
+unless ENV['MSPEC_RUNNER']
+  begin
+    require "pp"
+    require 'mspec/version'
+    require 'mspec/helpers'
+    require 'mspec/guards'
+    require 'mspec/runner/shared'
+    require 'mspec/matchers/be_ancestor_of'
+    require 'mspec/matchers/output'
+    require 'mspec/matchers/output_to_fd'
+    require 'mspec/matchers/complain'
+    require 'mspec/matchers/equal_element'
+    require 'mspec/matchers/equal_utf16'
+    require 'mspec/matchers/match_yaml'
+    require 'mspec/matchers/have_class_variable'
+    require 'mspec/matchers/have_constant'
+    require 'mspec/matchers/have_instance_method'
+    require 'mspec/matchers/have_instance_variable'
+    require 'mspec/matchers/have_method'
+    require 'mspec/matchers/have_private_instance_method'
+    require 'mspec/matchers/have_protected_instance_method'
+    require 'mspec/matchers/have_public_instance_method'
 
-require 'mspec'
-
-# stdlib
-require 'opal/date'
-require 'opal/enumerator'
-
-
-module Kernel
-  def opal_eval(str)
-    code = Opal::Parser.new.parse str
-    `eval(#{code})`
+  rescue LoadError
   end
-
-  def opal_parse(str, file='(string)')
-    Opal::Grammar.new.parse str, file
-  end
-
-  def opal_eval_compiled(javascript)
-    `eval(javascript)`
-  end
-
-  def eval(str)
-    opal_eval str
-  end
-end
-
-module Kernel
-  # FIXME: remove
-  def ruby_version_is(*); end
-  def pending(*); end
 end
 
