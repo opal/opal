f6e8d68d5178d81c6b5629c136426c5cd3df68f3
Adam Beynon <adam.beynon@gmail.com>
2013-04-25 15:42:21 +0100

    Cleanup custom mspec settings for specs

--- spec/ospec/runner.rb
+++ spec/ospec/runner.rb
@@ -1,6 +1,3 @@
-require 'ospec/phantom'
-require 'ospec/filter'
-
 # stdlib
 require 'opal/date'
 require 'opal/enumerator'
@@ -10,6 +7,136 @@ require 'opal/observer'
 require 'opal-localstorage'
 
 ENV = {}
+
+class OSpecFilter
+  def self.main
+    @main ||= self.new
+  end
+
+  def initialize
+    @filters = {}
+  end
+
+  def register
+    MSpec.register :exclude, self
+  end
+
+  def ===(description)
+    @filters.has_key? description
+  end
+
+  def register_filters(description, block)
+    instance_eval(&block)
+  end
+
+  def fails(description)
+    @filters[description] = true
+  end
+end
+
+class Object
+  def opal_filter(description, &block)
+    OSpecFilter.main.register_filters(description, block)
+  end
+end
+
+class PhantomFormatter
+  def initialize(out=nil)
+    @exception = @failure = false
+    @exceptions = []
+    @count = 0
+    @examples = 0
+
+    @current_state = nil
+  end
+
+  def register
+    MSpec.register :exception, self
+    MSpec.register :before,    self
+    MSpec.register :after,     self
+    MSpec.register :start,     self
+    MSpec.register :finish,    self
+    MSpec.register :abort,     self
+    MSpec.register :enter,     self
+  end
+
+  def green(str)
+    `console.log('\\033[32m' + str + '\\033[0m')`
+  end
+
+  def red(str)
+    `console.log('\\033[31m' + str + '\\033[0m')`
+  end
+
+  def log(str)
+    `console.log(str)`
+  end
+
+  def exception?
+    @exception
+  end
+
+  def failure?
+    @failure
+  end
+
+  def enter(describe); end
+
+  def before(state=nil)
+    @current_state = nil
+    @failure = @exception = false
+  end
+
+  def exception(exception)
+    @count += 1
+    @failure = @exception ? @failure && exception.failure? : exception.failure?
+    @exception = true
+    @exceptions << exception
+  end
+
+  def after(state = nil)
+    @current_state = nil
+    @examples += 1
+  end
+
+  def start
+    @start_time = Time.now.to_f
+  end
+
+  def finish
+    time = Time.now.to_f - @start_time
+
+    if @exceptions.empty?
+      log "\nFinished"
+      green "#{@examples} examples, #{@count} failures (time taken: #{time})"
+
+      finish_with_code 0
+    else
+      log "\nFailures:"
+
+      @exceptions.each_with_index do |exception, idx|
+        log "\n  #{idx + 1}. #{exception.description}"
+        red "\n    #{exception.message}"
+      end
+
+      log "\nFinished"
+      red "#{@examples} examples, #{@count} failures (time taken: #{time})"
+
+      finish_with_code(1)
+    end
+  end
+
+  def finish_with_code(code)
+    %x{
+      if (typeof(phantom) !== 'undefined') {
+        return phantom.exit(code);
+      }
+      else {
+        window.OPAL_SPEC_CODE = code;
+      }
+    }
+  end
+end
 
 class File
   def self.expand_path(*a); nil; end
