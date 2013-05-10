413671d29bf410ab596b43456940fe8f4eecfd80
Adam Beynon <adam.beynon@gmail.com>
2013-05-10 19:02:37 +0100

    Remove some commented code

--- opal/opal/native.rb
+++ opal/opal/native.rb
@@ -1,22 +1,3 @@
-#  def to_a
-#    %x{
-#      var n = #{@native}, result;
-
- #     if (n.length) {
-#        result = [];
-
-#        for (var i = 0, len = n.length; i < len; i++) {
-#          result.push(#{ Native.new `n[i]` });
-#        }
-#      }
-#      else {
-#        result = [n];
-#      }
-
-#      return result;
-#    }
-#  end
-
 $global = `Opal.global`
 $window = $global
 $document = $window.document
