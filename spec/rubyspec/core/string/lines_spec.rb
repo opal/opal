b8de1b6eb735d5a978087bd66b18f4ef5f9c4329
Dan Allen <dallen@redhat.com>
2013-05-11 05:18:31 -0600

    preserve trailing separator for String#lines

--- spec/rubyspec/core/string/lines_spec.rb
+++ spec/rubyspec/core/string/lines_spec.rb
@@ -3,6 +3,7 @@ describe "String#lines" do
     "first\nsecond\nthird".lines.class.should == Enumerator
     "first\nsecond\nthird".lines.entries.class.should == Array
     "first\nsecond\nthird".lines.entries.size.should == 3
-    "first\nsecond\nthird".lines.entries.should == ["first", "second", "third"]
+    "first\nsecond\nthird".lines.entries.should == ["first\n", "second\n", "third"]
+    "first\nsecond\nthird\n".lines.entries.should == ["first\n", "second\n", "third\n"]
   end
 end
