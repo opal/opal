229bb367e7f080b0c90cd245bad19660a0dfe879
Adam Beynon <adam.beynon@gmail.com>
2013-06-04 12:55:49 +0100

    Fix examples/native to use $global for window reference

--- examples/native/app/app.rb
+++ examples/native/app/app.rb
@@ -1,21 +1,27 @@
 require 'opal'
 
-$window.addEventListener 'DOMContentLoaded', proc {
+$global.addEventListener 'DOMContentLoaded', proc {
 
   css = <<-CSS
     body {
       font-family: 'Arial';
     }
+
+    h1 {
+      color: rgb(10, 94, 232);
+    }
   CSS
 
-  title = $document.createElement 'h1'
+  document = $global.document
+
+  title = document.createElement 'h1'
   title.className = 'main-title'
   title.innerHTML = 'Opal Native Example'
 
-  desc = $document.createElement 'p'
+  desc = document.createElement 'p'
   desc.innerHTML = "Hello world! From Opal."
 
-  target = $document.getElementById 'native-example'
+  target = document.getElementById 'native-example'
 
   unless target
     raise "'native-example' doesn't exist?"
@@ -24,15 +30,15 @@ $window.addEventListener 'DOMContentLoaded', proc {
   target.appendChild title
   target.appendChild desc
 
-  styles = $document.createElement 'style'
+  styles = document.createElement 'style'
   styles.type = 'text/css'
 
   if styles.respond_to? :styleSheet
     styles.styleSheet.cssText = css
   else
-    styles.appendChild $document.createTextNode css
+    styles.appendChild document.createTextNode css
   end
 
-  $document.getElementsByTagName('head')[0].appendChild(styles)
+  document.getElementsByTagName('head')[0].appendChild(styles)
 
 }, false
