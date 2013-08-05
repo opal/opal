253a3161611c4da59cfb9edcefb04e91f4e9bbcd
Adam Beynon <adam.beynon@gmail.com>
2013-08-05 15:18:08 +0100

    Add ERB::Util.html_escape and ERB::Util.h
    Used for escaping html content. Adds specs from rubyspec as well

--- stdlib/erb.rb
+++ stdlib/erb.rb
@@ -1,4 +1,17 @@
 require 'template'
 
 class ERB
+  module Util
+
+    `var escapes = { '&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&#39;'};`
+    `var escape_regexp = /[&<>"']/g;`
+
+    def html_escape(str)
+      `return ("" + str).replace(escape_regexp, function (m) { return escapes[m] });`
+    end
+
+    alias h html_escape
+    module_function :h
+    module_function :html_escape
+  end
 end
