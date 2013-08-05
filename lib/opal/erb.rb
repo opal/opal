8da941b60f5af5757fea0711bebf0a6676c13e84
Adam Beynon <adam.beynon@gmail.com>
2013-08-05 10:35:53 +0100

    Rewrite ERB compiler to be easier to follow (#346)
    The previous ERB compiler was a mess. This commit makes it easier to
    follow and read. It also makes it overridable, if we ever wish to.

--- lib/opal/erb.rb
+++ lib/opal/erb.rb
@@ -2,16 +2,42 @@ require 'opal/parser'
 
 module Opal
   module ERB
-    def self.parse(str, name='(erb)')
-      body = str.gsub('"', '\\"').gsub(/<%=([\s\S]+?)%>/) do
-        inner = $1.gsub(/\\'/, "'").gsub(/\\"/, '"')
-        "\")\nout.<<(#{ inner })\nout.<<(\""
-      end.gsub(/<%([\s\S]+?)%>/) do
-        "\")\n#{ $1 }\nout.<<(\""
+    def self.parse(source, file_name = '(erb)')
+      Compiler.new.compile source, file_name
+    end
+
+    class Compiler
+      def compile(source, file_name = '(erb)')
+        @source, @file_name, @result = source, file_name, source
+
+        self.fix_quotes
+        self.find_contents
+        self.find_code
+        self.wrap_compiled
+
+        Opal.parse @result
       end
 
-      code = "ERB.new('#{name}') do |out|\nout.<<(\"#{ body }\")\nout.join\nend\n"
-      Opal.parse code
+      def fix_quotes
+        @result = @result.gsub '"', '\\"'
+      end
+
+      def find_contents
+        @result = @result.gsub(/<%=([\s\S]+?)%>/) do
+          inner = $1.gsub(/\\'/, "'").gsub(/\\"/, '"')
+          "\")\nout.<<(#{ inner })\nout.<<(\""
+        end
+      end
+
+      def find_code
+        @result = @result.gsub(/<%([\s\S]+?)%>/) do
+          "\")\n#{ $1 }\nout.<<(\""
+        end
+      end
+
+      def wrap_compiled
+        @result = "ERB.new('#@file_name') do |out|\nout.<<(\"#@result\")\nout.join\nend\n"
+      end
     end
   end
 end
