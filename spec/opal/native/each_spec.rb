b8cf0b664e7cc37a0ed4aa54fc28c02b8822071a
meh <meh@schizofreni.co>
2013-08-20 16:03:43 +0200

    Adapt native specs to the latest Native changes

--- spec/opal/native/each_spec.rb
+++ spec/opal/native/each_spec.rb
@@ -5,10 +5,7 @@ describe "Native#each" do
     }
   end
 
-  it "returns an enumerator when no block is given" do
-    enum = Native(`{ a: 2, b: 3 }`).each
-
-    enum.should be_kind_of Enumerator
-    enum.to_a.should == [[:a, 2], [:b, 3]]
+  it "accesses the native when no block is given" do
+    Native(`{ a: 2, b: 3, each: function() { return 42; } }`).each.should == 42
   end
 end
--- spec/opal/native/new_spec.rb
+++ spec/opal/native/new_spec.rb
@@ -1,19 +1,6 @@
 describe "Native.new" do
-  it "fails when trying to instantiate a non derived class" do
-    lambda {
-      Native.new
-    }.should raise_error ArgumentError
-  end
-
-  it "should pass all arguments to derived classes" do
-    Class.new(Native) {
-      def initialize(a, b, &c)
-        @a, @b, @c = a, b, c
-      end
-
-      def proper?
-        not (@a.nil? || @b.nil? || @c.nil?)
-      end
-    }.new(1, 2) { }.proper?.should be_true
+  it "should return nil for null or undefined" do
+    Native(`null`).should be_nil
+    Native(`undefined`).should be_nil
   end
 end
