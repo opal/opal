438012deace6dfca6ec0863d6cb4f410b5879f2d
Adam Beynon <adam.beynon@gmail.com>
2013-09-18 14:49:10 +0100

    Tidy up more filters for rubyspecs

--- spec/rubyspec/language/block_spec.rb
+++ spec/rubyspec/language/block_spec.rb
@@ -435,42 +435,42 @@ describe "A block" do
 
   describe "taking |(a, b), c| arguments" do
     it "assigns nil to the arguments when yielded no values" do
-      # @y.z { |(a, b), c| [a, b, c] }.should == [nil, nil, nil]
+      @y.z { |(a, b), c| [a, b, c] }.should == [nil, nil, nil]
     end
 
     it "destructures a single one-level Array value yielded" do
-      # @y.s([1, 2]) { |(a, b), c| [a, b, c] }.should == [1, nil, 2]
+      @y.s([1, 2]) { |(a, b), c| [a, b, c] }.should == [1, nil, 2]
     end
 
     it "destructures a single multi-level Array value yielded" do
-      # @y.s([[1, 2, 3], 4]) { |(a, b), c| [a, b, c] }.should == [1, 2, 4]
+      @y.s([[1, 2, 3], 4]) { |(a, b), c| [a, b, c] }.should == [1, 2, 4]
     end
 
     it "calls #to_ary to convert a single yielded object to an Array" do
       obj = mock("block yield to_ary")
       obj.should_receive(:to_ary).and_return([1, 2])
 
-      # @y.s(obj) { |(a, b), c| [a, b, c] }.should == [1, nil, 2]
+      @y.s(obj) { |(a, b), c| [a, b, c] }.should == [1, nil, 2]
     end
 
     it "does not call #to_ary if the single yielded object is an Array" do
       obj = [1, 2]
       obj.should_not_receive(:to_ary)
 
-      # @y.s(obj) { |(a, b), c| [a, b, c] }.should == [1, nil, 2]
+      @y.s(obj) { |(a, b), c| [a, b, c] }.should == [1, nil, 2]
     end
 
     it "does not call #to_ary if the object does not respond to #to_ary" do
       obj = mock("block yield no to_ary")
 
-      # @y.s(obj) { |(a, b), c| [a, b, c] }.should == [obj, nil, nil]
+      @y.s(obj) { |(a, b), c| [a, b, c] }.should == [obj, nil, nil]
     end
 
     it "raises an TypeError if #to_ary does not return an Array" do
       obj = mock("block yield to_ary invalid")
       obj.should_receive(:to_ary).and_return(1)
 
-      # lambda { @y.s(obj) { |(a, b), c| } }.should raise_error(TypeError)
+      lambda { @y.s(obj) { |(a, b), c| } }.should raise_error(TypeError)
     end
   end
 
