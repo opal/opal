20d55fbab667dcae2653f7f266d8aebe09483838
Adam Beynon <adam.beynon@gmail.com>
2013-09-18 14:54:53 +0100

    Clean up some rubyspec filters

--- spec/filters/bugs/array/flatten.rb
+++ spec/filters/bugs/array/flatten.rb
@@ -1,9 +1,18 @@
 opal_filter "Array#flatten" do
+  fails "Array#flatten does not call flatten on elements"
+  fails "Array#flatten raises an ArgumentError on recursive arrays"
+  fails "Array#flatten flattens any element which responds to #to_ary, using the return value of said method"
+  fails "Array#flatten returns subclass instance for Array subclasses"
+  fails "Array#flatten with a non-Array object in the Array ignores the return value of #to_ary if it is nil"
+  fails "Array#flatten with a non-Array object in the Array raises a TypeError if the return value of #to_ary is not an Array"
   fails "Array#flatten raises a TypeError when the passed Object can't be converted to an Integer"
   fails "Array#flatten tries to convert passed Objects to Integers using #to_int"
 end
 
 opal_filter "Array#flatten!" do
+  fails "Array#flatten! does not call flatten! on elements"
+  fails "Array#flatten! raises an ArgumentError on recursive arrays"
+  fails "Array#flatten! flattens any elements which responds to #to_ary, using the return value of said method"
   fails "Array#flatten! raises a TypeError when the passed Object can't be converted to an Integer"
   fails "Array#flatten! tries to convert passed Objects to Integers using #to_int"
 end
