047b8efcd238803f975236937e7bbda54187deaa
Adam Beynon <adam.beynon@gmail.com>
2013-09-20 15:51:09 +0100

    Use true, false and nil specs from rubyspec

--- spec/rubyspec/core/nil/dup_spec.rb
+++
@@ -1,7 +0,0 @@
-require File.expand_path('../../../spec_helper', __FILE__)
-
-describe "NilClass#dup" do
-  it "raises a TypeError" do
-    lambda { nil.dup }.should raise_error(TypeError)
-  end
-end
