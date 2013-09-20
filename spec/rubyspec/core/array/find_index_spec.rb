c1de15a0fece818ea107a7f63f4b1ae8abaa4e9e
Adam Beynon <adam.beynon@gmail.com>
2013-09-20 14:59:19 +0100

    Convert some core/array specs to use rubyspec

--- spec/rubyspec/core/array/find_index_spec.rb
+++
@@ -1,8 +0,0 @@
-require File.expand_path('../../../spec_helper', __FILE__)
-require File.expand_path('../shared/index', __FILE__)
-
-describe "Array#find_index" do
-  ruby_version_is "1.8.7" do
-    it_behaves_like :array_index, :find_index
-  end
-end
