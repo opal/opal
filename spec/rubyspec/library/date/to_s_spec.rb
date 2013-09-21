33902e10254aeaefa83a165b616dfeb8192431ba
Adam Beynon <adam.beynon@gmail.com>
2013-09-21 16:15:41 +0100

    Remove last files from spec/rubyspec/library

--- spec/rubyspec/library/date/to_s_spec.rb
+++
@@ -1,7 +0,0 @@
-require "spec_helper"
-
-describe "Date#to_s" do
-  it "returns a string representation of date (YYYY-MM-DD)" do
-    Date.new(2012, 10, 26).to_s.should == "2012-10-26"
-  end
-end
--- spec/rubyspec/library/date/today_spec.rb
+++
@@ -1,7 +0,0 @@
-require "spec_helper"
-
-describe "Date.today" do
-  it "creates a new instance for current date" do
-    Date.today.should be_kind_of(Date)
-  end
-end
