c6866c26dd5bee13a924eb4d44601581cfa5e181
Elia Schito <elia@schito.me>
2013-09-21 15:29:55 +0200

    Add specs for 619d46b76acf9906909f62c1ad9248b26c4f1869 and 8a1f243f11723ab6e7320341a59d148ed20c73c6

---
+++ spec/opal/native/initialize_spec.rb
@@ -0,0 +1,17 @@
+require 'native'
+
+describe "Native::Base#initialize" do
+  it "works when Native::Base is included in a BasicObject" do
+    basic_class = Class.new(BasicObject)
+    basic_object = basic_class.new
+    lambda { basic_object.native? }.should raise_error(NoMethodError)
+
+    basic_class.send :include, Native::Base
+    lambda { basic_class.new(`{}`) }.should_not raise_error
+  end
+
+  it "detects a non native object" do
+    object = Object.new
+    lambda { Native.new(object) }.should raise_error(ArgumentError)
+  end
+end
