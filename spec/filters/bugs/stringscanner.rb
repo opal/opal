dbc02526dcb6909049b3b1159cdb576bc31e4f58
Adam Beynon <adam.beynon@gmail.com>
2013-09-21 15:39:34 +0100

    Use rubyspec for stringscanner specs

---
+++ spec/filters/bugs/stringscanner.rb
@@ -0,0 +1,18 @@
+opal_filter "StringScanner" do
+  fails "StringScanner#[] calls to_int on the given index"
+  fails "StringScanner#[] raises a TypeError if the given index is nil"
+  fails "StringScanner#[] raises a TypeError when a Range is as argument"
+
+  fails "StringScanner#get_byte is not multi-byte character sensitive"
+
+  fails "StringScanner#pos returns the position of the scan pointer"
+  fails "StringScanner#pos returns the position of the scan pointer for multibyte string"
+  fails "StringScanner#pos returns 0 in the reset position"
+  fails "StringScanner#pos returns the length of the string in the terminate position"
+  fails "StringScanner#pos returns the `bytesize` for multibyte string in the terminate position"
+  fails "StringScanner#pos= raises a RangeError if position too far backward"
+  fails "StringScanner#pos= raises a RangeError when the passed argument is out of range"
+
+  fails "StringScanner#scan returns the matched string for a multi byte string"
+  fails "StringScanner#scan raises a TypeError if pattern isn't a Regexp"
+end
