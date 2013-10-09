d2af4e568a976961df3354f3ad93fa6f215ad31a
Elia Schito <elia@schito.me>
2013-10-09 09:53:21 +0200

    Add Pathname#absolute? and #relative?

--- stdlib/pathname.rb
+++ stdlib/pathname.rb
@@ -9,6 +9,20 @@ class Pathname
   def == other
     other.path == @path
   end
+
+  def absolute?
+    @path.start_with? '/'
+  end
+
+  def relative?
+    !absolute?
+  end
+
+  def to_path
+    @path
+  end
+  alias :to_str :to_path
+  alias :to_s :to_path
 end
 
 module Kernel
