e7d9f16343f355d5dccef666edcb1725e3bd3c09
Adam Beynon <adam.beynon@gmail.com>
2013-10-17 21:00:50 +0100

    Fix bug where colon2 was being processed as an array, not sexp

--- lib/opal/parser.rb
+++ lib/opal/parser.rb
@@ -635,7 +635,7 @@ module Opal
         f("($opal.cvars[#{part[1].to_s.inspect}] != null ? 'class variable' : nil)", sexp)
       when :colon2
         # TODO: avoid try/catch, probably a #process_colon2 alternative that does not raise errors is needed
-        [f('(function(){try { return ((', sexp), process_colon2(part[1..-1], level), f(") != null ? 'constant' : nil); } "+
+        [f('(function(){try { return ((', sexp), process(part, level), f(") != null ? 'constant' : nil); } "+
                        "catch(err) { if(err._klass === Opal.NameError) { return nil; } else { throw(err); } }; })()" , sexp)]
       when :colon3
         f("($opal.Object._scope.#{sexp[0][1]} == null ? nil : 'constant')", sexp)
