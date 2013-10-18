2d28028717a7fedd3bbe09e9c47dd8768774fc31
Adam Beynon <adam.beynon@gmail.com>
2013-10-18 15:05:12 +0100

    Opal now supports mspec mocks/stubs. Enable some specs.

--- spec/filters/mspec/should_receive.rb
+++ spec/filters/mspec/should_receive.rb
@@ -1,5 +1,3 @@
 opal_filter "should_receive" do
   fails "Array#at tries to convert the passed argument to an Integer using #to_int"
-  fails "Array#include? calls == on elements from left to right until success"
-  fails "A block taking |a| arguments does not call #to_ary to convert a single yielded object to an Array"
 end
