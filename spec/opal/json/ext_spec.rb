443adc1a0a990fadf93b336445e458607661e7ab
meh <meh@schizofreni.co>
2013-10-21 15:43:08 +0200

    Fix a JSON spec

--- spec/opal/json/ext_spec.rb
+++ spec/opal/json/ext_spec.rb
@@ -3,7 +3,7 @@ require 'json'
 describe "Hash#to_json" do
   it "returns a string of all key and value pairs" do
     {}.to_json.should == "{}"
-    {"a" => 1, "b" => 2}.to_json.should == '{"a": 1, "b": 2}'
+    {"a" => 1, "b" => 2}.to_json.should == '{"a":1, "b":2}'
 
     hash = {"a" => 1, "b" => false, "c" => nil, "d" => true}
     JSON.parse(hash.to_json).should == hash
