23f7ccb3f0819dd9c4b45499a2b341eca211dda5
meh <meh@schizofreni.co>
2013-10-22 18:58:35 +0200

    Make Comparable compliant

--- corelib/comparable.rb
+++ corelib/comparable.rb
@@ -1,25 +1,56 @@
 module Comparable
-  def <(other)
-    (self <=> other) == -1
-  end
+  def self.normalize(what)
+    return what if Integer === what
 
-  def <=(other)
-    (self <=> other) <= 0
+    return  1 if what > 0
+    return -1 if what < 0
+    return  0
   end
 
   def ==(other)
-    (self <=> other) == 0
+    return true if equal?(other)
+
+    return false unless cmp = (self <=> other)
+    return Comparable.normalize(cmp) == 0
+  rescue StandardError
+    false
   end
 
   def >(other)
-    (self <=> other) == 1
+    unless cmp = (self <=> other)
+      raise ArgumentError, "comparison of #{self.class} with #{other.class} failed"
+    end
+
+    Comparable.normalize(cmp) > 0
   end
 
   def >=(other)
-    (self <=> other) >= 0
+    unless cmp = (self <=> other)
+      raise ArgumentError, "comparison of #{self.class} with #{other.class} failed"
+    end
+
+    Comparable.normalize(cmp) >= 0
+  end
+
+  def <(other)
+    unless cmp = (self <=> other)
+      raise ArgumentError, "comparison of #{self.class} with #{other.class} failed"
+    end
+
+    Comparable.normalize(cmp) < 0
+  end
+
+  def <=(other)
+    unless cmp = (self <=> other)
+      raise ArgumentError, "comparison of #{self.class} with #{other.class} failed"
+    end
+
+    Comparable.normalize(cmp) <= 0
   end
 
   def between?(min, max)
-    self > min && self < max
+    return false if self < min
+    return false if self > max
+    return true
   end
 end
