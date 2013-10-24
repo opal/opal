bf1a9295745ec982fe9466b9cb5f90f8f0ab87ed
meh <meh@schizofreni.co>
2013-10-24 23:05:49 +0200

    Cleanup and consistency fixes for Array#flatten

--- corelib/array.rb
+++ corelib/array.rb
@@ -593,14 +593,12 @@ class Array
     %x{
       var result = [];
 
-      for (var i = 0, length = #{self}.length, item; i < length; i++) {
-        item = #{self}[i];
+      for (var i = 0, length = self.length; i < length; i++) {
+        var item = self[i];
 
-        if (!item._isArray && #{`item`.respond_to?(:to_ary)}) {
-          item = item.$to_ary();
-        }
+        if (#{`item`.respond_to?(:to_ary)}) {
+          item = #{`item`.to_ary};
 
-        if (item._isArray) {
           if (level == null) {
             result = result.concat(#{`item`.flatten});
           }
