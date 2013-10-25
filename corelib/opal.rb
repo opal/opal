cbffd7c5062ec1a2a8d17cf4d672cc3dcf24dff3
meh <meh@schizofreni.co>
2013-10-25 21:58:45 +0200

    Add Opal::Type.coerce_to helper

--- corelib/opal.rb
+++ corelib/opal.rb
@@ -43,3 +43,17 @@ RUBY_ENGINE   = 'opal'
 RUBY_VERSION  = '1.9.3'
 RUBY_ENGINE_VERSION = '0.4.4'
 RUBY_RELEASE_DATE = '2013-08-13'
+
+module Opal
+  module Type
+    def self.coerce_to(object, type, method)
+      return object if type === object
+
+      unless object.respond_to? method
+        raise TypeError, "no implicit conversion of #{object.class} into #{type}"
+      end
+
+      object.__send__ method
+    end
+  end
+end
