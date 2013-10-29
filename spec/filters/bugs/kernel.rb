7cca2af6e213e17a92e200ab00e5072ff3533146
Adam Beynon <adam.beynon@gmail.com>
2013-10-29 18:51:03 +0000

    Re-enable ivars in classes spec

--- spec/filters/bugs/kernel.rb
+++ spec/filters/bugs/kernel.rb
@@ -1,6 +1,4 @@
 opal_filter "Kernel" do
-  fails "A class definition allows the definition of class-level instance variables in a class method"
-
   fails "Kernel.rand returns a float if no argument is passed"
   fails "Kernel.rand returns an integer for an integer argument"
 
