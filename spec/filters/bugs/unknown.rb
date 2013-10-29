443583a07b0127da1019120ff8a413f599b6cc5b
Adam Beynon <adam.beynon@gmail.com>
2013-10-29 18:48:24 +0000

    Remove irrelevant filters

--- spec/filters/bugs/unknown.rb
+++ spec/filters/bugs/unknown.rb
@@ -1,12 +1,8 @@
 opal_filter "Kernel#Integer() fix broke mspec" do
   fails "Array#shuffle calls #to_f on the Object returned by #rand"
-  fails "An exception occurred during: Mock.verify_count"
   fails "Array#shuffle raises a RangeError if the random generator returns a value less than 0.0"
-  fails "An exception occurred during: Mock.verify_count"
   fails "Array#shuffle raises a RangeError if the random generator returns a value equal to 1.0"
-  fails "An exception occurred during: Mock.verify_count"
   fails "Array#shuffle raises a RangeError if the random generator returns a value greater than 1.0"
-  fails "An exception occurred during: Mock.verify_count"
   fails "A singleton class has class Bignum as the superclass of a Bignum instance"
   fails "Hash.[] ignores elements that are arrays of more than 2 elements"
   fails "Hash.[] creates a Hash; values can be provided as a list of value-invalid-pairs in an array"
