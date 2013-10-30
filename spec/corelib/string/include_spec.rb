3b8b3af1060bdc04c3236be322eb47fd4758b89d
Adam Beynon <adam.beynon@gmail.com>
2013-10-30 12:13:07 +0000

    Run String#index specs from rubyspec

--- spec/corelib/string/include_spec.rb
+++
@@ -1,6 +0,0 @@
-describe "String#include?" do
-  it "returns true if self contains other_str" do
-    "hello".include?("lo").should == true
-    "hello".include?("ol").should == false
-  end
-end
--- spec/filters/bugs/string.rb
+++ spec/filters/bugs/string.rb
@@ -7,4 +7,12 @@ opal_filter "String" do
   fails "String#upcase is locale insensitive (only replaces a-z)"
   fails "String#size returns the length of self"
   fails "String#length returns the length of self"
+
+  fails "String#index with Regexp converts start_offset to an integer via to_int"
+  fails "String#index with Regexp supports \\G which matches at the given start offset"
+  fails "String#index with Regexp starts the search at the given offset"
+  fails "String#index with Regexp returns the index of the first match of regexp"
+  fails "String#index calls #to_int to convert the second argument"
+  fails "String#index calls #to_str to convert the first argument"
+  fails "String#index raises a TypeError if passed a Symbol"
 end
