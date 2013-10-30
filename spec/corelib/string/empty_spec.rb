8097548d56d7778b8110e797e6b5fa72c979282d
Adam Beynon <adam.beynon@gmail.com>
2013-10-30 12:01:19 +0000

    Add #include? and #empty? sring specs

--- spec/corelib/string/empty_spec.rb
+++
@@ -1,7 +0,0 @@
-describe "String#empty?" do
-  it "returns true if the string has a length of zero" do
-    "hello".empty?.should == false
-    " ".empty?.should == false
-    "".empty?.should == true
-  end
-end
