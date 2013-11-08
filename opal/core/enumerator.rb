82fd939363648b68745e9939117c78c996ff091c
meh <meh@schizofreni.co>
2013-11-08 17:06:05 +0100

    Cleanup Enumerator

--- opal/core/enumerator.rb
+++ opal/core/enumerator.rb
@@ -1,42 +1,6 @@
 class Enumerator
   include Enumerable
 
-  class Yielder
-    def initialize(enumerator, block, to)
-      @enumerator = enumerator
-      @block      = block
-      @to         = to
-    end
-
-    def yield(*values)
-      %x{
-        if ($opal.$yieldX(#@to, values) === $breaker) {
-          throw $breaker;
-        }
-      }
-
-      self
-    end
-
-    alias << yield
-
-    def call
-      %x{
-        try {
-          #@block(self)
-        }
-        catch (e) {
-          if (e === $breaker) {
-            return $breaker.$v;
-          }
-          else {
-            throw e;
-          }
-        }
-      }
-    end
-  end
-
   def initialize(obj = nil, method = :each, *args, &block)
     if block
       @block = block
@@ -51,7 +15,7 @@ class Enumerator
     return enum_for :each unless block_given?
 
     if @block
-      Yielder.new(self, @block, block).call
+      Yielder.new(self, @block).each(&block)
     else
       @object.__send__(@method, *@args, &block)
     end
@@ -69,5 +33,42 @@ class Enumerator
     @cache = nil
 
     self
+  end
+
+  class Yielder
+    def initialize(enumerator, block)
+      @enumerator = enumerator
+      @block      = block
+    end
+
+    def yield(*values)
+      %x{
+        if ($opal.$yieldX(#@to, values) === $breaker) {
+          throw $breaker;
+        }
+      }
+
+      self
+    end
+
+    alias << yield
+
+    def each(&block)
+      @to = block
+
+      %x{
+        try {
+          #@block(self)
+        }
+        catch (e) {
+          if (e === $breaker) {
+            return $breaker.$v;
+          }
+          else {
+            throw e;
+          }
+        }
+      }
+    end
   end
 end
