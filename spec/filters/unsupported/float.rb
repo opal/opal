3980f919db0fc014cab2a498c812242c93ab38ab
meh <meh@schizofreni.co>
2013-11-11 18:43:48 +0100

    Mark an Array#eql? spec as failing because of floats

--- spec/filters/unsupported/float.rb
+++ spec/filters/unsupported/float.rb
@@ -1,4 +1,5 @@
 opal_filter "Float" do
   fails "Array#inspect represents a recursive element with '[...]'"
   fails "Array#to_s represents a recursive element with '[...]'"
+  fails "Array#eql? returns false if any corresponding elements are not #eql?"
 end
