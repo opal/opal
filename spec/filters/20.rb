b6de33e604c8e53c732376b8aa3ceccba52498e7
Adam Beynon <adam.beynon@gmail.com>
2013-11-12 11:01:12 +0000

    Opal now targets ruby 2.0.0

--- spec/filters/20.rb
+++ spec/filters/20.rb
@@ -1,4 +1,5 @@
 opal_filter "2.0 behaviour" do
   fails "Enumerator.new ignores block if arg given"
   fails "String#end_with? ignores arguments not convertible to string"
+  fails "The predefined global constants includes RUBY_VERSION"
 end
