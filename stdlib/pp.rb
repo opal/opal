db3fbfe83170a2a9d06a5bbdebff79ece68ca210
Elia Schito <elia@schito.me>
2013-11-15 10:36:34 +0100

    Add Kernel#pp
    The implementation tries to use console.log directly to pretty inspect 
    object inside browsers. This is in some ways non compliant with MRI, 
    but given the interactive ouptut of console log is much prettier seems 
    a sensible implementation.

--- stdlib/pp.rb
+++ stdlib/pp.rb
@@ -1,3 +1,12 @@
 module Kernel
   def pretty_inspect; inspect; end
+
+  if `(typeof console === "undefined" || typeof console.log === "undefined")`
+    alias :pp :p
+  else
+    def pp *args
+      args.each { |obj| `console.log(obj);` }
+      args.length <= 1 ? args[0] : args
+    end
+  end
 end
