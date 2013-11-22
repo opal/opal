6b95661127c7a2eae3784df35140bd7ab13e5ac6
Adam Beynon <adam.beynon@gmail.com>
2013-11-22 14:44:19 +0000

    Refactor s(:next) and s(:while) sexp construction

--- spec/opal/parser/while_spec.rb
+++ spec/opal/parser/while_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 describe "The while keyword" do
   it "returns an s(:while) with the given expr, body and true for head" do
-    opal_parse("while 1; 2; end").should == [:while, [:int, 1], [:int, 2], true]
+    opal_parse("while 1; 2; end").should == [:while, [:int, 1], [:int, 2]]
   end
 
   it "uses an s(:block) if body has more than one statement" do
-    opal_parse("while 1; 2; 3; end").should == [:while, [:int, 1], [:block, [:int, 2], [:int, 3]], true]
+    opal_parse("while 1; 2; 3; end").should == [:while, [:int, 1], [:block, [:int, 2], [:int, 3]]]
   end
 
   it "treats the prefix while statement just like a regular while statement" do
-    opal_parse("1 while 2").should == [:while, [:int, 2], [:int, 1], true]
+    opal_parse("1 while 2").should == [:while, [:int, 2], [:int, 1]]
   end
 end
