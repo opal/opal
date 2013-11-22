83329085fa46d7b4f4d980d463cd92c014afb8b6
Adam Beynon <adam.beynon@gmail.com>
2013-11-22 14:47:20 +0000

    Adjust specs for parsing module names

--- spec/opal/parser/module_spec.rb
+++ spec/opal/parser/module_spec.rb
@@ -2,19 +2,19 @@ require 'spec_helper'
 
 describe "The module keyword" do
   it "returns an empty s(:block) when given an empty body" do
-    opal_parse('module A; end').should == [:module, :A, [:block]]
+    opal_parse('module A; end').should == [:module, [:const, :A], [:block]]
   end
 
   it "does not place single expressions into a s(:block)" do
-    opal_parse('module A; 1; end').should == [:module, :A, [:int, 1]]
+    opal_parse('module A; 1; end').should == [:module, [:const, :A], [:int, 1]]
   end
 
   it "adds multiple body expressions into a s(:block)" do
-    opal_parse('module A; 1; 2; end').should == [:module, :A, [:block, [:int, 1], [:int, 2]]]
+    opal_parse('module A; 1; 2; end').should == [:module, [:const, :A], [:block, [:int, 1], [:int, 2]]]
   end
 
   it "should accept just a constant for the module name" do
-    opal_parse('module A; end')[1].should == :A
+    opal_parse('module A; end')[1].should == [:const, :A]
   end
 
   it "should accept a prefix constant for the module name" do
