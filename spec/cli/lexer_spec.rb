3fe1c695d671d9db2f510cfa424e3d5485aca146
Adam Beynon <adam.beynon@gmail.com>
2013-11-23 11:07:30 +0000

    Add basic specs for column numbers from lexer

--- spec/cli/lexer_spec.rb
+++ spec/cli/lexer_spec.rb
@@ -18,10 +18,30 @@ describe Opal::Lexer do
     expect_lines("=begin\nfoo\nbar\n=end\n42\n43").to eq([5, 6])
   end
 
-  def expect_lines(source)
-    parsed = Opal::Parser.new.parse(source)
-    nodes = parsed.type == :block ? parsed.children : [parsed]
+  it "sets correct column for each token" do
+    expect_columns("1").to eq([0])
+    expect_columns("1;2; 3").to eq([0, 2, 5])
+    expect_columns("    \t3").to eq([5])
+  end
 
-    expect(nodes.map { |sexp| sexp.line })
+  it "sets the column to 0 on each new line" do
+    expect_columns("1\n2\n\n\n3\n 4").to eq([0, 0, 0, 1])
+  end
+
+  it "sets column with regard to whitespace and other tokens before it" do
+    expect_columns("true;  false;  self\n  ni;").to eq([0, 7, 15, 2])
+  end
+
+  def expect_lines(source)
+    expect(parsed_nodes(source).map { |sexp| sexp.line })
+  end
+
+  def expect_columns(source)
+    expect(parsed_nodes(source).map { |sexp| sexp.column })
+  end
+
+  def parsed_nodes(source)
+    parsed = Opal::Parser.new.parse(source)
+    parsed.type == :block ? parsed.children : [parsed]
   end
 end
