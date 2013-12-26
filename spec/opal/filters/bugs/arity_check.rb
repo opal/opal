7780bf3b6838a6243bad3923b7dc62ad933da04b
meh <meh@schizofreni.co>
2013-12-26 14:07:08 +0100

    Move arity check filters to opal bugs

--- spec/opal/filters/bugs/arity_check.rb
+++
@@ -1,4 +0,0 @@
-opal_filter "Wrong arity check" do
-  fails "Array#shift passed a number n as an argument raises an ArgumentError if more arguments are passed"
-  fails "Array#pop passed a number n as an argument raises an ArgumentError if more arguments are passed"
-end
--- spec/opal/filters/bugs/opal.rb
+++ spec/opal/filters/bugs/opal.rb
@@ -1,3 +1,7 @@
 opal_filter "Opal bugs" do
   fails "Array#join raises a NoMethodError if an element does not respond to #to_str, #to_ary, or #to_s"
+
+  # arity checking bugs
+  fails "Array#shift passed a number n as an argument raises an ArgumentError if more arguments are passed"
+  fails "Array#pop passed a number n as an argument raises an ArgumentError if more arguments are passed"
 end
