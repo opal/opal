aaebf065ca71342658075e5c6bdd8ee9fb8ca10e
meh <meh@schizofreni.co>
2013-12-30 15:16:18 +0100

    The Math module is supposed to be includable

--- opal/corelib/math.rb
+++ opal/corelib/math.rb
@@ -2,84 +2,83 @@ module Math
   E  = `Math.E`
   PI = `Math.PI`
 
-  def self.acos(x)
     `Math.acos(x)`
   end
 
   # TODO: reimplement this when unavailable
-  def self.acosh(x)
+  def acosh(x)
     `Math.acosh(x)`
   end
 
-  def self.asin(x)
+  def asin(x)
     `Math.asin(x)`
   end
 
   # TODO: reimplement this when unavailable
-  def self.asinh(x)
+  def asinh(x)
     `Math.asinh(x)`
   end
 
-  def self.atan(x)
+  def atan(x)
     `Math.atan(x)`
   end
 
-  def self.atan2(x, y)
+  def atan2(x, y)
     `Math.atan2(x, y)`
   end
 
   # TODO: reimplement this when unavailable
-  def self.atanh(x)
+  def atanh(x)
     `Math.atanh(x)`
   end
 
   # TODO: reimplement this when unavailable
-  def self.cbrt(x)
+  def cbrt(x)
     `Math.cbrt(x)`
   end
 
-  def self.cos(x)
+  def cos(x)
     `Math.cos(x)`
   end
 
   # TODO: reimplement this when unavailable
-  def self.cosh(x)
+  def cosh(x)
     `Math.cosh(x)`
   end
 
-  def self.erf(x)
+  def erf(x)
     raise NotImplementedError
   end
 
-  def self.erfc(x)
+  def erfc(x)
     raise NotImplementedError
   end
 
-  def self.exp(x)
+  def exp(x)
     `Math.exp(x)`
   end
 
-  def self.frexp(x)
+  def frexp(x)
     raise NotImplementedError
   end
 
-  def self.gamma(x)
+  def gamma(x)
     raise NotImplementedError
   end
 
-  def self.hypot(x, y)
+  def hypot(x, y)
     `Math.hypot(x, y)`
   end
 
-  def self.ldexp(flt, int)
+  def ldexp(flt, int)
     raise NotImplementedError
   end
 
-  def self.lgamma(x)
+  def lgamma(x)
     raise NotImplementedError
   end
 
-  def self.log(num, base = undefined)
+  def log(num, base = undefined)
     if `base !== undefined`
       raise NotImplementedError
     end
@@ -88,34 +87,38 @@ module Math
   end
 
   # TODO: reimplement this when unavailable
-  def self.log10(num)
+  def log10(num)
     `Math.log10(num)`
   end
 
   # TODO: reimplement this when unavailable
-  def self.log2(num)
+  def log2(num)
     `Math.log2(num)`
   end
 
-  def self.sin(x)
+  def sin(x)
     `Math.sin(x)`
   end
 
   # TODO: reimplement this when unavailable
-  def self.sinh(x)
+  def sinh(x)
     `Math.sinh(x)`
   end
 
-  def self.sqrt(x)
+  def sqrt(x)
     `Math.sqrt(x)`
   end
 
-  def self.tan(x)
+  def tan(x)
     `Math.tan(x)`
   end
 
   # TODO: reimplement this when unavailable
-  def self.tanh(x)
+  def tanh(x)
     `Math.tanh(x)`
+  end
+
+  class << self
+    include Math
   end
 end
