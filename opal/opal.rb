f753d315a04fb3fc44a43a3ea5fd7d9857142339
Adam Beynon <adam.beynon@gmail.com>
2014-01-07 11:10:56 +0000

    Add core constants/gvars to corelib/variables.rb

--- opal/opal.rb
+++ opal/opal.rb
@@ -27,28 +27,4 @@ require 'corelib/time'
 require 'corelib/struct'
 require 'corelib/io'
 require 'corelib/main'
-
-# regexp matches
-$& = $~ = $` = $' = nil
-
-# stub library path
-$: = []
-$" = []
-
-# split lines
-$/ = "\n"
-$, = nil
-
-ARGV = []
-ARGF = Object.new
-ENV  = {}
-
-$VERBOSE = false
-$DEBUG   = false
-$SAFE    = 0
-
-RUBY_PLATFORM       = 'opal'
-RUBY_ENGINE         = 'opal'
-RUBY_VERSION        = '2.0.0'
-RUBY_ENGINE_VERSION = '0.6.0'
-RUBY_RELEASE_DATE   = '2013-11-20'
+require 'corelib/variables'
