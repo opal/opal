6ff4d55d751dd8adf892e2a382fef82dc7c2fd71
meh <meh@schizofreni.co>
2014-01-29 14:38:37 +0100

    Also catch Errno::EPIPE in uglify and gzip

--- lib/opal/util.rb
+++ lib/opal/util.rb
@@ -9,7 +9,7 @@ module Opal
         i.close_write
         return i.read
       end
-    rescue Errno::ENOENT
+    rescue Errno::ENOENT, Errno::EPIPE
       $stderr.puts '"uglifyjs" command not found (install with: "npm install -g uglify-js")'
       nil
     end
@@ -21,7 +21,7 @@ module Opal
         i.close_write
         return i.read
       end
-    rescue Errno::ENOENT
+    rescue Errno::ENOENT, Errno::EPIPE
       $stderr.puts '"gzip" command not found, it is required to produce the .gz version'
       nil
     end
