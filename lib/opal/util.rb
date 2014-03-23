f2324db6bf9b11ee82c0b19363d65e64efb596d1
Elia Schito <elia@schito.me>
2014-03-23 23:32:19 +0100

    Let the Util module to rely on a "Command" class

--- lib/opal/util.rb
+++ lib/opal/util.rb
@@ -4,54 +4,65 @@ module Opal
 
     # Used for uglifying source to minify
     def uglify(str)
-      return unless command_installed? :uglifyjs, ' (install with: "npm install -g uglify-js")'
-      IO.popen("uglifyjs 2> #{null}", 'r+') do |i|
-        i.puts str
-        i.close_write
-        i.read
-      end
+      uglifyjs = DigestSourceCommand.new(:uglifyjs, nil, ' (install with: "npm install -g uglify-js")')
+      uglifyjs.digest(str)
     end
 
     # Gzip code to check file size
     def gzip(str)
-      return unless command_installed? :gzip, ', it is required to produce the .gz version'
-      IO.popen("gzip -f 2> #{null}", 'r+') do |i|
-        i.puts str
-        i.close_write
-        i.read
-      end
+      gzip = DigestSourceCommand.new(:gzip, '-f', ', it is required to produce the .gz version')
+      gzip.digest(str)
     end
 
 
-    private
-
-    def null
-      if (/mswin|mingw/ =~ RUBY_PLATFORM).nil?
-        '/dev/null'
-      else
-        'nul'
+    class DigestSourceCommand
+      def initialize(command, options, message)
+        @command, @options, @message = command, options, message
       end
-    end
+      attr_reader :command, :options, :message
 
-    # Code from http://stackoverflow.com/questions/2108727/which-in-ruby-checking-if-program-exists-in-path-from-ruby
-    def which(cmd)
-      exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
-      ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
-        exts.each { |ext|
-          exe = File.join(path, "#{cmd}#{ext}")
-          return exe if File.executable? exe
-        }
+      def digest(source)
+        return unless command_installed? command, message
+        IO.popen("#{command} #{options} #{hide_stderr}", 'r+') do |i|
+          i.puts source
+          i.close_write
+          i.read
+        end
       end
-      nil
-    end
 
-    INSTALLED = {}
-    def command_installed?(cmd, install_comment)
-      INSTALLED.fetch(cmd) do
-        unless INSTALLED[cmd] = which(cmd) != nil
-          $stderr.puts %Q("#{cmd}" command not found#{install_comment})
+
+      private
+
+      def hide_stderr
+        if (/mswin|mingw/ =~ RUBY_PLATFORM).nil?
+          '2> /dev/null'
+        else
+          '2> nul'
+        end
+      end
+
+      # Code from http://stackoverflow.com/questions/2108727/which-in-ruby-checking-if-program-exists-in-path-from-ruby
+      def which(cmd)
+        exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+        ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+          exts.each { |ext|
+            exe = File.join(path, "#{cmd}#{ext}")
+            return exe if File.executable? exe
+          }
+        end
+        nil
+      end
+
+      INSTALLED = {}
+      def command_installed?(cmd, install_comment)
+        cmd = cmd.to_s
+        INSTALLED.fetch(cmd) do
+          unless INSTALLED[cmd] = which(cmd) != nil
+            $stderr.puts %Q("#{cmd}" command not found#{install_comment})
+          end
         end
       end
     end
+
   end
 end
