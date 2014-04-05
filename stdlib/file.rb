fcd1590b58d5f35f1e5d7b5d2743d41b043bc68a
Elia Schito <elia@schito.me>
2014-04-05 02:08:37 +0200

    Give File.expand_path a more credible implementation

--- stdlib/file.rb
+++ stdlib/file.rb
@@ -1,7 +1,7 @@
 class File
   SEPARATOR = '/'
 
-  def self.expand_path(path, *)
-    path
+  def self.expand_path(path, basedir = nil)
+    [basedir || '.', path].join(SEPARATOR).gsub(%r{/[^/]+/\.\./}, '/')
   end
 end
