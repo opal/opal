361ca0d27a4ccd8ae9213f54e6fc2f17c1cf6da5
Elia Schito <elia@schito.me>
2014-04-07 11:15:14 +0200

    Get File.expand_path into adulthood

--- stdlib/file.rb
+++ stdlib/file.rb
@@ -2,6 +2,9 @@ class File
   SEPARATOR = '/'
 
   def self.expand_path(path, basedir = nil)
-    [basedir || '.', path].join(SEPARATOR).gsub(%r{/[^/]+/\.\./}, '/')
+    expand_regexp = %r{(/|^)[^/]+/\.\./}
+    full_path = [basedir || '.', path].join(SEPARATOR)
+    full_path = full_path.gsub(expand_regexp, '\1') while full_path =~ expand_regexp
+    full_path
   end
 end
