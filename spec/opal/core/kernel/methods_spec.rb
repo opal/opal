797d5503812bd27f3d50903932d463b74cae2b4c
Mike Owens <mike@filespanker.com>
2014-04-08 10:19:35 -0400

    Kernel#methods should not return method_missing stubs

--- spec/opal/core/kernel/methods_spec.rb
+++ spec/opal/core/kernel/methods_spec.rb
@@ -1,11 +1,25 @@
+module MethodsSpecs
+  class Issue < Object
+    def unique_method_name
+    end
+  end
+
+  # Trigger stub generation
+  Issue.new.unique_method_name
+end
+
 describe "Kernel#methods" do
   it "lists methods available on an object" do
     Object.new.methods.include?("puts").should == true
   end
-  
+
   it "lists only singleton methods if false is passed" do
     o = Object.new
     def o.foo; 123; end
     o.methods(false).should == ["foo"]
+  end
+
+  it "ignores stub methods" do
+    Object.methods.include?(:unique_method_name).should be_false
   end
 end
