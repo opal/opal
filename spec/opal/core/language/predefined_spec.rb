25cf2dfb69c2ffbe54b80b1b25272d1cbca5db7e
Elia Schito <elia@schito.me>
2014-04-15 01:01:45 +0200

    fixup! Bump version and update the CHANGELOG

--- spec/opal/core/language/predefined_spec.rb
+++ spec/opal/core/language/predefined_spec.rb
@@ -71,7 +71,7 @@ describe "The predefined global constants" do
 
   it "includes RUBY_VERSION" do
     Object.const_defined?(:RUBY_VERSION).should == true
-    RUBY_VERSION.should == "2.0.0"
+    RUBY_VERSION.should == "2.1.1"
   end
 
   it "includes RUBY_RELEASE_DATE" do
