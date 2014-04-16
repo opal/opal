6f1631b21d1e8caf1dcfea6422dfc2caf1b765b6
Elia Schito <elia@schito.me>
2014-04-16 17:28:42 +0200

    Rely on the sprockets path reader for deps tracking

--- spec/cli/sprockets/processor_spec.rb
+++ spec/cli/sprockets/processor_spec.rb
@@ -35,9 +35,13 @@ describe Opal::Processor do
 end
 
 describe Opal::Processor::SprocketsPathReader do
-  subject(:path_reader) { described_class.new(env) }
+  subject(:path_reader) { described_class.new(env, context) }
 
   let(:env) { Opal::Environment.new }
+
+  # TODO: use stubs and expect calls on #depend_on and #depend_on_asset
+  let(:context) { env.context_class.new(env, 'foo', Pathname('bar/baz/foo.js')) }
+
   let(:logical_path) { 'sprockets_file' }
   let(:fixtures_dir) { File.expand_path('../../fixtures/', __FILE__) }
   let(:full_path) { File.join(fixtures_dir, logical_path+'.js.rb') }
