b057caf8d506ba241aaca3d97687b6bff9189959
Elia Schito <elia@schito.me>
2014-05-05 02:41:44 +0200

    Add StringScanner failing specs to bugs
    Emerged after fixing the encoding of the phantomjs runner page.

--- spec/filters/bugs/stringscanner.rb
+++ spec/filters/bugs/stringscanner.rb
@@ -2,7 +2,7 @@ opal_filter "StringScanner" do
   fails "StringScanner#[] calls to_int on the given index"
   fails "StringScanner#[] raises a TypeError if the given index is nil"
   fails "StringScanner#[] raises a TypeError when a Range is as argument"
-  fails "StringScanner#[] raises a TypeError when a String is as argument"  
+  fails "StringScanner#[] raises a TypeError when a String is as argument"
 
   fails "StringScanner#get_byte is not multi-byte character sensitive"
 
@@ -16,4 +16,7 @@ opal_filter "StringScanner" do
 
   fails "StringScanner#scan returns the matched string for a multi byte string"
   fails "StringScanner#scan raises a TypeError if pattern isn't a Regexp"
+
+  fails "StringScanner#pos= can poin position that greater than string length for multibyte string"
+  fails "StringScanner#pos= positions from the end if the argument is negative for multibyte string"
 end
