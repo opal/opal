13637828cc7944772bfe39d227931d0022638b15
Adam Beynon <adam.beynon@gmail.com>
2014-05-07 08:04:55 +0100

    All for-loop specs from rubyspec now pass

--- lib/opal/nodes/for.rb
+++ lib/opal/nodes/for.rb
@@ -5,8 +5,32 @@ module Opal
     class ForNode < Base
       handle :for
 
+      children :value, :args_sexp, :body_sexp
+
       def compile
-        raise "s(:for) nodes are not supported"
+        with_temp do |loop_var|
+          if args_sexp.type == :array
+            assign = s(:masgn, args_sexp)
+            assign << s(:to_ary, s(:js_tmp, loop_var))
+          else
+            assign = args_sexp << s(:js_tmp, loop_var)
+          end
+
+          body = if body_sexp
+                   if body_sexp.first == :block
+                     body_sexp.insert 1, assign
+                     body_sexp
+                   else
+                     s(:block, assign, body_sexp)
+                   end
+                 else
+                   assign
+                 end
+
+          iter = s(:iter, s(:lasgn, loop_var), body)
+          sexp = s(:call, value, :each, s(:arglist), iter)
+          push expr(sexp)
+        end
       end
     end
   end
