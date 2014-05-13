6d5a262c4c1e4edce708bc3e5277f7ccd5029214
Elia Schito <elia@schito.me>
2014-05-13 19:39:08 +0200

    Reset opal paths before each spec

--- spec/cli/spec_helper.rb
+++ spec/cli/spec_helper.rb
@@ -5,4 +5,5 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.order = 'random'
+  config.before { Opal.instance_variable_set('@paths', nil) }
 end
