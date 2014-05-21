a1181437a09ba5ca401b68add2fb9ca055a7b450
Elia Schito <elia@schito.me>
2014-05-21 18:52:40 +0200

    Fix Server cli runner

--- lib/opal/cli_runners/server.rb
+++ lib/opal/cli_runners/server.rb
@@ -1,18 +1,19 @@
 require 'opal/cli_runners'
 
 module Opal
-  class CliServerRunner
-    def initialize(output, port)
-      @output ||= output || $stdout
-    end
-    attr_reader :output
+  module CliRunners
+    class Server
+      def initialize(output, port)
+        @output ||= output || $stdout
+        @port = port
+      end
+      attr_reader :output, :port, :server
 
-    def run(source)
-      require 'rack'
-      require 'webrick'
-      require 'logger'
+      def run(source)
+        require 'rack'
+        require 'webrick'
+        require 'logger'
 
-      @thread = Thread.new do
         @server = Rack::Server.start(
           :app       => app(source),
           :Port      => port,
@@ -20,28 +21,28 @@ module Opal
           :Logger    => Logger.new(output)
         )
       end
-    end
 
-    def app(source)
-      lambda do |env|
-        case env['PATH_INFO']
-        when '/'
-          body = <<-HTML
-          <!doctype html>
-          <html>
-            <head>
-              <meta charset="utf-8"/>
-              <script>
-              //<![CDATA[
-              #{source}
-              //]]>
-              </script>
-            </head>
-          </html>
-          HTML
-          [200, {}, [body]]
-        else
-          [404, {}, [body]]
+      def app(source)
+        lambda do |env|
+          case env['PATH_INFO']
+          when '/'
+            body = <<-HTML
+            <!doctype html>
+            <html>
+              <head>
+                <meta charset="utf-8"/>
+                <script>
+                //<![CDATA[
+                #{source}
+                //]]>
+                </script>
+              </head>
+            </html>
+            HTML
+            [200, {}, [body]]
+          else
+            [404, {}, [body]]
+          end
         end
       end
     end
