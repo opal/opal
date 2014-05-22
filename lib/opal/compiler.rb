396379c838ed7e074b15a2bf9efd57fc8b08655b
Elia Schito <elia@schito.me>
2014-05-23 00:10:00 +0200

    Add a “WARNING” prefix to warnings

--- lib/opal/compiler.rb
+++ lib/opal/compiler.rb
@@ -103,7 +103,7 @@ module Opal
     # method simply appends the filename and curent line number onto
     # the message and issues a warning.
     def warning(msg, line = nil)
-      warn "#{msg} :#{file}:#{line}"
+      warn "WARNING: #{msg} :#{file}:#{line}"
     end
 
     # Instances of `Scope` can use this to determine the current
