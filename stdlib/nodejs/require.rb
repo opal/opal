3c916fdb3e3184bb8528ca383349c7b5310ececd
Elia Schito <elia@schito.me>
2014-06-18 19:04:40 +0200

    Prepare required sources in nodejs/require

--- stdlib/nodejs/require.rb
+++ stdlib/nodejs/require.rb
@@ -1,27 +1,32 @@
 require 'opal-parser'
 
 module Kernel
-  def require path
+  def __prepare_require__(path)
     name = `$opal.normalize_loadable_path(#{path})`
+    full_path = name.end_with?('.rb') ? name : name+'.rb'
 
     if `!$opal.modules[#{name}]`
-      ruby = File.read(path)
-      js = Opal.compile(ruby, requirable: true, file: name)
+      ruby = File.read(full_path)
+      compiler = Opal::Compiler.new(ruby, requirable: true, file: name)
+      js = compiler.compile
+      compiler.requires.each do |sub_path|
+        __prepare_require__(sub_path)
+      end
       `eval(#{js})`
     end
 
+    name
+  rescue => e
+    raise [path, name, full_path].inspect+e.message
+  end
+
+  def require path
+    name = __prepare_require__(path)
     `$opal.require(#{name})`
   end
 
   def load path
-    name = `$opal.normalize_loadable_path(#{path})`
-
-    if `!$opal.modules[#{name}]`
-      ruby = File.read(path)
-      js = Opal.compile(ruby, requirable: true, file: name)
-      `eval(js)`
-    end
-
+    name = __prepare_require__(path)
     `$opal.load(#{name})`
   end
 end
