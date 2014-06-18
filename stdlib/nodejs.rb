c2ab935081ad26e0b55b51bc5f846f8dda29079a
Elia Schito <elia@schito.me>
2014-06-18 19:43:20 +0200

    Let stdlib/nodejs be only a manifest

--- stdlib/nodejs.rb
+++ stdlib/nodejs.rb
@@ -1,44 +1,5 @@
-require 'nodejs/runtime.js'
+require 'nodejs/runtime'
 require 'nodejs/file'
 require 'nodejs/dir'
-
-class LoadError < ScriptError; end
-
-module IO::Writable
-  def puts(*args)
-    write args.map { |arg| String(arg) }.join($/)+$/
-  end
-end
-
-STDERR = $stderr = IO.new
-STDIN  = $stdin  = IO.new
-STDOUT = $stdout = IO.new
-
-$stdout.write_proc = -> (string) {`process.stdout.write(#{string})`}
-$stderr.write_proc = -> (string) {`process.stderr.write(string)`}
-
-$stdout.extend(IO::Writable)
-$stderr.extend(IO::Writable)
-
-module Kernel
-  def exit
-    `process.exit()`
-  end
-end
-
-module NodeJS
-  def self.require name
-    `OpalNode.node_require(#{name})`
-  end
-end
-
-ARGV = `process.argv.slice(2)`
-
-ENV = Object.new
-def ENV.[]= name, value
-  `process.env[#{name.to_s}] = #{value.to_s}`
-end
-
-def ENV.[] name
-  `process.env[#{name}] || nil`
-end
+require 'nodejs/io'
+require 'nodejs/process'
---
+++ stdlib/nodejs/process.rb
@@ -0,0 +1,16 @@
+module Kernel
+  def exit
+    `process.exit()`
+  end
+end
+
+ARGV = `process.argv.slice(2)`
+
+ENV = Object.new
+def ENV.[]= name, value
+  `process.env[#{name.to_s}] = #{value.to_s}`
+end
+
+def ENV.[] name
+  `process.env[#{name}] || nil`
+end
