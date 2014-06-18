55b39633c8f2ba536f254d7e80e7ed01d307f89b
Elia Schito <elia@schito.me>
2014-06-18 09:11:36 +0200

    Extract __fs__ and __path__ in nodejs/file

--- stdlib/nodejs/file.rb
+++ stdlib/nodejs/file.rb
@@ -1,56 +1,50 @@
 class File
+  `__fs__ = OpalNode.node_require('fs')`
+  `__path__ = OpalNode.node_require('path')`
 
-  def self.__fs__
-    @fs ||= `OpalNode.node_require('fs')`
-  end
 
   def self.read path
-    `#{__fs__}.readFileSync(#{path}).toString()`
+    `__fs__.readFileSync(#{path}).toString()`
   end
 
   def self.exist? path
-    `#{__fs__}.existsSync(#{path})`
+    `__fs__.existsSync(#{path})`
   end
 
   def self.realpath(pathname, dir_string = nil, cache = nil, &block)
     pathname = join(dir_string, pathname) if dir_string
     if block_given?
       `
-      #{__fs__}.realpath(#{pathname}, #{cache}, function(error, realpath){
+      __fs__.realpath(#{pathname}, #{cache}, function(error, realpath){
         if (error) #{raise error.message}
         else #{block.call(`realpath`)}
       })
       `
     else
-      `#{__fs__}.realpathSync(#{pathname}, #{cache})`
+      `__fs__.realpathSync(#{pathname}, #{cache})`
     end
   end
 
-  def self.__path__
-    @path ||= `OpalNode.node_require('path')`
-  end
-
   def self.basename(path, ext = undefined)
-    `#{__path__}.basename(#{path}, #{ext})`
+    `__path__.basename(#{path}, #{ext})`
   end
 
   def self.dirname(path)
-    `#{__path__}.dirname(#{path})`
+    `__path__.dirname(#{path})`
   end
 
   def self.join(*paths)
-    __path = __path__
-    `#{__path}.join.apply(#{__path}, #{paths})`
+    `__path__.join.apply(__path__, #{paths})`
   end
 
   def self.directory? path
     return nil unless exist? path
-    `!!#{__fs__}.lstatSync(path).isDirectory()`
+    `!!__fs__.lstatSync(path).isDirectory()`
   end
 
   def self.file? path
     return nil unless exist? path
-    `!!#{__fs__}.lstatSync(path).isFile()`
+    `!!__fs__.lstatSync(path).isFile()`
   end
 end
 
