dd85db00696ea30eebf444854f4c22afeb7d0833
Elia Schito <elia@schito.me>
2014-06-23 19:14:33 +0200

    Reuse IO objects in stdlib/nodejs

--- stdlib/nodejs/io.rb
+++ stdlib/nodejs/io.rb
@@ -1,16 +1,2 @@
-module IO::Writable
-  def puts(*args)
-    write args.map { |arg| String(arg) }.join($/)+$/
-  end
-end
-
-STDERR = $stderr = IO.new
-STDIN  = $stdin  = IO.new
-STDOUT = $stdout = IO.new
-
 $stdout.write_proc = -> (string) {`process.stdout.write(#{string})`}
 $stderr.write_proc = -> (string) {`process.stderr.write(string)`}
-
-$stdout.extend(IO::Writable)
-$stderr.extend(IO::Writable)
-
