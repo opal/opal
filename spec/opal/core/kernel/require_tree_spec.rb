0a722ac62db14d5273730701a31a144017ae515b
Elia Schito <elia@schito.me>
2014-10-11 17:39:20 +0200

    Force ordering in Kernel#require_tree spec

--- spec/opal/core/kernel/require_tree_spec.rb
+++ spec/opal/core/kernel/require_tree_spec.rb
@@ -2,6 +2,6 @@ describe 'Kernel.require_tree' do
   it 'loads all the files in a directory' do
     $ScratchPad = []
     require_tree '../fixtures/require_tree_files'
-    $ScratchPad.should == ['file 1', 'file 2', 'file 3', 'file 4', 'file 5']
+    $ScratchPad.sort.should == ['file 1', 'file 2', 'file 3', 'file 4', 'file 5']
   end
 end
