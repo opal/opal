d33488ef9966ccab546fc27f68a40614b6d37570
Elia Schito <elia@schito.me>
2014-10-21 23:24:23 +0200

    Extract mspec special calls to a file

--- lib/mspec/opal/rake_task.rb
+++ lib/mspec/opal/rake_task.rb
@@ -1,30 +1,6 @@
-require 'opal/nodes'
-class Opal::Nodes::CallNode
-  # Rubyspec uses this call to load in language specific features at runtime.
-  # We can't do this at runtime, so handle it during compilation
-  add_special :language_version do
-    if meth == :language_version and scope.top?
-      lang_type = arglist[2][1]
-      target = "corelib/language/versions/#{lang_type}_1.9"
-
-      if File.exist?(target)
-        compiler.requires << target
-      end
-
-      push fragment("nil")
-    end
-  end
-
-  add_special :not_supported_on do
-    unless meth == :not_supported_on and arglist[1][1] == :opal
-      compile_default!
-    end
-  end
-end
-
-
 require 'rack'
 require 'webrick'
+require 'mspec/opal/special_calls'
 
 module MSpec
   module Opal
