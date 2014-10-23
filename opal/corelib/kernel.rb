149863f0db36b44479507ea86df3882a9a2b61d1
Elia Schito <elia@schito.me>
2014-10-24 00:24:45 +0200

    Ensure the $$id is generated via __id__

--- opal/corelib/kernel.rb
+++ opal/corelib/kernel.rb
@@ -289,7 +289,7 @@ module Kernel
   end
 
   def hash
-    `[self.$$class.$$name,self.$$class.$$id,self.$$id].join(':')`
+    `[self.$$class.$$name,#{`self.$$class`.__id__},#{__id__}].join(':')`
   end
 
   def initialize_copy(other)
