f6928bba7418ef45692b40786948623c7b01b646
Elia Schito <elia@schito.me>
2014-10-27 23:53:12 +0100

    Add some Pathname methods

--- stdlib/pathname.rb
+++ stdlib/pathname.rb
@@ -1,5 +1,5 @@
 class Pathname
-  def initialize path
+  def initialize(path)
     raise ArgumentError if path == "\0"
     @path = path
   end
@@ -18,14 +18,38 @@ class Pathname
     !absolute?
   end
 
+  def root?
+    @path == '/'
+  end
+
+  def parent
+    new_path = @path.sub(%r{/([^/]+/?$)}, '')
+    new_path = absolute? ? '/' : '.' if new_path == ''
+    Pathname.new(new_path)
+  end
+
+  def sub(*args)
+    Pathname.new(@path.sub(*args))
+  end
+
+  def cleanpath
+    `return $opal.normalize_loadable_path(#@path)`
+  end
+
   def to_path
     @path
   end
+
+  def hash
+    @path
+  end
+
   alias :to_str :to_path
   alias :to_s :to_path
 end
 
 module Kernel
   def Pathname(path)
+    Pathname.new(path)
   end
 end
