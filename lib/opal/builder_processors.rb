56fa17acd62fbab6703a74035388af3830928fe0
Adam Beynon <adam.beynon@gmail.com>
2014-11-03 09:33:24 +0000

    Move Haml builder out to opal-haml gem

--- lib/opal/builder_processors.rb
+++ lib/opal/builder_processors.rb
@@ -134,26 +134,6 @@ module Opal
       end
     end
 
-    class HamlProcessor < RubyProcessor
-      handles :haml
-
-      def initialize(*args)
-        super
-        @source = prepare(@source, @filename)
-      end
-
-      def requires
-        ['opal-haml'] + super
-      end
-
-      def prepare(source, path)
-        haml = ::Haml::Engine.new(source, :ugly => true).precompiled
-        haml = haml.gsub('_hamlout.buffer', '_hamlout')
-
-        ::Opal::Haml.wrap haml, path
-      end
-    end
-
     class ERB_Processor < Processor
       handles :erb
 
