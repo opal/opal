8e3edd031916c4c8a180e63ab06ec0eb33cfb532
Elia Schito <elia@schito.me>
2014-11-03 23:53:35 +0100

    Add the failing Date#strftime spec to bugs

--- spec/filters/bugs/date.rb
+++ spec/filters/bugs/date.rb
@@ -11,4 +11,5 @@ opal_filter "Date" do
   fails "Date#strftime should be able to show the commercial week"
   fails "Date#strftime should be able to show the commercial week day"
   fails "Date#strftime should be able to show the week number with the week starting on sunday and monday"
+  fails "Date#strftime should be able to show the number of seconds since the unix epoch"
 end
