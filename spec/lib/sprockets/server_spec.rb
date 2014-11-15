9deb13834a00c6d878d45371a4e592496eae0bc3
Elia Schito <elia@schito.me>
2014-11-15 17:42:42 +0100

    Add a failing test case for subfolders sourcemaps
    for #648

--- spec/lib/sprockets/server_spec.rb
+++ spec/lib/sprockets/server_spec.rb
@@ -8,6 +8,7 @@ describe Opal::Server do
     described_class.new { |s|
       s.main = 'opal'
       s.debug = false
+      s.append_path "#{__dir__}/../fixtures"
       s.sprockets.logger = Logger.new('/dev/null')
     }
   end
@@ -17,4 +18,33 @@ describe Opal::Server do
     expect(last_response).to be_ok
   end
 
+  describe 'source maps' do
+    it 'serves map on a top level file' do
+      get '/assets/source_map.js'
+      expect(last_response).to be_ok
+
+      get '/assets/source_map.map'
+      expect(last_response).to be_ok
+    end
+
+    it 'serves map on a subfolder file' do
+      js_path = '/assets/source_map/subfolder/other_file.js'
+      map_path = '/assets/source_map/subfolder/other_file.map'
+
+      get js_path
+
+      expect(last_response).to be_ok
+      received_map_path = extract_linked_map(last_response.body)
+      expect(File.expand_path(received_map_path, js_path+'/..')).to eq(map_path)
+
+      get '/assets/source_map/subfolder/other_file.map'
+      expect(last_response).to be_ok
+    end
+  end
+
+  def extract_linked_map(body)
+    source_map_comment_regexp = %r{//# sourceMappingURL=(.*)$}
+    expect(body).to match(source_map_comment_regexp)
+    body.scan(source_map_comment_regexp).first.first
+  end
 end
