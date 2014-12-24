73d94cfe3d8c50a893f212161587f694effc2463
catmando <mitch@catprint.com>
2014-12-24 02:41:19 +0000

    now all promise resolutions can take multiple values

--- spec/opal/stdlib/promise/then_spec.rb
+++ spec/opal/stdlib/promise/then_spec.rb
@@ -22,12 +22,19 @@ describe 'Promise#then' do
     Promise.value(2).then { |v| v * 2 }.then { |v| v * 4 }.then { |v| x = v }
     x.should == 16
   end
+  
+  it 'works with multiple values' do
+    x = 42
+    Promise.values(1,2,3).then { |a, b, c| x = a+b+c }
+    x.should == 6
+  end
 
   it 'works when a block returns a promise' do
     a = Promise.new
     b = Promise.new
 
     x = 42
+      
     a.then { b }.then { |v| x = v }
 
     a.resolve(42)
@@ -35,7 +42,19 @@ describe 'Promise#then' do
 
     x.should == 23
   end
+  
+  it 'can use multiple values in a returned promise' do
+    a = Promise.new
 
+    x = 42
+        
+    a.then { |v0| Promise.values(1,2,3,v0) }.then { |v1, v2, v3, v4| x = v1+v2+v3+v4 }
+      
+    a.resolve(4)
+      
+    x.should == 10
+  end
+      
   it 'sends raised exceptions as rejections' do
     x = nil
 
