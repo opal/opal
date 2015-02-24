9912dec0643385d0f3834509b0ee168db7be6366
meh <meh@schizofreni.co>
2015-02-24 20:31:23 +0100

    Cleanup stdlib/pp

--- stdlib/pp.rb
+++ stdlib/pp.rb
@@ -1,11 +1,13 @@
 module Kernel
-  def pretty_inspect; inspect; end
+  def pretty_inspect
+    inspect
+  end
 
-  if `(typeof console === "undefined" || typeof console.log === "undefined")`
-    alias :pp :p
+  if `(typeof(console) === "undefined" || typeof(console.log) === "undefined")`
+    alias pp p
   else
-    def pp *args
-      args.each { |obj| `console.log(obj);` }
+    def pp(*args)
+      args.each { |obj| `console.log(obj)` }
       args.length <= 1 ? args[0] : args
     end
   end
