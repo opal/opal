5240cbd0007158cf6f3dde8b75fce384a8a9515c
Sidu Ponnappa <ckponnappa@gmail.com>
2015-03-18 01:16:17 +0530

    Fixing failing RubySpec: Singleton#_dump returns an empty string Singleton#_dump returns an empty string from a singleton subclass

--- spec/filters/bugs/singleton.rb
+++ spec/filters/bugs/singleton.rb
@@ -1,6 +1,4 @@
 opal_filter "Singleton" do
-  fails "Singleton#_dump returns an empty string"
-  fails "Singleton#_dump returns an empty string from a singleton subclass"
   fails "Singleton.instance returns an instance of the singleton's clone"
   fails "Singleton.instance returns the same instance for multiple class to instance on clones"
 end
