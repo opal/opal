2bcab6087d50cd6729acf7e5a1a46069c8eb294f
Vais Salikhov <vsalikhov@gmail.com>
2015-05-02 13:34:16 +0700

    String#index fully compliant with rubyspec

--- spec/filters/bugs/opal.rb
+++ spec/filters/bugs/opal.rb
@@ -5,10 +5,5 @@ opal_filter "Opal bugs" do
   fails "Array#shift passed a number n as an argument raises an ArgumentError if more arguments are passed"
   fails "Array#pop passed a number n as an argument raises an ArgumentError if more arguments are passed"
 
-  # lacking regexp conversion
-  fails "String#index with Regexp supports \\G which matches at the given start offset"
-  fails "String#index with Regexp starts the search at the given offset"
-  fails "String#index with Regexp returns the index of the first match of regexp"
-
   fails "Kernel#warn requires multiple arguments"
 end
