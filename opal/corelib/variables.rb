1eb478021c7c6fb03c6df0a52a86af8afe556b66
Elia Schito <elia@schito.me>
2015-05-08 13:21:23 +0200

    Another safety net around RUBY_ENGINE_VERSION updates

--- opal/corelib/variables.rb
+++ opal/corelib/variables.rb
@@ -20,5 +20,5 @@ $SAFE    = 0
 RUBY_PLATFORM       = 'opal'
 RUBY_ENGINE         = 'opal'
 RUBY_VERSION        = '2.1.1'
-RUBY_ENGINE_VERSION = '0.8.0.dev'
+RUBY_ENGINE_VERSION = '0.8.0.beta1'
 RUBY_RELEASE_DATE   = '2015-02-14'
