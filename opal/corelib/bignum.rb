53185f5f6bbe82a831c09bd13af57c5d24ba712d
Jannis Hübl <jannis.huebl@web.de>
2015-05-18 21:13:15 +0200

    add implementation of biginteger and basefunktions
    source: https://raw.githubusercontent.com/jtobey/javascript-bignum/master/biginteger.js

--- opal/corelib/bignum.rb
+++ opal/corelib/bignum.rb
@@ -1,21 +1,31 @@
-require 'biginteger'
+require 'corelib/biginteger.js'
+require 'corelib/comparable'
 
 class Bignum #< Integer
   include Comparable
 
-  def initialize(value)
-    @value = `BigInteger.parse("123", 10)`
-    puts value.inspect
+  attr_reader :value
+
+  private :value
+
+  def initialize(value, base = 10)
+    @value = `BigInteger.parse(#{value}, #{base})`
   end
   
   def +(other)
     raise TypeError, "#{other.class} can't be coerced into Numeric" unless other.kind_of?(Numeric) || other.kind_of?(Bignum)
-    other
+    other = other.value if other.kind_of?(Bignum)
+    Bignum.new `#{value}.add(#{other})`
   end
 
   def -(other)
     raise TypeError, "#{other.class} can't be coerced into Numeric" unless other.kind_of?(Numeric) || other.kind_of?(Bignum)
-    other
+    other = other.value if other.kind_of?(Bignum)
+    Bignum.new `#{value}.subtract(#{other})`
+  end
+
+  def -@
+    Bignum.new `#{value}.negate()`
   end
 
   def coerce(other)
@@ -23,21 +33,25 @@ class Bignum #< Integer
   end
 
   def eql?(other)
-    false
+    return false unless other.kind_of?(Numeric) || other.kind_of?(Bignum)
+    self == other
   end
 
   def ==(other)
-    false
+    raise TypeError, "#{other.class} can't be coerced into Numeric" unless other.kind_of?(Numeric) || other.kind_of?(Bignum)
+    other = other.value if other.kind_of?(Bignum)
+    `#{value}.compare(#{other})` == 0
   end
 
   def to_f
     1
   end
 
-#  %x{
-#  self.valueOf = function() {
-#  1234
-#  }
-#  }
+
+  def inspect
+    `#{value}.toString()`
+  end
+
+
   
 end
