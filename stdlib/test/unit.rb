35292a50cf7eb28ec2c7895772fcf530e8eb652a
Elia Schito <elia@schito.me>
2015-05-23 16:16:00 +0200

    Backport assertion in test-unit/minitest adapter

--- stdlib/test/unit.rb
+++ stdlib/test/unit.rb
@@ -5,6 +5,19 @@ module Test
   module Unit
     class TestCase < Minitest::Test
       alias assert_raise assert_raises
+
+      def assert_nothing_raised(*)
+        yield
+      end
+
+      def assert_raise_with_message(exception, err_message, msg = nil)
+        err = assert_raises(exception, msg) { yield }
+        if err_message.is_a?(Regexp)
+          assert_matches err_message, err.message
+        else
+          assert_equal err_message, err.message
+        end
+      end
     end
   end
 end
