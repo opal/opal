12167e4005f214d014e92eb2654a8d81a5ae4882
Jannis Hübl <jannis.huebl@web.de>
2015-05-24 14:10:07 +0200

    add bigdecimal js-lib

--- stdlib/bigdecimal.rb
+++ stdlib/bigdecimal.rb
@@ -1,4 +1,17 @@
+require 'bigdecimal/bignumber.min.js'
 
 class BigDecimal
+
+  def initialize(value)
+    @value = `new BigNumber(#{value})`
+  end
+
+
+  def ==(other)
+
+    puts other
+    puts other.class
+    `#{@value}.equals(#{other})`
+  end
 
 end
