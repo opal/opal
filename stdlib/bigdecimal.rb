4aa2cf04fd59471a328f9fceb4d92016e4be1515
Jannis Huebl <jannis.huebl@web.de>
2015-05-26 06:45:02 +0200

    add regex matching a bigdecimal string

--- stdlib/bigdecimal.rb
+++ stdlib/bigdecimal.rb
@@ -3,11 +3,27 @@ class BigDecimal
 
 
   def initialize(value)
+    value = value.gsub("_", "")
+    value = value.gsub("d", "E")
+    value = value.gsub("D", "E")
+    value = value.gsub("e", "E")
+    value = value.gsub("E", "E")
+    value = value.strip
+    
+    x = value.match(/^[\-|\+]?[0-9]*([\.][0-9]*)?([E][\-|\+]?[0-9]*)?/)
+
+    value = "0.0"
+    value = x[0] if x && x[0] && x[0] != ""
+
     @value = `new forge.BigNumber(#{value})`
   end
 
-  def ==(other)
+  def -@
+    value = `#{@value}.neg()`
+    BigDecimal.new `value.toString()`
+  end
 
+  def ==(other)
     `#{@value}.equals(#{other})`
   end
 
