cd27418fdcd97aba98f840f006287aa8b752385f
Jannis Huebl <jannis.huebl@web.de>
2015-06-01 17:01:21 +0200

    add bignum#div tests to bugs
    I'm not sure if it is really a bug.
    Bug or Feature? MRI ruby returns -1 if result is -0.999. JSBN returns 0 for -0.999

---
+++ spec/filters/bugs/bignum.rb
@@ -0,0 +1,5 @@
+opal_filter "Bignum" do
+  # Bug or Feature? MRI ruby returns -1 if result is -0.999. JSBN returns 0 for -0.999
+  fails "Bignum#div returns self divided by other"
+  fails "Bignum#div returns a result of integer division of self by a float argument"
+end
