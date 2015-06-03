acbce9f56e1a45dcd833f72bcc1a608bdf156df5
Jeremy Evans <code@jeremyevans.net>
2015-06-03 16:26:12 -0700

    Add JS module for nicer calling of native javascript operators and global functions
    This adds support for calling the following javascript operators:
    delete, in, instanceof, new, typeof, and void.  It also supports
    calling global javascript functions via call.  call is aliased
    to method_missing, so that you can do JS.globalFunction() to
    call global javascript functions.

---
+++ spec/opal/stdlib/js_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+require 'js'
+
+describe 'javascript operations using JS module' do
+  it 'JS.typeof uses typeof to return underlying javascript type' do
+    [1, `null`, `undefined`, Object.new, [], ""].each do |v|
+      JS.typeof(v).should == `typeof #{v}`
+    end
+  end
+
+  it 'JS.new uses new to create new instance' do
+    f = `function(){}`
+    f.JS[:prototype].JS[:color] = 'black'
+    JS.new(f).JS[:color].should == 'black'
+  end
+
+  it 'JS.instanceof uses instanceof to check if value is an instance of a function' do
+    f = `function(){}`
+    JS.instanceof(JS.new(f), f).should == true
+    JS.instanceof(JS.new(f), `function(){}`).should == false 
+  end
+
+  it 'JS.delete uses delete to remove properties from objects' do
+    f = `{a:1}`
+    f.JS[:a].should == 1
+    JS.delete(f, :a)
+    `#{f.JS[:a]} === undefined`.should == true
+  end
+
+  it 'JS.in uses in to check for properties in objects' do
+    f = `{a:1}`
+    JS.in(:a, f).should == true
+    JS.in(:b, f).should == false
+  end
+
+  it 'JS.void returns undefined' do
+    f = 1
+    `#{JS.void(f += 1)} === undefined`.should == true
+    f.should == 2
+  end
+
+  it 'JS.call calls global javascript methods' do
+    JS.call(:parseFloat, '1.0').should == 1
+    JS.call(:parseInt, '1').should == 1
+    JS.call(:eval, "({a:1})").JS[:a].should == 1
+  end
+
+  it 'JS.method_missing calls global javascript methods' do
+    JS.parseFloat('1.0').should == 1
+    JS.parseInt('1').should == 1
+  end
+end
