2849d056f9c927968ad4492f5d371d4a8a264a58
Vais Salikhov <vsalikhov@gmail.com>
2015-06-04 18:23:58 +0700

    JSHint: do not use reserved keywords for identifiers (specs)

--- spec/opal/core/runtime/eval_spec.rb
+++ spec/opal/core/runtime/eval_spec.rb
@@ -1,5 +1,5 @@
 describe "Opal.eval()" do
   it "evaluates ruby code by compiling it to javascript and running" do
-    `Opal.eval("'foo'.class")`.should == String
+    `Opal['eval']("'foo'.class")`.should == String
   end
 end
