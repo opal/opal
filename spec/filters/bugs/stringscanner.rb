ee156ba12baef4686ed28823e296be18494351a7
Vais Salikhov <vsalikhov@gmail.com>
2015-06-13 17:47:00 +0700

    Remove passing specs from bugs/stringscanner.rb

--- spec/filters/bugs/stringscanner.rb
+++ spec/filters/bugs/stringscanner.rb
@@ -2,23 +2,12 @@ opal_filter "StringScanner" do
   fails "StringScanner#[] calls to_int on the given index"
   fails "StringScanner#[] raises a TypeError if the given index is nil"
   fails "StringScanner#[] raises a TypeError when a Range is as argument"
-  fails "StringScanner#[] raises a TypeError when a String is as argument"
-
-  fails "StringScanner#get_byte is not multi-byte character sensitive"
 
   fails "StringScanner#pos returns the position of the scan pointer"
-  fails "StringScanner#pos returns the position of the scan pointer for multibyte string"
-  fails "StringScanner#pos returns 0 in the reset position"
-  fails "StringScanner#pos returns the length of the string in the terminate position"
-  fails "StringScanner#pos returns the `bytesize` for multibyte string in the terminate position"
   fails "StringScanner#pos= raises a RangeError if position too far backward"
   fails "StringScanner#pos= raises a RangeError when the passed argument is out of range"
 
-  fails "StringScanner#scan returns the matched string for a multi byte string"
   fails "StringScanner#scan raises a TypeError if pattern isn't a Regexp"
-
-  fails "StringScanner#pos= can poin position that greater than string length for multibyte string"
-  fails "StringScanner#pos= positions from the end if the argument is negative for multibyte string"
 
   fails "StringScanner#peek taints the returned String if the input was tainted"
   fails "StringScanner#peek returns an instance of String when passed a String subclass"
