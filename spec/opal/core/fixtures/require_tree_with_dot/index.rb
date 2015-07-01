1aa9b14baed4ccf06eded8707b40f8e8ce56d391
Elia Schito <elia@schito.me>
2015-07-02 00:35:10 +0200

    Add a spec example for Kernel#require_tree('.')
    ref opal/opal-rails#50
    fixes #832

---
+++ spec/opal/core/fixtures/require_tree_with_dot/index.rb
@@ -0,0 +1,3 @@
+$ScratchPad << File.basename(__FILE__)+'pre'
+require_tree '.'
+$ScratchPad << File.basename(__FILE__)+'post'
--- spec/opal/core/kernel/require_tree_spec.rb
+++ spec/opal/core/kernel/require_tree_spec.rb
@@ -6,4 +6,13 @@ describe 'Kernel.require_tree' do
     $ScratchPad.sort.should == ['file 1', 'file 2', 'file 3', 'file 4', 'file 5',
                                 'nested 1', 'nested 2', 'other 1']
   end
+
+  it 'can be used with "."' do
+    $ScratchPad = []
+    require_relative '../fixtures/require_tree_with_dot/index'
+
+    $ScratchPad[0].should == 'indexpre'
+    $ScratchPad[1...-1].sort.should == ['file 1', 'file 2', 'file 3']
+    $ScratchPad[-1].should == 'indexpost'
+  end
 end
