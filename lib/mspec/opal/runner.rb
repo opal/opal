43dea1d68668d082dfae7e1d5ecb2b4d3d6afa86
Elia Schito <elia@schito.me>
2015-07-10 18:19:10 +0200

    Remove duplicated line, fixes #991

--- lib/mspec/opal/runner.rb
+++ lib/mspec/opal/runner.rb
@@ -172,10 +172,6 @@ class NodeJSFormatter < BrowserFormatter
   end
 
   def finish_with_code(code)
-    `global.OPAL_SPEC_CODE = code;`
-  end
-
-  def finish_with_code(code)
     puts "\n\n"
     exit(code)
   end
