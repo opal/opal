04f44c0d72e6be8ea6b4648a5120b64cb78866e8
Vais Salikhov <vsalikhov@gmail.com>
2015-07-12 08:33:55 +0700

    Update to latest RubySpec

--- spec/filters/unsupported/fixnum.rb
+++ spec/filters/unsupported/fixnum.rb
@@ -1,10 +1,15 @@
 opal_filter "Fixnum" do
+  fails "Fixnum#+ overflows to Bignum when the result does not fit in Fixnum"
   fails "Fixnum#- returns a Bignum only if the result is too large to be a Fixnum"
   fails "Fixnum#<< with n << m returns 0 when m < 0 and m == p where 2**p > n >= 2**(p-1)"
   fails "Fixnum#<< with n << m returns 0 when m < 0 and m is a Bignum"
+  fails "Fixnum#<< with n << m returns a Bignum == fixnum_max * 2 when fixnum_max << 1 and n > 0"
   fails "Fixnum#<< with n << m returns a Bignum == fixnum_max() * 2 when fixnum_max() << 1 and n > 0"
+  fails "Fixnum#<< with n << m returns a Bignum == fixnum_min * 2 when fixnum_min << 1 and n < 0"
   fails "Fixnum#<< with n << m returns a Bignum == fixnum_min() * 2 when fixnum_min() << 1 and n < 0"
   fails "Fixnum#>> with n >> m returns 0 when m is a Bignum"
+  fails "Fixnum#>> with n >> m returns a Bignum == fixnum_max * 2 when fixnum_max >> -1 and n > 0"
   fails "Fixnum#>> with n >> m returns a Bignum == fixnum_max() * 2 when fixnum_max() >> -1 and n > 0"
+  fails "Fixnum#>> with n >> m returns a Bignum == fixnum_min * 2 when fixnum_min >> -1 and n < 0"
   fails "Fixnum#>> with n >> m returns a Bignum == fixnum_min() * 2 when fixnum_min() >> -1 and n < 0"
 end
