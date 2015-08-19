0a8be0d59339c0a3d99a81dbc4ed2f5bdc264940
meh <meh@schizofreni.co>
2015-08-19 20:40:47 +0200

    Remove wrong module Opal specs

--- spec/opal/core/module/name_spec.rb
+++ spec/opal/core/module/name_spec.rb
@@ -32,21 +32,4 @@ describe "Module#name" do
     ModuleNameSpec::A.name.should == "ModuleNameSpec::A"
     ModuleNameSpec::A::B.name.should == "ModuleNameSpec::A::B"
   end
-
-  it "uses just child name when class set inside anonymous parent" do
-    a = Class.new
-    b = Class.new
-    a.const_set :Child, b
-    b.name.should == "Child"
-  end
-
-  it "uses parent name once parent anonymous class gets name" do
-    a = Class.new
-    b = Class.new
-    a.const_set :Child, b
-    b.name.should == "Child"
-
-    ModuleNameSpec.const_set :Parent, a
-    b.name.should == "ModuleNameSpec::Parent::Child"
-  end
 end
