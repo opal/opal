c79117384258e13c501c723c931f0435f8c6ae3e
meh <meh@schizofreni.co>
2015-09-02 14:17:41 +0200

    Fix Module#to_s to not break when Class.name is redefined
    Fixes #816

--- opal/corelib/module.rb
+++ opal/corelib/module.rb
@@ -532,7 +532,7 @@ class Module
   end
 
   def to_s
-    name || "#<#{`self.$$is_mod ? 'Module' : 'Class'`}:0x#{__id__.to_s(16)}>"
+    `Opal.Module.$name.call(self)` || "#<#{`self.$$is_mod ? 'Module' : 'Class'`}:0x#{__id__.to_s(16)}>"
   end
 
   def undef_method(*names)
