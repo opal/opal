356b0c142f1ac3535a501877d4fc2519da484eb6
Elia Schito <elia@schito.me>
2015-09-30 23:52:36 +0200

    Add FileUtils.chmod to the Node.js adapter

--- stdlib/nodejs/fileutils.rb
+++ stdlib/nodejs/fileutils.rb
@@ -2,6 +2,13 @@ module FileUtils
   extend self
   `var __fs__ = #{File}.__fs__`
 
+  def chmod(mode, file_list)
+    raise NotImplementedError, 'symbolic mode is not supported, use numeric mode' if String === mode
+    Array(file_list).each do |file|
+      `__fs__.chmodSync(mode, file)`
+    end
+  end
+
   def cp source, target
     target = File.join(target, File.basename(source)) if File.directory? target
     `__fs__.writeFileSync(target, __fs__.readFileSync(source));`
