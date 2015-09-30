294c215893154b952372385e575aae63b478eb20
Elia Schito <elia@schito.me>
2015-09-30 23:50:59 +0200

    Reorder methods

--- stdlib/nodejs/fileutils.rb
+++ stdlib/nodejs/fileutils.rb
@@ -2,11 +2,6 @@ module FileUtils
   extend self
   `var __fs__ = #{File}.__fs__`
 
-  def mkdir_p path
-    return true if File.directory? path
-    `__fs__.mkdirSync(#{path})`
-  end
-
   def cp source, target
     target = File.join(target, File.basename(source)) if File.directory? target
     `__fs__.writeFileSync(target, __fs__.readFileSync(source));`
@@ -16,11 +11,16 @@ module FileUtils
     `__fs__.unlinkSync(path)`
   end
 
-  def mv source, target
-    target = File.join(target, File.basename(source)) if File.directory? target
-    `__fs__.renameSync(source, target)`
+  def mkdir_p path
+    return true if File.directory? path
+    `__fs__.mkdirSync(#{path})`
   end
 
   alias mkpath mkdir_p
   alias makedirs mkdir_p
+
+  def mv source, target
+    target = File.join(target, File.basename(source)) if File.directory? target
+    `__fs__.renameSync(source, target)`
+  end
 end
