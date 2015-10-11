57521cdf49ec03e737cf30c18203ad249f217550
Brady Wied <brady@bswtechconsulting.com>
2015-10-11 17:24:14 -0600

    White space

--- lib/opal/nodes/top.rb
+++ lib/opal/nodes/top.rb
@@ -84,7 +84,7 @@ module Opal
       def compile_method_stubs
         if compiler.method_missing?
           calls = compiler.method_calls
-          stubs = calls.to_a.map { |k| "'$#{k}'" }.join(', ')          
+          stubs = calls.to_a.map { |k| "'$#{k}'" }.join(', ')
           line "Opal.add_stubs([#{stubs}]);" unless stubs.empty?
         end
       end
