2e0769b4870e43a1aac00f5711587969b8bb8931
Vais Salikhov <vsalikhov@gmail.com>
2015-12-02 21:55:43 +0700

    Update to latest RubySpec

--- spec/filters/bugs/time.rb
+++ spec/filters/bugs/time.rb
@@ -24,4 +24,5 @@ opal_filter "Time" do
   fails "Time#to_f returns the float number of seconds + usecs since the epoch"
   fails "Time#to_s formats the fixed offset time following the pattern 'yyyy-MM-dd HH:mm:ss +/-HHMM'"
   fails "Time#zone Encoding.default_internal is set returns an ASCII string"
+  fails "Time.at passed Numeric roundtrips a Rational produced by #to_r"
 end
