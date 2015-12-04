5b7d33c6fdd6642371fb491b050e635c8c922dc1
Elia Schito <elia@schito.me>
2015-12-05 00:47:34 +0100

    fixup! e4a8015f0f (Add a missing local ref to the defined module)
    Was missing the variable declaration.

--- lib/opal/nodes/module.rb
+++ lib/opal/nodes/module.rb
@@ -12,7 +12,7 @@ module Opal
         helper :module
 
         push "(function($base) {"
-        line "  var self = $#{name} = $module($base, '#{name}');"
+        line "  var $#{name}, self = $#{name} = $module($base, '#{name}');"
 
         in_scope do
           scope.name = name
