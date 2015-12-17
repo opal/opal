36cdabf97327d5df5b0071278200326628407290
Ilya Bylich <ibylich@gmail.com>
2015-12-17 19:13:39 +0300

    Removed passing rubyspec from the filters.

--- spec/filters/bugs/struct.rb
+++ spec/filters/bugs/struct.rb
@@ -3,5 +3,4 @@ opal_filter "Struct" do
   fails "Struct#initialize can be overriden"
   fails "Struct#inspect returns a string representation of some kind"
   fails "Struct#members does not override the instance accessor method"
-  fails "Struct.new with a block passes same struct class to the block"
 end
