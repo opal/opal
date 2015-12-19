53871e96c1c445e5a605ddd8896ac411dd8a9d1c
Vais Salikhov <vsalikhov@gmail.com>
2015-12-19 14:50:30 +0700

    break: Fix Enumerator#reject

--- opal/corelib/enumerator.rb
+++ opal/corelib/enumerator.rb
@@ -329,7 +329,7 @@ class Enumerator
 
       Lazy.new(self, nil) {|enum, *args|
         %x{
-          Opal.yieldX(block, args);
+          var value = Opal.yieldX(block, args);
 
           if (#{Opal.falsy?(`value`)}) {
             #{enum.yield(*args)};
