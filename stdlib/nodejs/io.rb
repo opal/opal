a9c55d0fcfbcfca705c6e800b8c4c6a4bd7ab9ad
Guillaume Grossetie <ggrossetie@gmail.com>
2016-02-14 11:16:19 +0100

    Resolves #1332 #1221, Implement IO.read & IO.each_line method

--- stdlib/nodejs/io.rb
+++ stdlib/nodejs/io.rb
@@ -1,3 +1,58 @@
+class IO
+
+  @__fs__ = node_require :fs
+  `var __fs__ = #{@__fs__}`
+
+  attr_reader :eof
+  attr_reader :lineno
+
+  def initialize
+    @eof = false
+    @lineno = 0
+  end
+
+  def read
+    if @eof
+      ''
+    else
+      res = `__fs__.readFileSync(#{@path}).toString()`
+      @eof = true
+      @lineno = res.size
+      res
+    end
+  end
+
+  def each_line(separator = $/, &block)
+    if @eof
+      return block_given? ? self : [].to_enum
+    end
+
+    if block_given?
+      lines = File.read(@path)
+      %x{
+        self.eof = false;
+        self.lineno = 0;
+        var chomped  = #{lines.chomp},
+            trailing = lines.length != chomped.length,
+            splitted = chomped.split(separator);
+        for (var i = 0, length = splitted.length; i < length; i++) {
+          self.lineno += 1;
+          if (i < length - 1 || trailing) {
+            #{yield `splitted[i] + separator`};
+          }
+          else {
+            #{yield `splitted[i]`};
+          }
+        }
+        self.eof = true;
+      }
+      self
+    else
+      read.each_line
+    end
+  end
+end
+
 STDOUT.write_proc = -> (string) {`process.stdout.write(string)`}
 STDERR.write_proc = -> (string) {`process.stderr.write(string)`}
 
--- test/nodejs/test_file.rb
+++ test/nodejs/test_file.rb
@@ -12,4 +12,19 @@ class TestNodejsFile < Test::Unit::TestCase
 
     assert_equal(contents, File.read(path))
   end
+
+  def test_read_file
+    File.write('tmp/foo', 'bar')
+    assert_equal("bar", File.read('tmp/foo'))
+  end
+
+  def test_read_each_line
+    File.write('tmp/bar', "one\ntwo")
+    file = File.new('tmp/bar', 'r')
+    lines = []
+    file.each_line do |line|
+      lines << line
+    end
+    assert_equal(lines.length, 2)
+  end
 end
