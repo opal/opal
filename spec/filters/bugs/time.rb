43d6e28f66543cf8a3987a3720143c0e976ae330
Elia Schito <elia@schito.me>
2016-02-26 01:30:40 +0100

    Reorder filters

--- spec/filters/bugs/time.rb
+++ spec/filters/bugs/time.rb
@@ -24,6 +24,6 @@ opal_filter "Time" do
   fails "Time#to_f returns the float number of seconds + usecs since the epoch"
   fails "Time#to_s formats the fixed offset time following the pattern 'yyyy-MM-dd HH:mm:ss +/-HHMM'"
   fails "Time#zone Encoding.default_internal is set returns an ASCII string"
-  fails "Time.at passed Numeric roundtrips a Rational produced by #to_r"
   fails "Time.at passed Numeric passed BigDecimal doesn't round input value"
+  fails "Time.at passed Numeric roundtrips a Rational produced by #to_r"
 end
