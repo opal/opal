fd3032ca90bb1c90c290c8071d6d6411b634b9d8
pjskeptic <psparrow25@gmail.com>
2016-02-26 13:47:40 -0500

    Fix nodejs/yaml so YAML.load_path works.

--- stdlib/nodejs/yaml.rb
+++ stdlib/nodejs/yaml.rb
@@ -1,10 +1,9 @@
-require 'native'
-
 module YAML
-  `var __yaml__ = OpalNode.node_require('js-yaml')`
+  @__yaml__ = node_require 'js-yaml'
+  `var __yaml__ = #{@__yaml__}`
 
   def self.load_path path
-    loaded = `__yaml__.yaml.safeLoad(#{File.__fs__}.readFileSync(#{path}, 'utf8'))`
+    loaded = `__yaml__.safeLoad(#{File}.__fs__.readFileSync(#{path}, 'utf8'))`
     loaded = Hash.new(loaded) if native?(loaded)
     loaded
   end
