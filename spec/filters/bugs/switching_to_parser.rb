7192acd79f0c57cc512ad45ec5d73400a2c18795
Brady Wied <brady@bswtechconsulting.com>
2016-06-03 14:43:29 -0600

    Min/max ruby specs now pass, the string one is not related to super from what I can tell (still fails)

--- spec/filters/bugs/switching_to_parser.rb
+++ spec/filters/bugs/switching_to_parser.rb
@@ -11,14 +11,6 @@ opal_filter 'To fix in corelib/**:' do
   # hash
   fails "Hash#default_proc= uses :to_proc on its argument" # looks like it didn't work before, returning value must be a passed argument
 
-  # range (most probably a super issue)
-  fails "Range#min given a block passes each pair of values in the range to the block"
-  fails "Range#min given a block passes each pair of elements to the block where the first argument is the current element, and the last is the first element"
-  fails "Range#min given a block returns the element the block determines to be the minimum"
-  fails "Range#max given a block passes each pair of values in the range to the block"
-  fails "Range#max given a block passes each pair of elements to the block in reversed order"
-  fails "Range#max given a block returns the element the block determines to be the maximum"
-
   # string
   fails "String#rindex with Regexp returns nil if the substring isn't found"
 end
