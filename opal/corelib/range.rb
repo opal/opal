a87e53b3acb7af5daad5bca99077e41861ff8f39
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-06-21 11:03:06 +0100

    Improvements for Range class
    * private/tainted/untrusted are not supported
    * Conforming Range#to_s and Range#inspect
    * Starting Range#bsearch implementation
    * Simple Range#step implementation
    * Fixing Range#min for empty Ranges
    * Fixing Range#last(n) Range#first(n) and one edge case of Range#each
    * Fixing some Range#step issues on String ranges
    * Simple Range#bsearch implementation, passes about half the specs
    * Assorted Range fixes
    * Consistent raise syntax
    * Minor syntax fixes
    * Minor styling improvements. Fixed size of Range#step.
    * Compile complex ranges to "Range.new" so there will be a check for begin and end to be comparable.

--- opal/corelib/range.rb
+++ opal/corelib/range.rb
@@ -8,7 +8,8 @@ class Range
   attr_reader :begin, :end
 
   def initialize(first, last, exclude = false)
-    raise ArgumentError unless first <=> last
+    raise NameError, "'initialize' called twice" if @begin
+    raise ArgumentError, "bad value for range" unless first <=> last
 
     @begin   = first
     @end     = last
@@ -32,7 +33,14 @@ class Range
   end
 
   def cover?(value)
-    @begin <= value && (@exclude ? value < @end : value <= @end)
+    beg_cmp = (@begin <=> value)
+    return false unless beg_cmp && beg_cmp <= 0
+    end_cmp = (value <=> @end)
+    if @exclude
+      end_cmp && end_cmp < 0
+    else
+      end_cmp && end_cmp <= 0
+    end
   end
 
   def each(&block)
@@ -62,7 +70,11 @@ class Range
     current = @begin
     last    = @end
 
-    while current < last
+    unless current.respond_to?(:succ)
+      raise TypeError, "can't iterate from #{current.class}"
+    end
+
+    while (current <=> last) < 0
       yield current
 
       current = current.succ
@@ -85,16 +97,26 @@ class Range
     @exclude
   end
 
-  alias :first :begin
+  def first(n = undefined)
+    return @begin if `n == null`
+    super
+  end
 
   alias :include? :cover?
 
-  alias :last :end
+  def last(n = undefined)
+    return @end if `n == null`
+    to_a.last(n)
+  end
 
   # FIXME: currently hardcoded to assume range holds numerics
   def max
     if block_given?
       super
+    elsif @begin > @end
+      nil
+    elsif @exclude && @begin == @end
+      nil
     else
       `#@exclude ? #@end - 1 : #@end`
     end
@@ -105,12 +127,14 @@ class Range
   def min
     if block_given?
       super
+    elsif @begin > @end
+      nil
+    elsif @exclude && @begin == @end
+      nil
     else
       @begin
     end
   end
-
-  alias member? include?
 
   def size
     _begin = @begin
@@ -126,18 +150,117 @@ class Range
   end
 
   def step(n = 1)
-    raise NotImplementedError
+    %x{
+      function coerceStepSize() {
+        if (!n.$$is_number) {
+          n = #{Opal.coerce_to!(n, Integer, :to_int)}
+        }
+
+        if (n < 0) {
+          #{raise ArgumentError, "step can't be negative"}
+        } else if (n === 0) {
+          #{raise ArgumentError, "step can't be 0"}
+        }
+      }
+
+      function enumeratorSize() {
+        if (!#{@begin.respond_to?(:succ)}) {
+          return nil;
+        }
+
+        if (#{@begin}.$$is_string && #{@end}.$$is_string) {
+          return nil;
+        }
+
+        if (n % 1 === 0) {
+          return #{(size / n).ceil};
+        } else {
+          // n is a float
+          var begin = self.begin, end = self.end,
+              abs = Math.abs, floor = Math.floor,
+              err = (abs(begin) + abs(end) + abs(end - begin)) / abs(n) * #{Float::EPSILON},
+              size;
+
+          if (err > 0.5) {
+            err = 0.5;
+          }
+
+          if (self.exclude) {
+            size = floor((end - begin) / n - err);
+            if (size * n + begin < end) {
+              size++;
+            }
+          } else {
+            size = floor((end - begin) / n + err) + 1
+          }
+
+          return size;
+        }
+      }
+    }
+
+
+    unless block_given?
+      return enum_for(:step, n) {
+        %x{
+          coerceStepSize();
+          return enumeratorSize();
+        }
+      }
+    end
+
+    `coerceStepSize()`
+
+    if `self.begin.$$is_number && self.end.$$is_number`
+      i = 0
+      loop do
+        current = @begin + i * n
+        if @exclude
+          break if current >= @end
+        else
+          break if current > @end
+        end
+        yield(current)
+        i += 1
+      end
+    else
+      %x{
+        if (#{@begin}.$$is_string && #{@end}.$$is_string && n % 1 !== 0) {
+          #{raise TypeError, "no implicit conversion to float from string"}
+        }
+      }
+      each_with_index do |value, idx|
+        yield(value) if idx % n == 0
+      end
+    end
+    self
+  end
+
+  def bsearch(&block)
+    return enum_for(:bsearch) unless block_given?
+
+    unless `self.begin.$$is_number && self.end.$$is_number`
+      raise TypeError, "can't do binary search for #{@begin.class}"
+    end
+
+    to_a.bsearch(&block)
   end
 
   def to_s
-    `#{@begin.inspect} + (#@exclude ? '...' : '..') + #{@end.inspect}`
+    "#{@begin}#{ @exclude ? '...' : '..' }#{@end}"
   end
 
-  alias inspect to_s
+  def inspect
+    "#{@begin.inspect}#{ @exclude ? '...' : '..' }#{@end.inspect}"
+  end
 
   def marshal_load(args)
     @begin = args[:begin]
     @end = args[:end]
     @exclude = args[:excl]
+  end
+
+  def hash
+    [@begin, @end, @exclude].hash
   end
 end
