87caa64f077bc94966a97c6fed80e561b3d6e55d
Elia Schito <elia@schito.me>
2016-09-13 23:28:57 +0200

    Add opal/simple_server for simple non cached apps
    The quickest way to get up and running with Opal:
    
      rackup -ropal -ropal/simple_server -b 'Opal.append_path("app"); run Opal::SimpleServer.new'

---
+++ spec/lib/simple_server_spec.rb
@@ -0,0 +1,45 @@
+require 'lib/spec_helper'
+require 'rack/test'
+
+describe Opal::SimpleServer do
+  include Rack::Test::Methods
+
+  attr_accessor :app
+
+  before do
+    Opal.append_path "#{__dir__}/fixtures"
+    self.app = described_class.new(main: 'console')
+  end
+
+  it 'serves opal assets' do
+    response = get '/assets/console.js'
+    expect(response.body).to eq(Opal::Builder.build('console').to_s)
+  end
+
+  it 'serves index for all non opal paths' do
+    %w[/ /foo /foo/bar/baz].each do |path|
+      response = get path
+      expect(response.body).to include('<html>')
+      expect(response.body).to include('<script')
+      expect(response.body).to include('src="/assets/console.js')
+      expect(response.headers['Content-type']).to eq('text/html')
+    end
+  end
+
+  it 'serves the source map for the compiled asset' do
+    response = get '/assets/console.map'
+    expect(response.body).to eq(Opal::Builder.build('console').source_map.to_json)
+  end
+
+  it 'takes a :prefix option to set the assets prefix' do
+    self.app = described_class.new(main: 'opal', prefix: 'foo')
+    expect(get('/foo/console.js').body).to eq(Opal::Builder.build('console').to_s)
+    self.app = described_class.new(main: 'opal', prefix: '/foo')
+    expect(get('/foo/console.js').body).to eq(Opal::Builder.build('console').to_s)
+  end
+
+  it 'takes a :main option to set the main asset' do
+    self.app = described_class.new(main: 'foo')
+    expect(get('/').body).to include('src="/assets/foo.js')
+  end
+end
