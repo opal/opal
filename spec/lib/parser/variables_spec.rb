52a4fdac56ee6b00409649bb1d2514156c1a7e6c
Elia Schito <elia@schito.me>
2016-12-02 23:33:23 +0100

    Revert "No more encoding problems on JRuby"
    This reverts commit 9b5297ec5e9ae4344776e86ca05edac59ce3e3ca.

--- spec/lib/parser/variables_spec.rb
+++ spec/lib/parser/variables_spec.rb
@@ -99,6 +99,10 @@ describe Opal::Parser do
   end
 
   describe 'parsing unicode' do
+    if RUBY_ENGINE == 'jruby'
+      before { pending 'Until https://github.com/jruby/jruby/issues/3719 is fixed' }
+    end
+
     it "works for constants" do
       parsed("FOOλ = 1").should == [:cdecl, :FOOλ, [:int, 1]]
     end
