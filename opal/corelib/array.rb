057e576660e1799fad79969568ff26576c932a86
Elia Schito <elia@schito.me>
2017-02-24 02:00:27 +0100

    Avoid "has own" check by using prototype-less obj

--- opal/corelib/array.rb
+++ opal/corelib/array.rb
@@ -1143,19 +1143,18 @@ class Array < `Array`
 
       try {
         if (top) {
-          Opal.hash_ids = {};
+          Opal.hash_ids = Object.create(null);
         }
 
-        if (Opal.hash_ids.hasOwnProperty(hash_id)) {
+        // return early for recursive structures
+        if (Opal.hash_ids[hash_id]) {
           return 'self';
         }
 
         for (key in Opal.hash_ids) {
-          if (Opal.hash_ids.hasOwnProperty(key)) {
-            item = Opal.hash_ids[key];
-            if (#{eql?(`item`)}) {
-              return 'self';
-            }
+          item = Opal.hash_ids[key];
+          if (#{eql?(`item`)}) {
+            return 'self';
           }
         }
 
--- opal/corelib/hash.rb
+++ opal/corelib/hash.rb
@@ -475,19 +475,17 @@ class Hash
 
       try {
         if (top) {
-          Opal.hash_ids = {};
+          Opal.hash_ids = Object.create(null);
         }
 
-        if (Opal.hash_ids.hasOwnProperty(hash_id)) {
+        if (Opal[hash_id]) {
           return 'self';
         }
 
         for (key in Opal.hash_ids) {
-          if (Opal.hash_ids.hasOwnProperty(key)) {
-            item = Opal.hash_ids[key];
-            if (#{eql?(`item`)}) {
-              return 'self';
-            }
+          item = Opal.hash_ids[key];
+          if (#{eql?(`item`)}) {
+            return 'self';
           }
         }
 
