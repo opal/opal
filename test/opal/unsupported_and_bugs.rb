233aa984d1ea71d0c89179de3f34bfd9e4678926
Elia Schito <elia@schito.me>
2017-03-13 00:08:38 +0100

    Mark some CRuby tests as bug/unsupported

--- test/opal/unsupported_and_bugs.rb
+++ test/opal/unsupported_and_bugs.rb
@@ -27,4 +27,13 @@ end
 
 class TestBase64
   bug :test_strict_decode64
+  bug :test_urlsafe_decode64_unpadded
+end
+
+class TestCall
+  # They work fine if `"a".sub! "b"` is replaced by `[].slice 1`
+  unsupported :test_safe_call_block_command
+  unsupported :test_safe_call_block_call
+  unsupported :test_safe_call_block_call_brace
+  unsupported :test_safe_call_block_call_command
 end
