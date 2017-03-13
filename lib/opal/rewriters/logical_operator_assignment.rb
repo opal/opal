b3121251042838feb70a632f2536a94ac8976b1d
Ilya Bylich <ibylich@gmail.com>
2017-03-13 20:49:54 +0300

    Rewrite logical operator assignment (or_asgn/and_asgn) to condition before compilation.
    `lhs ||= rhs` becomes `lhs = lhs || rhs`
    `lhs &&= rhs` becomes `lhs = lhs && rhs`
    `lhs +=  rhs` becomes `lhs = lhs +  rhs`

---
+++ lib/opal/rewriters/logical_operator_assignment.rb
@@ -0,0 +1,154 @@
+require 'opal/rewriters/base'
+
+module Opal
+  module Rewriters
+    class LogicalOperatorAssignment < Base
+      def self.reset_tmp_counter!
+        @@counter = 0
+      end
+
+      def self.new_temp
+        @@counter ||= 0
+        @@counter += 1
+        :"$logical_op_recvr_tmp_#{@@counter}"
+      end
+
+      GET_SET = ->(get_type, set_type) {
+
+        ->(lhs, rhs, root_type) {
+          get_node = lhs.updated(get_type) # lhs
+          condition_node = s(root_type, get_node, rhs) # lhs || rhs
+
+          lhs.updated(set_type, [*lhs, condition_node]) # lhs = lhs || rhs
+        }
+
+      }
+
+      # Takes    `lhs ||= rhs`
+      # Produces `lhs = lhs || rhs`
+      LocalVariableHandler = GET_SET[:lvar, :lvasgn]
+
+      # Takes    `@lhs ||= rhs`
+      # Produces `@lhs = @lhs || rhs`
+      InstanceVariableHandler = GET_SET[:ivar, :ivasgn]
+
+      # Takes    `LHS ||= rhs`
+      # Produces `LHS = LHS || rhs`
+      ConstantHandler = GET_SET[:const, :casgn]
+
+      # Takes    `$lhs ||= rhs`
+      # Produces `$lhs = $lhs || rhs`
+      GlobalVariableHandler = GET_SET[:gvar, :gvasgn]
+
+      # Takes    `@@lhs ||= rhs`
+      # Produces `@@lhs = @@lhs || rhs`
+      ClassVariableHandler = GET_SET[:cvar, :cvasgn]
+
+      # Takes    `recvr.meth ||= rhs`
+      # Produces `recvr.meth || recvr.meth = rhs`
+      # (lhs is a recvr.meth)
+      class SendHandler < self
+        def self.call(lhs, rhs, root_type)
+          recvr, reader_method, *args = *lhs
+
+          # If recvr is a complex expression it must be cached.
+          # MRI calls recvr in `recvr.meth ||= rhs` only once.
+          if recvr && recvr.type == :send
+            recvr_tmp = new_temp
+            cache_recvr = s(:lvasgn, recvr_tmp, recvr)                         # $tmp = recvr
+            recvr = s(:js_tmp, recvr_tmp)
+          end
+
+          writer_method = :"#{reader_method}="
+
+          call_reader = lhs.updated(:send, [recvr, reader_method, *args])      # $tmp.meth
+          call_writer = lhs.updated(:send, [recvr, writer_method, *args, rhs]) # $tmp.meth = rhs
+          get_or_set = s(root_type, call_reader, call_writer)
+
+          if cache_recvr
+            s(:begin, cache_recvr, get_or_set)
+          else
+            get_or_set
+          end
+        end
+      end
+
+      # Takes    `recvr&.meth ||= rhs`
+      # Produces `recvr.nil? ? nil : recvr.meth ||= rhs`
+      #   NOTE: Later output of this handler gets post-processed by this rewriter again
+      #   using SendHandler to `recvr.nil? ? nil : (recvr.meth || recvr.meth = rhs)`
+      class ConditionalSendHandler < self
+        def self.call(lhs, rhs, root_type)
+          root_type = :"#{root_type}_asgn"
+
+          recvr, meth, *args = *lhs
+
+          recvr_tmp = new_temp
+          cache_recvr = s(:lvasgn, recvr_tmp, recvr)          # $tmp = recvr
+          recvr = s(:js_tmp, recvr_tmp)
+
+          recvr_is_nil = s(:send, recvr, :nil?)                 # recvr.nil?
+          nil_node = s(:nil)                                    # nil
+          plain_send = lhs.updated(:send, [recvr, meth, *args]) # recvr.meth
+          plain_or_asgn = s(root_type, plain_send, rhs)         # recvr.meth ||= rhs
+
+          s(:begin,
+            cache_recvr,
+            s(:if, recvr_is_nil,                          # if recvr.nil?
+              s(:nil),                                    #   nil
+                                                          # else
+              plain_or_asgn))                             #   recvr.meth ||= rhs
+                                                          # end
+        end
+      end
+
+      HANDLERS = {
+        lvasgn: LocalVariableHandler,
+        ivasgn: InstanceVariableHandler,
+        casgn:  ConstantHandler,
+        gvasgn: GlobalVariableHandler,
+        cvasgn: ClassVariableHandler,
+        send:   SendHandler,
+        csend:  ConditionalSendHandler
+      }
+
+      # lhs ||= rhs
+      def on_or_asgn(node)
+        lhs, rhs = *node
+
+        result = HANDLERS
+          .fetch(lhs.type) { raise NotImplementedError }
+          .call(lhs, rhs, :or)
+
+        process(result)
+      end
+
+      # lhs &&= rhs
+      def on_and_asgn(node)
+        lhs, rhs = *node
+
+        result = HANDLERS
+          .fetch(lhs.type) { raise NotImplementedError }
+          .call(lhs, rhs, :and)
+
+        process(result)
+      end
+
+      ASSIGNMENT_STRING_NODE = s(:str, 'assignment')
+
+      # Rewrites any or_asgn and and_asgn node like
+      #   `defined?(a ||= 1)`
+      # and
+      #   `defined?(a &&= 1)`
+      # to a static "assignment" string node
+      def on_defined?(node)
+        inner, _ = *node
+        if [:or_asgn, :and_asgn].include?(inner.type)
+          ASSIGNMENT_STRING_NODE
+        else
+          super(node)
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/rewriters/logical_operator_assignment_spec.rb
@@ -0,0 +1,202 @@
+require 'lib/spec_helper'
+
+describe Opal::Rewriters::LogicalOperatorAssignment do
+  def s(type, *children)
+    ::Opal::AST::Node.new(type, children)
+  end
+
+  let(:rewriter) { Opal::Rewriters::LogicalOperatorAssignment.new }
+
+  def parse(source)
+    parser = Opal::Parser.default_parser
+    buffer = ::Parser::Source::Buffer.new('(eval)')
+    buffer.source = source
+    parser.parse(buffer)
+  end
+
+  def rewrite(ast)
+    rewriter.process(ast)
+  end
+
+  around(:each) do |e|
+    Opal::Rewriters::LogicalOperatorAssignment.reset_tmp_counter!
+    Opal::Rewriter.disable { e.run }
+  end
+  let(:cache_tmp_name) { :$logical_op_recvr_tmp_1 }
+  let(:cached) { s(:js_tmp, cache_tmp_name) }
+
+  shared_examples 'it rewrites' do |from, to|
+    it "rewrites #{from.inspect} to #{to.inspect}" do
+      input = parse(from)
+      rewritten = rewrite(input)
+      expected = parse(to)
+
+      expect(rewritten).to eq(expected)
+    end
+  end
+
+  context 'rewriting or_asgn and and_asgn nodes' do
+    context 'local variable' do
+      include_examples 'it rewrites', 'a = local; a ||= 1', 'a = local; a = a || 1'
+      include_examples 'it rewrites', 'a = local; a &&= 1', 'a = local; a = a && 1'
+    end
+
+    context 'instance variable' do
+      include_examples 'it rewrites', '@a ||= 1', '@a = @a || 1'
+      include_examples 'it rewrites', '@a &&= 1', '@a = @a && 1'
+    end
+
+    context 'constant' do
+      include_examples 'it rewrites', 'CONST ||= 1', 'CONST = CONST || 1'
+      include_examples 'it rewrites', 'CONST &&= 1', 'CONST = CONST && 1'
+    end
+
+    context 'global variable' do
+      include_examples 'it rewrites', '$g ||= 1', '$g = $g || 1'
+      include_examples 'it rewrites', '$g &&= 1', '$g = $g && 1'
+    end
+
+    context 'class variable' do
+      include_examples 'it rewrites', '@@a ||= 1', '@@a = @@a || 1'
+      include_examples 'it rewrites', '@@a &&= 1', '@@a = @@a && 1'
+    end
+
+    context 'simple method call' do
+      include_examples 'it rewrites', 'recvr = 1; recvr.meth ||= rhs', 'recvr = 1; recvr.meth || recvr.meth = rhs'
+      include_examples 'it rewrites', 'recvr = 1; recvr.meth &&= rhs', 'recvr = 1; recvr.meth && recvr.meth = rhs'
+    end
+
+    context '[] / []= method call' do
+      include_examples 'it rewrites', 'recvr = 1; recvr[idx] ||= rhs', 'recvr = 1; recvr[idx] || recvr[idx] = rhs'
+      include_examples 'it rewrites', 'recvr = 1; recvr[idx] &&= rhs', 'recvr = 1; recvr[idx] && recvr[idx] = rhs'
+    end
+
+    context '[] / []= method call with multiple arguments' do
+      include_examples 'it rewrites',
+        'recvr = 1; recvr[idx1, idx2] ||= rhs',
+          'recvr = 1; recvr[idx1, idx2] || recvr[idx1, idx2] = rhs'
+
+      include_examples 'it rewrites',
+        'recvr = 1; recvr[idx1, idx2] &&= rhs',
+          'recvr = 1; recvr[idx1, idx2] && recvr[idx1, idx2] = rhs'
+    end
+
+    context 'chain of method calls' do
+      it 'rewrites ||= by caching receiver to a temporary local variable' do
+        input = parse('recvr.a.b ||= rhs')
+        rewritten = rewrite(input)
+
+        expected = s(:begin,
+          s(:lvasgn, cache_tmp_name, parse('recvr.a')), # cached = recvr.a
+          s(:or,
+            s(:send, cached, :b),                       # cached.b
+            s(:send, cached, :b=, parse('rhs'))))       # cached.b = rhs
+
+        expect(rewritten).to eq(expected)
+      end
+
+      it 'rewrites &&= by caching receiver to a temporary local variable' do
+        input = parse('recvr.a.b &&= rhs')
+        rewritten = rewrite(input)
+
+        expected = s(:begin,
+          s(:lvasgn, cache_tmp_name, parse('recvr.a')), # cached = recvr.a
+          s(:and,
+            s(:send, cached, :b),                       # cached.b
+            s(:send, cached, :b=, parse('rhs'))))       # cached.b = rhs
+
+        expect(rewritten).to eq(expected)
+      end
+    end
+
+    context 'method call using safe nafigator' do
+      it 'rewrites ||= by caching receiver and rewriting it to if and or_asgn' do
+        input = parse('recvr&.meth ||= rhs')
+        rewritten = rewrite(input)
+
+        expected = s(:begin,
+          s(:lvasgn, cache_tmp_name, parse('recvr')), # cached = recvr
+          s(:if, s(:send, cached, :nil?),             # if cached.nil?
+            s(:nil),                                  #   nil
+                                                      # else
+            s(:or,
+              s(:send, cached, :meth),                #   cached.meth ||
+              s(:send, cached, :meth=, parse('rhs'))) #     cached.meth = rhs
+            )                                         # end
+          )
+
+        expect(rewritten).to eq(expected)
+      end
+
+      it 'rewrites &&= by caching receiver and rewriting it to if and or_asgn' do
+        input = parse('recvr&.meth &&= rhs')
+        rewritten = rewrite(input)
+
+        expected = s(:begin,
+          s(:lvasgn, cache_tmp_name, parse('recvr')), # cached = recvr
+          s(:if, s(:send, cached, :nil?),             # if cached.nil?
+            s(:nil),                                  #   nil
+                                                      # else
+            s(:and,
+              s(:send, cached, :meth),                #   cached.meth ||
+              s(:send, cached, :meth=, parse('rhs'))) #     cached.meth = rhs
+            )                                         # end
+          )
+
+        expect(rewritten).to eq(expected)
+      end
+    end
+  end
+
+  context 'rewriting defined?(or_asgn) and defined?(and_asgn)' do
+    context 'local variable' do
+      include_examples 'it rewrites', 'a = nil; defined?(a ||= 1)', 'a = nil; "assignment"'
+      include_examples 'it rewrites', 'a = nil; defined?(a &&= 1)', 'a = nil; "assignment"'
+    end
+
+    context 'instance variable' do
+      include_examples 'it rewrites', 'defined?(@a ||= 1)', %q("assignment")
+      include_examples 'it rewrites', 'defined?(@a &&= 1)', %q("assignment")
+    end
+
+    context 'constant' do
+      include_examples 'it rewrites', 'defined?(CONST ||= 1)', %q("assignment")
+      include_examples 'it rewrites', 'defined?(CONST &&= 1)', %q("assignment")
+    end
+
+    context 'global variable' do
+      include_examples 'it rewrites', 'defined?($g ||= 1)', %q("assignment")
+      include_examples 'it rewrites', 'defined?($g &&= 1)', %q("assignment")
+    end
+
+    context 'class variable' do
+      include_examples 'it rewrites', 'defined?(@@a ||= 1)', %q("assignment")
+      include_examples 'it rewrites', 'defined?(@@a &&= 1)', %q("assignment")
+    end
+
+    context 'simple method call' do
+      include_examples 'it rewrites', 'defined?(recvr.meth ||= rhs)', %q("assignment")
+      include_examples 'it rewrites', 'defined?(recvr.meth &&= rhs)', %q("assignment")
+    end
+
+    context '[] / []= method call' do
+      include_examples 'it rewrites', 'defined?(recvr[idx] ||= rhs)', %q("assignment")
+      include_examples 'it rewrites', 'defined?(recvr[idx] &&= rhs)', %q("assignment")
+    end
+
+    context '[] / []= method call with multiple arguments' do
+      include_examples 'it rewrites', 'defined?(recvr[idx1, idx2] ||= rhs)', %q("assignment")
+      include_examples 'it rewrites', 'defined?(recvr[idx1, idx2] &&= rhs)', %q("assignment")
+    end
+
+    context 'chain of method calls' do
+      include_examples 'it rewrites', 'defined?(recvr.a.b.c ||= rhs)', %q("assignment")
+      include_examples 'it rewrites', 'defined?(recvr.a.b.c &&= rhs)', %q("assignment")
+    end
+
+    context 'method call using safe nafigator' do
+      include_examples 'it rewrites', 'defined?(recvr&.meth ||= rhs)', %q("assignment")
+      include_examples 'it rewrites', 'defined?(recvr&.meth &&= rhs)', %q("assignment")
+    end
+  end
+end
