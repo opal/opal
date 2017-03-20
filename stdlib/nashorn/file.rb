ec9e52c62558a89c061db9784f0c3cc818717728
Guillaume Grossetie <ggrossetie@gmail.com>
2017-03-20 09:57:57 +0100

    Implement file.file? and file.readable? for Nashorn

--- stdlib/nashorn/file.rb
+++ stdlib/nashorn/file.rb
@@ -10,4 +10,19 @@ class File
         return data.join("\n");
       )
   end
+
+  def self.file? path
+    %x(
+      var Files = Java.type('java.nio.file.Files');
+      return Files.exists(path) && Files.isRegularFile(path);
+    )
+  end
+
+  def self.readable? path
+    %x(
+      var Files = Java.type('java.nio.file.Files');
+      return Files.exists(path) && Files.isReadable(path);
+    )
+  end
+
 end
