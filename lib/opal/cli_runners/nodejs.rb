0ac5939319f3dac5a5d293ef678db28976306ec3
Elia Schito <elia@schito.me>
2017-04-20 22:49:41 +0200

    Allow node to accept external options

--- lib/opal/cli_runners/nodejs.rb
+++ lib/opal/cli_runners/nodejs.rb
@@ -1,5 +1,6 @@
 require 'opal/cli_runners'
 require 'opal/paths'
+require 'shellwords'
 
 module Opal
   module CliRunners
@@ -25,7 +26,8 @@ module Opal
         # tempfile = File.new('opal-nodejs-runner.js', 'w') # for debugging
         tempfile.write code
         tempfile.close
-        system_with_output({'NODE_PATH' => node_modules}, 'node', tempfile.path, *argv)
+        node_opt = ENV['NODE_OPT'].to_s.shellsplit
+        system_with_output({'NODE_PATH' => node_modules}, 'node', *node_opt, tempfile.path, *argv)
       rescue Errno::ENOENT
         raise MissingNodeJS, 'Please install Node.js to be able to run Opal scripts.'
       end
