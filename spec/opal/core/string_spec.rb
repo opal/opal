fc3a4c3843f4be7640ee2ac1a7d354b294ed0bd7
Elia Schito <elia@schito.me>
2017-11-14 21:38:44 +0000

    Keep an internal registry of encodings
    Previously it was relying just on constants.

--- spec/opal/core/string_spec.rb
+++ spec/opal/core/string_spec.rb
@@ -47,4 +47,17 @@ describe 'Encoding' do
     "è".force_encoding('BINARY').should == "\xC3\xA8"
     "è".force_encoding('binary').should == "\xC3\xA8"
   end
+
+  describe '.find' do
+    it 'finds the encoding regardless of the case' do
+      Encoding.find('ASCII').should == Encoding::ASCII
+      Encoding.find('ascii').should == Encoding::ASCII
+      Encoding.find('US-ASCII').should == Encoding::ASCII
+      Encoding.find('us-ascii').should == Encoding::ASCII
+      Encoding.find('ASCII-8BIT').should == Encoding::ASCII
+      Encoding.find('ascii-8bit').should == Encoding::ASCII
+      Encoding.find('BINARY').should == Encoding::ASCII
+      Encoding.find('binary').should == Encoding::ASCII
+    end
+  end
 end
