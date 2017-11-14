d00ec0fb5a83cd1a3c678b4664660cf25624dba0
Elia Schito <elia@schito.me>
2017-11-14 23:48:17 +0100

    Fix deprecation

--- stdlib/securerandom.rb
+++ stdlib/securerandom.rb
@@ -1,6 +1,6 @@
 module SecureRandom
   def self.uuid
-    "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".gsub /[xy]/ do |ch,|
+    "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".gsub(/[xy]/) do |ch,|
       %x{
         var r = Math.random() * 16 | 0,
             v = ch == "x" ? r : (r & 3 | 8);
