54cfba439584f6e56e3968178ba580e966de94a6
Ilya Bylich <ibylich@gmail.com>
2017-12-22 19:11:40 +0300

    Fix spec that fails due to recent changes in MSpec.

--- spec/opal/core/module/attr_accessor_spec.rb
+++ spec/opal/core/module/attr_accessor_spec.rb
@@ -10,9 +10,12 @@ module AttrAccessorSpec
   end
 end
 
+class OpalAttrAccessorSpec
+  attr_accessor *%w{foo bar baz}
+end
+
 describe "Module#attr_accessor" do
   it "can be passed a splat of arguments" do
-    eval "class OpalAttrAccessorSpec; attr_accessor *%w{foo bar baz}; end"
     OpalAttrAccessorSpec.new.foo.should be_nil
   end
 
