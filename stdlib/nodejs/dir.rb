9e770e9eea4a240885cdcd41b9d6aba8fa2528f9
Guillaume Grossetie <ggrossetie@gmail.com>
2017-12-27 16:43:16 +0100

    Resolves #1727, Implement Dir#glob for Node.js

--- stdlib/nodejs/dir.rb
+++ stdlib/nodejs/dir.rb
@@ -28,6 +28,15 @@ class Dir
       }
     end
 
+    def glob(pattern)
+      pattern = [pattern] unless pattern.respond_to? :each
+      pattern.flat_map do |subpattern|
+        subpattern = subpattern.to_path if subpattern.respond_to? :to_path
+        subpattern = Opal.coerce_to!(subpattern, String, :to_str)
+        `__glob__.sync(subpattern)`
+      end
+    end
+
     alias getwd pwd
   end
 end
--- test/nodejs/test_dir.rb
+++ test/nodejs/test_dir.rb
@@ -10,13 +10,30 @@ class TestNodejsDir < Test::Unit::TestCase
   end
 
   def test_dir_entries
-    path = tmpdir + "/testing_nodejs_dir_implementation_#{Time.now.to_i}"
+    path = tmpdir + "/testing_nodejs_dir_entries_implementation_#{Time.now.to_i}"
     Dir.mkdir(path)
     result = Dir.entries(path)
     assert_equal(result.length, 0)
     File.open(path + '/bar', "w") {}
     File.open(path + '/baz', "w") {}
     result = Dir.entries(path)
+    assert_equal(result.length, 2)
+  end
+  
+  def test_dir_glob
+    path = tmpdir + "/testing_nodejs_dir_glob_implementation_#{Time.now.to_i}"
+    Dir.mkdir(path)
+    result = Dir.glob(path + '/*')
+    assert_equal(result.length, 0)
+    File.open(path + '/bar', "w") {}
+    File.open(path + '/baz', "w") {}
+    result = Dir.glob(path + '/ba*')
+    assert_equal(result.length, 2)
+    result = Dir.glob(path + '/*z')
+    assert_equal(result.length, 1)
+    result = Dir.glob([path + '/baz', path + '/bar'])
+    assert_equal(result.length, 2)
+    result = Dir.glob(Pathname.new(path).join('*'))
     assert_equal(result.length, 2)
   end
 end
