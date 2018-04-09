ceda559a286a454cfb9a2c5b418ef21d6c8e0d28
Guillaume Grossetie <g.grossetie@gmail.com>
2018-04-09 15:20:32 +0200

    Move IO methods that rely on path to File (#1800)

--- stdlib/nodejs/io.rb
+++ stdlib/nodejs/io.rb
@@ -35,47 +35,6 @@ class IO
     File.read(path)
   end
 
-  def read
-    if @eof
-      ''
-    else
-      res = `executeIOAction(function(){return __fs__.readFileSync(#{@path}).toString()})`
-      @eof = true
-      @lineno = res.size
-      res
-    end
-  end
-
-  def each_line(separator = $/, &block)
-    if @eof
-      return block_given? ? self : [].to_enum
-    end
-
-    if block_given?
-      lines = File.read(@path)
-      %x{
-        self.eof = false;
-        self.lineno = 0;
-        var chomped  = #{lines.chomp},
-            trailing = lines.length != chomped.length,
-            splitted = chomped.split(separator);
-        for (var i = 0, length = splitted.length; i < length; i++) {
-          self.lineno += 1;
-          if (i < length - 1 || trailing) {
-            #{yield `splitted[i] + separator`};
-          }
-          else {
-            #{yield `splitted[i]`};
-          }
-        }
-        self.eof = true;
-      }
-      self
-    else
-      read.each_line
-    end
-  end
-
   def self.binread(path)
     `return executeIOAction(function(){return __fs__.readFileSync(#{path}).toString('binary')})`
   end
