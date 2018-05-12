a7124eeb08ec5e19706f30513caf1d6882c76cbd
Guillaume Grossetie <g.grossetie@gmail.com>
2018-05-12 14:12:53 +0200

    Implement binread (mimic ASCII-8BIT encoding) (#1824)
    * Implement binread (mimic ASCII-8BIT encoding)
    
    * Use toString method

--- stdlib/nodejs/io.rb
+++ stdlib/nodejs/io.rb
@@ -36,7 +36,7 @@ class IO
   end
 
   def self.binread(path)
-    `return executeIOAction(function(){return __fs__.readFileSync(#{path}).toString('binary')})`
+    `return executeIOAction(function(){return __fs__.readFileSync(#{path}).toString('utf-8')})`
   end
 end
 
