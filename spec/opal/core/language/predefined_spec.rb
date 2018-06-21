943fc1471e12147d19e3e944a49ef04ec492ed8e
Elia Schito <elia@schito.me>
2018-06-21 23:46:34 +0200

    Fix a spec failing on double digits ruby version

--- spec/opal/core/language/predefined_spec.rb
+++ spec/opal/core/language/predefined_spec.rb
@@ -114,7 +114,7 @@ describe "The predefined global constants" do
 
   it "includes RUBY_VERSION" do
     Object.const_defined?(:RUBY_VERSION).should == true
-    RUBY_VERSION.should =~ /^\d\.\d\.\d$/
+    RUBY_VERSION.should =~ /^\d+\.\d+\.\d+$/
   end
 
   it "includes RUBY_RELEASE_DATE" do
