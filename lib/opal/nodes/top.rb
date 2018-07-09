60cc336126350f1f4a75d2470c67488e14e49ae4
Jan Biedermann <jan@kursator.de>
2018-07-09 03:12:48 +0200

    fix

--- lib/opal/nodes/top.rb
+++ lib/opal/nodes/top.rb
@@ -16,7 +16,7 @@ module Opal
         push version_comment unless compiler.es6_modules?
 
         if compiler.es6_modules?
-          module_name = self.class.module_name(compiler.file)
+          module_name = Opal::Compiler.module_name_from_paths(compiler.file)
           opening(module_name)
         else
           opening
