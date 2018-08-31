d71bcc928d6ff34c1f285ccedee5436a616c62e9
Guillaume Grossetie <ggrossetie@gmail.com>
2018-08-31 17:07:19 +0200

    Use a local variable Regexp

--- spec/opal/core/string/gsub_spec.rb
+++ spec/opal/core/string/gsub_spec.rb
@@ -2,18 +2,18 @@ require 'spec_helper'
 
 describe 'String#gsub' do
   it 'handles recursive gsub' do
-    PassSlotRx = /{(\d+)}/
-    def recurse_gsub text
-      text.gsub(PassSlotRx) {
+    pass_slot_rx = /{(\d+)}/
+    recurse_gsub = -> text {
+      text.gsub(pass_slot_rx) {
         index = $1.to_i
         if index == 0
-          recurse_gsub '{1}'
+          recurse_gsub.call '{1}'
         else
           'value'
         end
       }
-    end
-    result = recurse_gsub '<code>{0}</code>'
+    }
+    result = recurse_gsub.call '<code>{0}</code>'
     result.should == '<code>value</code>'
   end
 end
