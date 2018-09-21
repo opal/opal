1ce73afd8ee7d8145a66eb572e8b9ee510f6be26
Guillaume Grossetie <g.grossetie@gmail.com>
2018-09-21 13:56:35 +0200

    Remove extra space after the dot

--- stdlib/nodejs/kernel.rb
+++ stdlib/nodejs/kernel.rb
@@ -18,7 +18,7 @@ module Kernel
 
   # @deprecated Please use `require('module')` instead
   def node_require(path)
-    warn '[DEPRECATION] node_require is deprecated.  Please use `require(\'module\')` instead.'
+    warn '[DEPRECATION] node_require is deprecated. Please use `require(\'module\')` instead.'
     `#{NODE_REQUIRE}(#{path.to_str})`
   end
 end
