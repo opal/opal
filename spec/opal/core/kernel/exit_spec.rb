1462f01817df26bf8bf6da73affb14ea64b94a23
Elia Schito <elia@schito.me>
2018-11-03 18:20:42 +0100

    Try to coerce Kernel#exit status to int
    Before any non boolean, non numeric object was treated as zero.

--- spec/opal/core/kernel/exit_spec.rb
+++ spec/opal/core/kernel/exit_spec.rb
@@ -1,15 +1,18 @@
 describe "Kernel#exit" do
   it "forwards the status code to Opal.exit(status)" do
     received_status { Kernel.exit }.should == 0
+
     received_status { Kernel.exit(0) }.should == 0
     received_status { Kernel.exit(1) }.should == 1
     received_status { Kernel.exit(2) }.should == 2
     received_status { Kernel.exit(123) }.should == 123
+
     received_status { Kernel.exit(true) }.should == 0
     received_status { Kernel.exit(false) }.should == 1
-    received_status { Kernel.exit(Object.new) }.should == 0
-    received_status { Kernel.exit([]) }.should == 0
-    received_status { Kernel.exit(/123/) }.should == 0
+
+    lambda { received_status { Kernel.exit(Object.new) } }.should raise_error(TypeError)
+    lambda { received_status { Kernel.exit([]) } }.should raise_error(TypeError)
+    lambda { received_status { Kernel.exit(/123/) } }.should raise_error(TypeError)
   end
 
   def received_status
