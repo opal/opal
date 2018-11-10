186853d19c139b6de16750671961b049b9ec1eb9
Elia Schito <elia@schito.me>
2018-11-11 00:47:26 +0100

    Typo

--- opal/corelib/runtime.js
+++ opal/corelib/runtime.js
@@ -1130,7 +1130,7 @@
   // This method is used to setup a native constructor (e.g. Array), to have
   // its prototype act like a normal Ruby class. Firstly, a new Ruby class is
   // created using the native constructor so that its prototype is set as the
-  // target for th new class. Note: all bridged classes are set to inherit
+  // target for the new class. Note: all bridged classes are set to inherit
   // from Object.
   //
   // Example:
