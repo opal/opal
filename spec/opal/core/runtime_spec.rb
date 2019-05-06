81d126a6fa410c81ea28786854a1dca435a99884
Jan Biedermann <jan@kursator.de>
2019-05-06 21:34:43 +0200

    add test for identifying strict_mode

--- spec/opal/core/runtime_spec.rb
+++ spec/opal/core/runtime_spec.rb
@@ -56,3 +56,9 @@ describe 'javascript access using .JS' do
     `a["foo"]`.should == 1
   end
 end
+
+describe 'runtime in strict mode' do
+  it 'correctly identifies strict mode or non strict mode' do
+    `(function(){return !this;})()`.should == `Opal.in_strict_mode`
+  end
+end
