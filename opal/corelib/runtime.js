cdc27a2fc1fc28f6bb11c898937652cecbc04e5b
Jan Biedermann <jan@kursator.de>
2019-05-06 20:15:58 +0200

    remove unused vars

--- opal/corelib/runtime.js
+++ opal/corelib/runtime.js
@@ -1155,12 +1155,6 @@
       throw Opal.ArgumentError.$new("already bridged");
     }
 
-    var klass_to_inject, klass_reference;
-
-    klass_to_inject = klass.$$super || Opal.Object;
-    klass_reference = klass;
-    var original_prototype = klass.$$prototype;
-
     // constructor is a JS function with a prototype chain like:
     // - constructor
     //   - super
