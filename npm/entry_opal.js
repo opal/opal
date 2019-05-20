c8ae14ae22afbb27e63ceeed62cddd1c90d25af3
Jan Biedermann <jan@kursator.de>
2019-05-20 03:12:36 +0200

    Add promise by default, required for require_lazy.

--- npm/entry_opal.js
+++ npm/entry_opal.js
@@ -1,4 +1,7 @@
 import opal from 'opal.rb';
 opal();
 Opal.load('opal');
+// make promise available by default, used by require_lazy
+import promise from 'promise.rb'
+Opal.load('promise');
 export default Opal;
--- npm/entry_opal_node.js
+++ npm/entry_opal_node.js
@@ -4,4 +4,7 @@ Opal.load('opal');
 import opal_node from 'nodejs.rb';
 opal_node();
 Opal.load('nodejs');
+// make promise available by default, used by require_lazy
+import promise from 'promise.rb'
+Opal.load('promise');
 export default Opal;
