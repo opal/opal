eb56b411c69232d55ed38a7af0ac9b5a502fb4de
Elia Schito <elia@schito.me>
2019-09-07 13:15:37 +0200

    Remove workaround targeted at JRuby 1.7
    At this moment the current JRuby version is 9.2.8.0, so should be safe
    to assume we're good now.

--- lib/opal/cli_runners/nodejs.rb
+++ lib/opal/cli_runners/nodejs.rb
@@ -42,22 +42,9 @@ module Opal
           return
         end
 
-        if RUBY_PLATFORM == 'java'
-          # JRuby has issues in dealing with subprocesses (at least up to 1.7.15)
-          # @headius told me it's mostly fixed on master, but while we wait for it
-          # to ship here's a tempfile workaround.
-          require 'tempfile'
-          require 'shellwords'
-          tempfile = Tempfile.new('opal-node-output')
-          system(env, cmd.shelljoin + " > #{tempfile.path}")
-          @exit_status = $?.exitstatus
-          captured_output = File.read tempfile.path
-          tempfile.close
-        else
-          require 'open3'
-          captured_output, status = Open3.capture2(env, *cmd)
-          @exit_status = status.exitstatus
-        end
+        require 'open3'
+        captured_output, status = Open3.capture2(env, *cmd)
+        @exit_status = status.exitstatus
 
         output.write captured_output
       end
