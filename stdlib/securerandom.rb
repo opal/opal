2e3c1c948209b236fb34a0a5a5832f319239c1c5
Jan Biedermann <jan@kursator.de>
2019-11-02 13:38:09 +0100

    Implement SecureRandom#hex

--- stdlib/securerandom.rb
+++ stdlib/securerandom.rb
@@ -1,4 +1,20 @@
 module SecureRandom
+  def self.hex(count)
+    %x{
+      var repeat = Math.floor(count / 6),
+          remain = count % 6,
+          string = '';
+
+      for (var i = 0; i < repeat; i++) {
+        string = string + Math.floor(Math.random()*parseInt('ff'.repeat(6), 16)).toString(16);
+      }
+      if (remain > 0) {
+        string = string + Math.floor(Math.random()*parseInt('ff'.repeat(remain), 16)).toString(16);
+      }
+      return string;
+    }
+  end
+
   def self.uuid
     'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.gsub(/[xy]/) do |ch,|
       %x{
