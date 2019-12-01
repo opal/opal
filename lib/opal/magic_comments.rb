4321155772cfefa0e820b8aa7eec1ac6e724a6ab
Elia Schito <elia@schito.me>
2019-12-01 15:52:52 +0100

    Let magic comments have complex values
    Allowing magic comments to have complex values will make the good
    vehicles for method names and other stuff.

--- lib/opal/magic_comments.rb
+++ lib/opal/magic_comments.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 module Opal::MagicComments
-  MAGIC_COMMENT_RE = /\A# *(\w+) *: *(\w+) *$/.freeze
-  EMACS_MAGIC_COMMENT_RE = /\A# *-\*- *(\w+) *: *(\w+) *-\*- *$/.freeze
+  MAGIC_COMMENT_RE = /\A# *(\w+) *: *(\S+.*?) *$/.freeze
+  EMACS_MAGIC_COMMENT_RE = /\A# *-\*- *(\w+) *: *(\S+.*?) *-\*- *$/.freeze
 
   def self.parse(sexp, comments)
     flags = {}
