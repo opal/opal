885263a1f3a80da8bf9db57579c6bd94f96e6003
Elia Schito <elia@schito.me>
2019-12-08 20:36:31 +0100

    Bump version to 1.0.1

--- opal/corelib/constants.rb
+++ opal/corelib/constants.rb
@@ -1,8 +1,8 @@
 RUBY_PLATFORM       = 'opal'
 RUBY_ENGINE         = 'opal'
-RUBY_VERSION        = '2.5.3'
-RUBY_ENGINE_VERSION = '1.0.0'
-RUBY_RELEASE_DATE   = '2019-05-12'
+RUBY_VERSION        = '2.5.5'
+RUBY_ENGINE_VERSION = '1.0.1'
+RUBY_RELEASE_DATE   = '2019-12-08'
 RUBY_PATCHLEVEL     = 0
 RUBY_REVISION       = 0
 RUBY_COPYRIGHT      = 'opal - Copyright (C) 2013-2019 Adam Beynon and the Opal contributors'
