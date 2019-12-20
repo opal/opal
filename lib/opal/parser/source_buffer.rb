bef6d177b4ce3eb8a836f0d4594bd996ebeceed8
Elia Schito <elia@schito.me>
2019-12-20 11:22:20 +0100

    Do not try to force coding when parsing from JS
    We do the (fair, we believe) assumption that everything that comes in
    is already correctly encoded.

--- lib/opal/parser/source_buffer.rb
+++ lib/opal/parser/source_buffer.rb
@@ -6,6 +6,13 @@ module Opal
       def self.recognize_encoding(string)
         super || Encoding::UTF_8
       end
+
+      # Skip encoding while compiling from a JavaScript runtime
+      if RUBY_PLATFORM == 'opal'
+        def self.reencode_string(input)
+          input
+        end
+      end
     end
   end
 end
