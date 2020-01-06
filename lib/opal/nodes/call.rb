f7309c64409ea152747843232a0390febfc26053
Jan Biedermann <jan@kursator.de>
2020-01-06 17:35:46 +0100

    Autoload for nodes/call.rb

--- lib/opal/nodes/call.rb
+++ lib/opal/nodes/call.rb
@@ -233,11 +233,29 @@ module Opal
         push fragment(')')
       end
 
-      add_special :autoload do |compile_default|
-        if scope.class_scope?
-          str = DependencyResolver.new(compiler, arglist.children[1]).resolve
-          compiler.requires << str unless str.nil?
-          compile_default.call
+      add_special :autoload do
+        unless scope.top?
+          push recv(receiver_sexp), method_jsid, '(', expr(arglist.children[0]), ', '
+          if arglist.children[1].type == :str && arglist.children[1].children[0] != ''
+            str = DependencyResolver.new(compiler, arglist.children[1]).resolve
+            if str.nil?
+              push expr(arglist.children[1])
+            else
+              file_path = Opal::ModulesHelpers.determine_real_module_path(str)
+              if file_path
+                compiler.requires << str
+                filename = arglist.children[1].children[0]
+                filename += '.rb' unless filename.end_with?('.rb')
+                push Opal::Compiler.module_name_from_paths(filename.inspect)
+              else
+                warn "Warning: File '#{arglist.children[1].children[0]}' for autoload of constant '#{arglist.children[0].children[0]}' does not exist!"
+                push expr(arglist.children[1])
+              end
+            end
+          else
+            push expr(arglist.children[1])
+          end
+          push ')'
         end
       end
 
