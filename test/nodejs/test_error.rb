be224890b6655129423a6c140e4ba366df7c4e07
Elia Schito <elia@schito.me>
2020-01-24 00:31:41 +0100

    Use "from" instead of "at" for source-maps
    This better mirrors ruby.

--- test/nodejs/test_error.rb
+++ test/nodejs/test_error.rb
@@ -6,7 +6,7 @@ class TestNodejsError < Test::Unit::TestCase
   def test_should_preserve_stack
     raise ArgumentError.new('oops')
   rescue => ex
-    backtrace_line = "at nodejs/test_error.rb:#{__LINE__ - 2}"
+    backtrace_line = " from nodejs/test_error.rb:#{__LINE__ - 2}"
     wrapped_ex = ex.exception %(context - #{ex.message})
     assert(`wrapped_ex.stack`.include?(backtrace_line))
   end
@@ -14,7 +14,7 @@ class TestNodejsError < Test::Unit::TestCase
   def test_should_set_stack
     raise ArgumentError.new('oops')
   rescue => ex
-    backtrace_line = "at nodejs/test_error.rb:#{__LINE__ - 2}"
+    backtrace_line = " from nodejs/test_error.rb:#{__LINE__ - 2}"
     wrapped_ex = ex.exception %(context - #{ex.message})
     wrapped_ex.set_backtrace ex.backtrace
     assert(`wrapped_ex.stack`.include?(backtrace_line))
@@ -23,7 +23,7 @@ class TestNodejsError < Test::Unit::TestCase
   def test_should_get_stack
     raise ArgumentError.new('oops')
   rescue => ex
-    backtrace_line = "at nodejs/test_error.rb:#{__LINE__ - 2}"
+    backtrace_line = " from nodejs/test_error.rb:#{__LINE__ - 2}"
     assert(`ex.stack`.include?(backtrace_line))
   end
 end
