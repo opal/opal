c92dfd90fd7ec2c6e9200f8907c183d9dcc8162a
Elia Schito <elia@schito.me>
2020-02-01 16:21:40 +0100

    Fix parsing unicode chars from Opal
    JavaScript works with UTF-16/UCS-2 and the strings we're passing to
    the lexer ar not UTF-8, so the lexer tries to unpack them as 8-bit
    chars, messing up the source ranges in which to look for pieces of
    code and ultimately shifting all source lookups after having
    encountered a unicode string.
    
    E.g.:
      # the string '5' is reported at index 10 when using C*, 6 with U*
      "123️⃣45".unpack('C*').index('5'.unpack('C*').first) # => 10
      "123️⃣45".unpack('U*').index('5'.unpack('U*').first) # => 6

--- lib/opal/parser/patch.rb
+++ lib/opal/parser/patch.rb
@@ -1,6 +1,21 @@
 # frozen_string_literal: true
 
 if RUBY_ENGINE == 'opal'
+  class Parser::Lexer
+    def source_buffer=(source_buffer)
+      @source_buffer = source_buffer
+
+      if @source_buffer
+        source = @source_buffer.source
+        # Force UTF8 unpacking even if JS works with UTF-16/UCS-2
+        # See: https://mathiasbynens.be/notes/javascript-encoding
+        @source_pts = source.unpack('U*')
+      else
+        @source_pts = nil
+      end
+    end
+  end
+
   class Parser::Lexer::Literal
     undef :extend_string
 
