6d87470172cb5ec37873a6a6efe2b5aca335c28a
Elia Schito <elia@schito.me>
2020-12-13 13:07:32 +0100

    Update the expected backtrace lines for nodejs

--- test/nodejs/test_error.rb
+++ test/nodejs/test_error.rb
@@ -7,7 +7,7 @@ class TestNodejsError < Test::Unit::TestCase
     raise ArgumentError.new('oops')
   rescue => ex
     wrapped_ex = ex.exception %(context - #{ex.message})
-    assert(`wrapped_ex.stack`.include?('at constructor.$$test_should_preserve_stack'))
+    assert_includes `#{wrapped_ex}.stack`, 'at constructor.$test_should_preserve_stack'
   end
 
   def test_should_set_stack
@@ -15,12 +15,12 @@ class TestNodejsError < Test::Unit::TestCase
   rescue => ex
     wrapped_ex = ex.exception %(context - #{ex.message})
     wrapped_ex.set_backtrace ex.backtrace
-    assert(`wrapped_ex.stack`.include?('at constructor.$$test_should_set_stack'))
+    assert_includes `#{wrapped_ex}.stack`, 'at constructor.$test_should_set_stack'
   end
 
   def test_should_get_stack
     raise ArgumentError.new('oops')
   rescue => ex
-    assert(`ex.stack`.include?('at constructor.$$test_should_get_stack'))
+    assert_includes `ex.stack`, 'at constructor.$test_should_get_stack'
   end
 end
