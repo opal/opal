8f704ab0100fe7b8543c9129a8aaad97f501a0c6
Kenichi Kamiya <kachick1@gmail.com>
2021-02-28 01:59:22 +0900

    Update Hash#{merge, merge!, update} as taking multiple arguments

--- opal/corelib/hash.rb
+++ opal/corelib/hash.rb
@@ -762,53 +762,51 @@ class Hash
 
   alias member? has_key?
 
-  def merge(other, &block)
-    dup.merge!(other, &block)
+  def merge(*others, &block)
+    dup.merge!(*others, &block)
   end
 
-  def merge!(other, &block)
+  def merge!(*others, &block)
     %x{
-      if (!other.$$is_hash) {
-        other = #{Opal.coerce_to!(other, Hash, :to_hash)};
-      }
+      var i, j, other, other_keys, length, key, value, other_value;
+      for (i = 0; i < others.length; ++i) {
+        other = #{Opal.coerce_to!(`others[i]`, Hash, :to_hash)};
+        other_keys = other.$$keys, length = other_keys.length;
 
-      var i, other_keys = other.$$keys, length = other_keys.length, key, value, other_value;
+        if (block === nil) {
+          for (j = 0; j < length; j++) {
+            key = other_keys[j];
 
-      if (block === nil) {
-        for (i = 0; i < length; i++) {
-          key = other_keys[i];
+            if (key.$$is_string) {
+              other_value = other.$$smap[key];
+            } else {
+              other_value = key.value;
+              key = key.key;
+            }
 
-          if (key.$$is_string) {
-            other_value = other.$$smap[key];
-          } else {
-            other_value = key.value;
-            key = key.key;
+            Opal.hash_put(self, key, other_value);
           }
-
-          Opal.hash_put(self, key, other_value);
-        }
-
-        return self;
-      }
-
-      for (i = 0; i < length; i++) {
-        key = other_keys[i];
-
-        if (key.$$is_string) {
-          other_value = other.$$smap[key];
         } else {
-          other_value = key.value;
-          key = key.key;
+          for (j = 0; j < length; j++) {
+            key = other_keys[j];
+
+            if (key.$$is_string) {
+              other_value = other.$$smap[key];
+            } else {
+              other_value = key.value;
+              key = key.key;
+            }
+
+            value = Opal.hash_get(self, key);
+
+            if (value === undefined) {
+              Opal.hash_put(self, key, other_value);
+              continue;
+            }
+
+            Opal.hash_put(self, key, block(key, value, other_value));
+          }
         }
-
-        value = Opal.hash_get(self, key);
-
-        if (value === undefined) {
-          Opal.hash_put(self, key, other_value);
-          continue;
-        }
-
-        Opal.hash_put(self, key, block(key, value, other_value));
       }
 
       return self;
