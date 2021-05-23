7194e21ab8e9448cb8d9b32fc8117a54a0986e2f
hmdne <54514036+hmdne@users.noreply.github.com>
2021-05-23 14:48:47 +0200

    Parser 3.0: Add missing tests for forward_args and numblocks rewriters

---
+++ spec/lib/rewriters/numblocks_spec.rb
@@ -0,0 +1,44 @@
+require 'lib/spec_helper'
+require 'support/rewriters_helper'
+require 'opal/rewriters/numblocks'
+
+RSpec.describe Opal::Rewriters::Numblocks do
+  include RewritersHelper
+  extend  RewritersHelper
+
+  before(:each) { Opal::Rewriters::ForRewriter.reset_tmp_counter! }
+
+  correct_names = proc do |ast|
+    case ast
+    when Opal::AST::Node
+      ast.children.map do |i|
+        correct_names.(i)
+      end.yield_self { |children| s(ast.type, *children) }
+    when :arg1 then :_1
+    when :arg2 then :_2
+    when :arg3 then :_3
+    else
+      ast
+    end
+  end
+
+  include_examples 'it rewrites source-to-AST', <<~ENDSOURCE, correct_names.(parse(<<~ENDDEST))
+    proc do
+      _1
+    end
+  ENDSOURCE
+    proc do |arg1|
+      arg1
+    end
+  ENDDEST
+
+  include_examples 'it rewrites source-to-AST', <<~ENDSOURCE, correct_names.(parse(<<~ENDDEST))
+    proc do
+      _3
+    end
+  ENDSOURCE
+    proc do |arg1, arg2, arg3|
+      arg3
+    end
+  ENDDEST
+end
