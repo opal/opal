ceed2729a34d7f048eb39cd647b4f3c19ac8cca4
hmdne <54514036+hmdne@users.noreply.github.com>
2021-08-15 13:31:06 +0200

    Allow aliasing on main

--- opal/corelib/runtime.js
+++ opal/corelib/runtime.js
@@ -1948,8 +1948,15 @@
   Opal.alias = function(obj, name, old) {
     var id     = '$' + name,
         old_id = '$' + old,
-        body   = obj.$$prototype['$' + old],
+        body,
         alias;
+
+    // Aliasing on main means aliasing on Object...
+    if (typeof obj.$$prototype === 'undefined') {
+      obj = Opal.Object;
+    }
+
+    body = obj.$$prototype['$' + old];
 
     // When running inside #instance_eval the alias refers to class methods.
     if (obj.$$eval) {
