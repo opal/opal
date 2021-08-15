4443fee7f84bbd219164d121144b609f5d3dc24e
hmdne <54514036+hmdne@users.noreply.github.com>
2021-08-15 13:47:57 +0200

    Buffer#to_s, Kernel#`, Kernel#system

--- stdlib/nodejs/kernel.rb
+++ stdlib/nodejs/kernel.rb
@@ -3,10 +3,31 @@
 module Kernel
   NODE_REQUIRE = `require`
 
+  @__child_process__ = `require('child_process')`
+  `var __child_process__ = #{@__child_process__}`
+
   # @deprecated Please use `require('module')` instead
   def node_require(path)
     warn '[DEPRECATION] node_require is deprecated. Please use `require(\'module\')` instead.'
     `#{NODE_REQUIRE}(#{path.to_str})`
+  end
+
+  def system(cmdname, *argv, exception: false)
+    out = if argv.empty?
+      `__child_process__.spawnSync(#{cmdname}, { shell: true, stdio: 'inherit' })`
+    elsif Array === cmdname
+      `__child_process__.spawnSync(#{cmdname}, #{argv}, { stdio: 'inherit' })`
+    else
+      `__child_process__.spawnSync(#{cmdname[0]}, #{argv}, { argv0: #{cmdname[1]}, stdio: 'inherit' })`
+    end
+
+    $? = `#{out}.status`
+    raise "Command failed with exit #{$?}: #{cmdname}" if exception && $? != 0
+    $? == 0
+  end
+
+  def `(cmdline)
+    Buffer.new(`__child_process__.execSync(#{cmdline})`).to_s.encode('UTF-8')
   end
 end
 
