78a25909f47fa6bc72ba8c3e7ba412c8372485bd
ppmasa8 <kiyokiyopip56@gmail.com>
2021-08-26 16:09:32 +0900

    exclude whitespace

--- stdlib/json.rb
+++ stdlib/json.rb
@@ -168,7 +168,7 @@ class Hash
         result.push(#{`key`.to_s.to_json} + ':' + #{`value`.to_json});
       }
 
-      return '{' + result.join(', ') + '}';
+      return '{' + result.join(',') + '}';
     }
   end
 end
