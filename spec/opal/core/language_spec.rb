opal/opal

commit 9d3a43dc9f4a5f59c7bd2660841888741541e51e

Move the autoload handling from const_missing to const dispatching
Co-Authored-By: Jan Biedermann <jan@kursator.de>
Co-Authored-By: hmdne <54514036+hmdne@users.noreply.github.com>

opal/corelib/runtime.js
@@ -234,6 +234,25 @@
     }
   }
 
+  function handle_autoload(cref, name) {
+    if (!cref.$$autoload[name].loaded) {
+      cref.$$autoload[name].loaded = true;
+      try {
+        Opal.Kernel.$require(cref.$$autoload[name].path);
+      } catch (e) {
+        cref.$$autoload[name].exception = e;
+        throw e;
+      }
+      cref.$$autoload[name].required = true;
+      if (cref.$$const[name]) {
+        cref.$$autoload[name].success = true;
+        return cref.$$const[name];
+      }
+    } else if (cref.$$autoload[name].loaded && !cref.$$autoload[name].required) {
+      if (cref.$$autoload[name].exception) { throw cref.$$autoload[name].exception; }
+    }
+  }
+
   // Constants
   // ---------
   //
@@ -248,7 +267,12 @@
 
   // Get the constant in the scope of the current cref
   function const_get_name(cref, name) {
-    if (cref) return cref.$$const[name];
+    if (cref) {
+      if (cref.$$const[name]) { return cref.$$const[name]; }
+      if (cref.$$autoload && cref.$$autoload[name]) {
+        return handle_autoload(cref, name);
+      }
+    }
   }
 
   // Walk up the nesting array looking for the constant
@@ -261,7 +285,11 @@
     // and in order. The ancestors of those elements are ignored.
     for (i = 0, ii = nesting.length; i < ii; i++) {
       constant = nesting[i].$$const[name];
-      if (constant != null) return constant;
+      if (constant != null) {
+        return constant;
+      } else if (nesting[i].$$autoload && nesting[i].$$autoload[name]) {
+        return handle_autoload(nesting[i], name);
+      }
     }
   }
 
@@ -276,6 +304,8 @@
     for (i = 0, ii = ancestors.length; i < ii; i++) {
       if (ancestors[i].$$const && $has_own.call(ancestors[i].$$const, name)) {
         return ancestors[i].$$const[name];
+      } else if (ancestors[i].$$autoload && ancestors[i].$$autoload[name]) {
+        return handle_autoload(ancestors[i], name);
       }
     }
   }
@@ -417,6 +447,11 @@
       for (constant in module.$$const) {
         constants[constant] = true;
       }
+      if (module.$$autoload) {
+        for (constant in module.$$autoload) {
+          constants[constant] = true;
+        }
+      }
     }
 
     return Object.keys(constants);
@@ -432,7 +467,7 @@
       return old;
     }
 
-    if (cref.$$autoload != null && cref.$$autoload[name] != null) {
+    if (cref.$$autoload && cref.$$autoload[name]) {
       delete cref.$$autoload[name];
       return nil;
     }

opal/corelib/module.rb
@@ -199,9 +199,13 @@ class Module
         #{raise ArgumentError, 'empty file name'}
       }
 
-      if (self.$$autoload == null) self.$$autoload = {};
-      Opal.const_cache_version++;
-      self.$$autoload[#{const}] = #{path};
+      if (!self.$$const.hasOwnProperty(#{const})) {
+        if (!self.$$autoload) {
+          self.$$autoload = {};
+        }
+        Opal.const_cache_version++;
+        self.$$autoload[#{const}] = { path: #{path}, loaded: false, required: false, success: false, exception: false };
+      }
       return nil;
     }
   end
@@ -291,7 +295,13 @@ class Module
 
       for (i = 0, ii = modules.length; i < ii; i++) {
         module = modules[i];
-        if (module.$$const[name] != null) {
+        if (module.$$const[#{name}] != null) { return true; }
+        if (
+          module.$$autoload &&
+          module.$$autoload[#{name}] &&
+          !module.$$autoload[#{name}].required &&
+          !module.$$autoload[#{name}].success
+        ) {
           return true;
         }
       }
@@ -325,18 +335,6 @@ class Module
   end
 
   def const_missing(name)
-    %x{
-      if (self.$$autoload) {
-        var file = self.$$autoload[name];
-
-        if (file) {
-          self.$require(file);
-
-          return #{const_get name};
-        }
-      }
-    }
-
     full_const_name = self == Object ? name : "#{self}::#{name}"
 
     raise NameError.new("uninitialized constant #{full_const_name}", name)

commit 5eddf17a7a3a627baa3df9e023a5689aeadb21b7

Fix #2351
This commit fixes a regression introduced by 9d3a43d. In runtime,
const_get_name had a check like: `if(cref.$$const[name])` which
broke if the const was `0`.

opal/corelib/runtime.js
@@ -244,7 +244,7 @@
         throw e;
       }
       cref.$$autoload[name].required = true;
-      if (cref.$$const[name]) {
+      if (cref.$$const[name] != null) {
         cref.$$autoload[name].success = true;
         return cref.$$const[name];
       }
@@ -268,7 +268,7 @@
   // Get the constant in the scope of the current cref
   function const_get_name(cref, name) {
     if (cref) {
-      if (cref.$$const[name]) { return cref.$$const[name]; }
+      if (cref.$$const[name] != null) { return cref.$$const[name]; }
       if (cref.$$autoload && cref.$$autoload[name]) {
         return handle_autoload(cref, name);
       }

spec/opal/core/language_spec.rb
@@ -27,3 +27,17 @@ describe "Bridging" do
     `typeof(String.call)`.should == "function"
   end
 end
+
+describe "Constants" do
+  it "doesn't raise error when a JS falsey constant is referenced" do
+    z = Class.new {
+      C1 = 0
+      C2 = nil
+      C3 = false
+      C4 = ''
+      C5 = C3
+    }
+
+    [z::C1, z::C2, z::C3, z::C4, z::C5].should == [0, nil, false, '', false]
+  end
+end