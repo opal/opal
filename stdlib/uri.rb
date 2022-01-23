57e2c7cdeac7c13403c602d438cd21974154eddf
HoneyryderChuck <cardoso_tiago@hotmail.com>
2022-01-23 01:35:34 +0000

    added URI.decode_www_form

---
+++ stdlib/uri.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module URI
+	def self.decode_www_form(str, separator: '&', isindex: false)
+		%x{
+			var str = #{str};
+			var ary = [];
+			if (str.length == 0) {
+				return ary;
+			}
+			var parts = str.split(#{separator});
+			for (var i = 0; i < parts.length; i++) {
+				var string = parts[i];
+				var comps = string.split('=');
+				var key = comps[0];
+				var val = comps[1];
+				if (#{isindex}) {
+					if (comps.length < 2) {
+						val = key;
+						key = '';
+					}
+				}
+
+				key = decodeURIComponent(key.replace(/\+/g, ' '));
+				if (val) {
+					val = decodeURIComponent(val.replace(/\+/g, ' '));
+				} else {
+					val = '';
+				}
+				ary.push([key, val]);
+			}
+			return ary;
+		}
+	end
+end
