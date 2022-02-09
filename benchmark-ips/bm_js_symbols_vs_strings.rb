ac3477d97b94fef1626d0972c0d6da110d9d650e
Elia Schito <elia@schito.me>
2022-02-09 11:07:05 +0100

    Improve the benchmark for symbol property access

--- benchmark-ips/bm_js_symbols_vs_strings.rb
+++ benchmark-ips/bm_js_symbols_vs_strings.rb
@@ -1,33 +1,58 @@
 Benchmark.ips do |x|
   %x{
+    const c_foo = 'foo'
+    const v_foo = 'foo'
+    const cfoo = Symbol('foo')
+    var vfoo = Symbol('foo')
+    const cgfoo = Symbol.for('foo')
+    var vgfoo = Symbol.for('foo')
+
     var o = {}
-    o[Symbol.for('foo')] = 123
-    o.foo = 123
-    var foo = Symbol('foo')
-    var gfoo = Symbol.for('foo')
-    o[foo] = 123
-    var a = 0, b = 0, c = 0
+    o[cfoo] = 1
+    o[cgfoo] = 1
+    o[c_foo] = 1
+    o[vfoo] = 1
+
+    let a1 = 0, a2 = 0, a3 = 0, a4 = 0, a5 = 0, a6 = 0, a7 = 0, a8 = 0
   }
 
+  x.report('const string ref') do
+    `a1 += o[c_foo]`
+  end
+
+  x.report('var string ref') do
+    `a2 += o[v_foo]`
+  end
+
   x.report('live global symbol') do
-    `a += o[Symbol.for('foo')]`
+    `a3 += o[Symbol.for('foo')]`
   end
 
-  x.report('stored global symbol') do
-    `a += o[gfoo]`
+  x.report('const global symbol') do
+    `a4 += o[cgfoo]`
   end
 
-  x.report('stored symbol') do
-    `a += o[foo]`
+  x.report('var global symbol') do
+    `a5 += o[vgfoo]`
+  end
+
+  x.report('const symbol') do
+    `a6 += o[cfoo]`
+  end
+
+  x.report('var symbol') do
+    `a6 += o[vfoo]`
   end
 
   x.report('ident') do
-    `b += o.foo`
+    `a7 += o.foo`
   end
 
-  x.report('string') do
-    `c += o['foo']`
+  x.report('live string') do
+    `a8 += o['foo']`
   end
+
+  x.time = 10
 
   x.compare!
 end
