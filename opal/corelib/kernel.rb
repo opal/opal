0c6c89dc3cb94a367f639d7dcd4c9a19a6d2daf3
Elia Schito <elia@schito.me>
2022-08-11 23:40:17 +0200

    Let the compiler handle the reference to ::Object

--- opal/corelib/kernel.rb
+++ opal/corelib/kernel.rb
@@ -855,6 +855,6 @@ class ::Object
   # Object.require has been set to runtime.js Opal.require
   # Now we have Kernel loaded, make sure Object.require refers to Kernel.require
   # which is what ruby does and allows for overwriting by autoloaders
-  `delete $Object.$$prototype.$require`
+  `delete #{self}.$$prototype.$require`
   include ::Kernel
 end
