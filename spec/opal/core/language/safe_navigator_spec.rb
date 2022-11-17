opal/opal

commit 6ef5abea33259822ef5ee10d1121025c58b40687

CSendNode is now handled by CallNode
Conditional send in Ruby can be used together with writers to
return a correct value.

lib/opal/nodes/call.rb
@@ -8,7 +8,7 @@ require 'opal/rewriters/break_finder'
 module Opal
   module Nodes
     class CallNode < Base
-      handle :send
+      handle :send, :csend
 
       attr_reader :recvr, :meth, :arglist, :iter
 
@@ -45,7 +45,7 @@ module Opal
         handle_special do
           compiler.method_calls << meth.to_sym if record_method?
 
-          with_writer do
+          with_wrapper do
             if using_eval?
               # if trying to access an lvar in eval or irb mode
               compile_eval_var
@@ -90,6 +90,11 @@ module Opal
 
       def invoke_using_refinement?
         !scope.scope.collect_refinements_temps.empty?
+      end
+
+      # Is it a conditional send, ie. `foo&.bar`?
+      def csend?
+        @sexp.type == :csend
       end
 
       def default_compile
@@ -147,7 +152,7 @@ module Opal
       end
 
       def compile_receiver
-        push recv(receiver_sexp)
+        push @conditional_recvr || recv(receiver_sexp)
       end
 
       def compile_method_name
@@ -456,8 +461,12 @@ module Opal
         )
       end
 
-      def with_writer(&block)
-        if call_is_writer_that_needs_handling?
+      def with_wrapper(&block)
+        if csend? && !@conditional_recvr
+          handle_conditional_send do
+            with_wrapper(&block)
+          end
+        elsif call_is_writer_that_needs_handling?
           handle_writer(&block)
         else
           yield
@@ -466,6 +475,19 @@ module Opal
 
       def call_is_writer_that_needs_handling?
         (expr? || recv?) && (meth.to_s =~ /^\w+=$/ || meth == :[]=)
+      end
+
+      # Handle safe-operator calls: foo&.bar / foo&.bar ||= baz / ...
+      def handle_conditional_send
+        # temporary variable that stores method receiver
+        receiver_temp = scope.new_temp
+        push "#{receiver_temp} = ", expr(recvr)
+
+        # execute the sexp only if the receiver isn't nil
+        push ", (#{receiver_temp} === nil || #{receiver_temp} == null) ? nil : "
+        @conditional_recvr = receiver_temp
+        yield
+        wrap '(', ')'
       end
 
       def handle_writer

lib/opal/nodes.rb
@@ -5,7 +5,6 @@ require 'opal/nodes/literal'
 require 'opal/nodes/variables'
 require 'opal/nodes/constants'
 require 'opal/nodes/call'
-require 'opal/nodes/csend'
 require 'opal/nodes/call_special'
 require 'opal/nodes/module'
 require 'opal/nodes/class'

lib/opal/nodes/csend.rb
@@ -1,24 +1,0 @@
-# frozen_string_literal: true
-
-require 'opal/nodes/call'
-
-module Opal
-  module Nodes
-    # Safe navigator recv&.meth(arglist, &block)
-    class CSendNode < CallNode
-      handle :csend
-
-      def default_compile
-        helper :send
-
-        conditional_send(recv(receiver_sexp)) do |receiver_temp|
-          push '$send(', receiver_temp
-          compile_method_name
-          compile_arguments
-          compile_block_pass
-          push ')'
-        end
-      end
-    end
-  end
-end

lib/opal/nodes/helpers.rb
@@ -114,25 +114,6 @@ module Opal
           end
         end
       end
-
-      # Usefule for safe-operator calls: foo&.bar / foo&.bar ||= baz / ...
-      #
-      # @param recvr [sexp_pushable] The receiver of the call that will be
-      #        stored in a temporary variable
-      # @yields receiver_temp [String] the name of the temporary variable
-      #         holding the ref to the original receiver, inside the block
-      #         an expr() should be pushed.
-      #
-      def conditional_send(recvr)
-        # temporary variable that stores method receiver
-        receiver_temp = scope.new_temp
-        push "#{receiver_temp} = ", recvr
-
-        # execute the sexp only if the receiver isn't nil
-        push ", (#{receiver_temp} === nil || #{receiver_temp} == null) ? nil : "
-        yield receiver_temp
-        wrap '(', ')'
-      end
     end
   end
 end

spec/filters/bugs/language.rb
@@ -213,7 +213,6 @@ opal_filter "language" do
   fails "Predefined global $~ raises an error if assigned an object not nil or instanceof MatchData"
   fails "Ruby String interpolation returns a string with the source encoding by default" # Expected #<Encoding:UTF-8> == #<Encoding:ASCII-8BIT (dummy)> to be truthy but was false
   fails "Ruby String interpolation returns a string with the source encoding, even if the components have another encoding" # ArgumentError: unknown encoding name - euc-jp
-  fails "Safe navigator allows assignment methods"
   fails "The ** operator hash with omitted value accepts mixed syntax" # NameError: uninitialized constant MSpecEnv::a
   fails "The ** operator hash with omitted value accepts short notation 'key' for 'key: value' syntax" # NameError: uninitialized constant MSpecEnv::a
   fails "The ** operator hash with omitted value ignores hanging comma on short notation" # NameError: uninitialized constant MSpecEnv::a
@@ -285,8 +284,6 @@ opal_filter "language" do
   fails "The redo statement in a method is invalid and raises a SyntaxError" # Expected SyntaxError but no exception was raised ("m" was returned)
   fails "The redo statement triggers ensure block when re-executing a block"
   fails "The rescue keyword allows rescue in 'do end' block" # NoMethodError: undefined method `call' for nil
-  fails "The rescue keyword can capture the raised exception using a setter method" # NoMethodError: undefined method `message' for nil
-  fails "The rescue keyword can capture the raised exception using a square brackets setter" # ArgumentError: [SquareBracketsCaptor#[]=] wrong number of arguments(1 for 2)
   fails "The rescue keyword inline form can be inlined" # Expected Infinity to equal 1
   fails "The rescue keyword only accepts Module or Class in rescue clauses" # RuntimeError: error
   fails "The rescue keyword only accepts Module or Class in splatted rescue clauses" # RuntimeError: error

commit 18290d9f4fe97c6ac3cd90a8f29a138e40064e2d

Fix compilation of a call: `gets&.chomp`
There was missing logic in CallNode#compile_simple_call_chain
(this is the most basic version of call compilation, which
didn't use the CallNode#compile_receiver).

This caused `opal-repl -Rchrome` to stop working.

The bug was introduced by 6ef5abea33259822ef5ee10d1121025c58b40687

This fixes #2467

lib/opal/nodes/call.rb
@@ -186,7 +186,8 @@ module Opal
       end
 
       def compile_simple_call_chain
-        push recv(receiver_sexp), method_jsid, '(', expr(arglist), ')'
+        compile_receiver
+        push method_jsid, '(', expr(arglist), ')'
       end
 
       def splat?
@@ -469,7 +470,7 @@ module Opal
       def handle_conditional_send
         # temporary variable that stores method receiver
         receiver_temp = scope.new_temp
-        push "#{receiver_temp} = ", expr(recvr)
+        push "#{receiver_temp} = ", expr(receiver_sexp)
 
         # execute the sexp only if the receiver isn't nil
         push ", (#{receiver_temp} === nil || #{receiver_temp} == null) ? nil : "

opal/corelib/io.rb
@@ -89,7 +89,7 @@ class ::IO
     parts = ''
 
     # Will execure at most twice - one time reading from a buffer
-    # second time
+    # second time executing read proc
     begin
       @read_buffer += parts
       if @read_buffer != ''

spec/opal/core/language/safe_navigator_spec.rb
@@ -4,4 +4,14 @@ describe 'Safe navigator' do
       value&.unknown.should == nil
     end
   end
+
+  it "calls a receiver exactly once" do
+    def receiver
+      @calls += 1
+    end
+    @calls = 0
+    receiver&.itself.should == 1
+    @calls = 0
+    receiver&.itself{}.should == 1
+  end
 end