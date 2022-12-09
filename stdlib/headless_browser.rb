fd4080a28b17e66c94825dcefee970c2607051b1
hmdne <54514036+hmdne@users.noreply.github.com>
2022-12-09 18:18:33 +0100

    Move stdlib/headless_browser{.rb,/base.rb}

--- stdlib/headless_browser.rb
+++ stdlib/headless_browser.rb
@@ -1,15 +1,4 @@
-%x{
-  // Inhibit the default exit behavior
-  window.OPAL_EXIT_CODE = "noexit";
+# frozen_string_literal: true
 
-  Opal.exit = function(code) {
-    // The first call to Opal.exit should save an exit code.
-    // All next invocations must be ignored.
-    // Then we send an event to Chrome CDP Interface that we are finished
-
-    if (window.OPAL_EXIT_CODE === "noexit") {
-      window.OPAL_EXIT_CODE = code;
-      window.alert("opalheadlessbrowserexit");
-    }
-  }
-}
+require 'headless_browser/base'
+require 'headless_browser/file'
