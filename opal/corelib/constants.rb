0d25a3140eeb4aadc51a803e75a133c6ae34e03b
hmdne <54514036+hmdne@users.noreply.github.com>
2022-12-16 07:36:10 +0100

    Switch reported version to Ruby 3.2; set Opal version to 1.7.0dev

--- opal/corelib/constants.rb
+++ opal/corelib/constants.rb
@@ -1,8 +1,8 @@
 ::RUBY_PLATFORM       = 'opal'
 ::RUBY_ENGINE         = 'opal'
-::RUBY_VERSION        = '3.1.0'
-::RUBY_ENGINE_VERSION = '1.6.1'
-::RUBY_RELEASE_DATE   = '2022-12-09'
+::RUBY_VERSION        = '3.2.0'
+::RUBY_ENGINE_VERSION = '1.7.0dev'
+::RUBY_RELEASE_DATE   = '2022-12-16'
 ::RUBY_PATCHLEVEL     = 0
 ::RUBY_REVISION       = '0'
 ::RUBY_COPYRIGHT      = 'opal - Copyright (C) 2013-2022 Adam Beynon and the Opal contributors'
