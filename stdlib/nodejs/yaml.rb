d2af83d791f3c691b0094c3c14b17b489f4fbefc
hmdne <54514036+hmdne@users.noreply.github.com>
2023-06-09 23:16:40 +0200

    nodejs/yaml: restore functionality
    Due to the course of time, nodejs/yaml has broken. This PR:
    - restores the functionality
    - adds a YAML.load method
    - adds minitest tests, so it wouldn't break again
    
    This PR has been sponsored by Ribose Inc. ref: plurimath/plurimath#159

--- stdlib/nodejs/yaml.rb
+++ stdlib/nodejs/yaml.rb
@@ -1,14 +1,18 @@
 # backtick_javascript: true
 
 require 'native'
-require_relative './js-yaml-3-6-1'
+require 'nodejs/js-yaml-3-6-1'
 
 module YAML
-  @__yaml__ = `jsyaml`
+  @__yaml__ = `globalThis.jsyaml`
   `var __yaml__ = #{@__yaml__}`
 
   def self.load_path(path)
-    loaded = `__yaml__.safeLoad(#{File}.__fs__.readFileSync(#{path}, 'utf8'))`
+    load(`#{File}.__fs__.readFileSync(#{path}, 'utf8')`)
+  end
+
+  def self.load(data)
+    loaded = `__yaml__.safeLoad(data)`
     loaded = Hash.new(loaded) if native?(loaded)
     loaded
   end
