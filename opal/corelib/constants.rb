6975ccd8602dea5415e082826fe3dfe247f025f4
Elia Schito <elia@schito.me>
2023-10-12 11:20:22 +0200

    Release v1.8.0.beta1

--- opal/corelib/constants.rb
+++ opal/corelib/constants.rb
@@ -1,8 +1,8 @@
 ::RUBY_PLATFORM       = 'opal'
 ::RUBY_ENGINE         = 'opal'
 ::RUBY_VERSION        = '3.2.0'
-::RUBY_ENGINE_VERSION = '1.8.0.alpha1'
-::RUBY_RELEASE_DATE   = '2023-08-04'
+::RUBY_ENGINE_VERSION = '1.8.0.beta1'
+::RUBY_RELEASE_DATE   = '2023-10-12'
 ::RUBY_PATCHLEVEL     = 0
 ::RUBY_REVISION       = '0'
 ::RUBY_COPYRIGHT      = 'opal - Copyright (C) 2011-2023 Adam Beynon and the Opal contributors'
