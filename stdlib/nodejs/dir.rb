0b457f86db4e47e30323c59cbc4c725173456e91
Guillaume Grossetie <ggrossetie@yuzutech.fr>
2024-06-29 10:00:31 +0200

    resolves #2666 replace glob by picomatch

--- stdlib/nodejs/dir.rb
+++ stdlib/nodejs/dir.rb
@@ -1,22 +1,28 @@
 # backtick_javascript: true
 
 class Dir
-  @__glob__ = `require('glob')`
+  @__pm__ = `require('picomatch')`
   @__fs__ = `require('fs')`
   @__path__ = `require('path')`
   @__os__ = `require('os')`
-  `var __glob__ = #{@__glob__}`
+  `var __pm__ = #{@__pm__}`
   `var __fs__ = #{@__fs__}`
   `var __path__ = #{@__path__}`
   `var __os__ = #{@__os__}`
 
+  %x{
+    function pwd() {
+      return process.cwd().split(__path__.sep).join(__path__.posix.sep)
+    }
+  }
+
   class << self
-    def [](glob)
-      `__glob__.sync(#{glob})`
+    def [](pattern)
+      glob(pattern)
     end
 
     def pwd
-      `process.cwd().split(__path__.sep).join(__path__.posix.sep)`
+      `pwd()`
     end
 
     def home
@@ -44,11 +50,38 @@ class Dir
 
     def glob(pattern)
       pattern = [pattern] unless pattern.respond_to? :each
-      pattern.flat_map do |subpattern|
+      pattern = pattern.map do |subpattern|
         subpattern = subpattern.to_path if subpattern.respond_to? :to_path
-        subpattern = ::Opal.coerce_to!(subpattern, String, :to_str)
-        `__glob__.sync(subpattern)`
+        ::Opal.coerce_to!(subpattern, String, :to_str)
       end
+      %x{
+        return #{pattern}.flatMap((subpattern) => {
+          const scanResult = __pm__.scan(subpattern)
+          let base = scanResult.negated || scanResult.base === ''
+            ? pwd()
+            : scanResult.base
+          try {
+            const stat = __fs__.statSync(base)
+            if (stat.isFile()) {
+              base = __path__.dirname(base)
+            }
+          } catch (e) {
+            if (e.code === 'ENOENT' || e.code === 'EACCES' || e.code === 'ENOTDIR') {
+              return []
+            }
+          }
+          try {
+            const files = __fs__.readdirSync(base, {recursive: true})
+            const isMatch = __pm__(subpattern, { windows: true })
+            return files.filter(f => isMatch(__path__.join(base, f)))
+          } catch (e) {
+            if (e.code === 'ENOENT' || e.code === 'EACCES' || e.code === 'ENOTDIR') {
+              return []
+            }
+            throw e
+          }
+        })
+      }
     end
 
     alias getwd pwd
