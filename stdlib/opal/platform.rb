ddd8f8f2e2a09876693623320194b965d1628292
Jan Biedermann <jan@isomorfeus.com>
2024-09-18 01:10:56 +0200

    Update opal/platform

--- stdlib/opal/platform.rb
+++ stdlib/opal/platform.rb
@@ -1,13 +1,14 @@
 require 'opal-platform'
 
 case OPAL_PLATFORM
-when 'nashorn'          then require 'nashorn'
+when 'bun'              then require 'bun'
+when 'deno'             then require 'deno'
 when 'gjs'              then require 'gjs'
-when 'quickjs'          then require 'quickjs'
-when 'deno'             then require 'deno/base'
-when 'nodejs'           then require 'nodejs/base'
 when 'headless-chrome'  then require 'headless_browser/base'
 when 'headless-firefox' then require 'headless_browser/base'
-when 'safari'           then require 'headless_browser/base'
+when 'nashorn'          then require 'nashorn'
+when 'nodejs'           then require 'nodejs'
 when 'opal-miniracer'   then require 'opal/miniracer'
+when 'quickjs'          then require 'quickjs'
+when 'safari'           then require 'headless_browser/base'
 end
