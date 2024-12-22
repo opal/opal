5148b05ea42e96386b2e319812c62a87c53c9e3f
hmdne <54514036+hmdne@users.noreply.github.com>
2024-12-22 03:08:45 +0100

    Isolate array functions into their own runtime module

--- opal/runtime/boot.js
+++ opal/runtime/boot.js
@@ -626,58 +626,6 @@
     return ancestors.indexOf(klass) !== -1;
   };
 
-  // Helpers for implementing multiple assignment
-  // Our code for extracting the values and assigning them only works if the
-  // return value is a JS array.
-  // So if we get an Array subclass, extract the wrapped JS array from it
-
-  // Used for: a, b = something (no splat)
-  Opal.to_ary = function(value) {
-    if (value.$$is_array) {
-      return value;
-    }
-    else if (value['$respond_to?']('to_ary', true)) {
-      var ary = value.$to_ary();
-      if (ary === nil) {
-        return [value];
-      }
-      else if (ary.$$is_array) {
-        return ary;
-      }
-      else {
-        $raise(Opal.TypeError, "Can't convert " + value.$$class +
-          " to Array (" + value.$$class + "#to_ary gives " + ary.$$class + ")");
-      }
-    }
-    else {
-      return [value];
-    }
-  };
-
-  // Used for: a, b = *something (with splat)
-  Opal.to_a = function(value) {
-    if (value.$$is_array) {
-      // A splatted array must be copied
-      return value.slice();
-    }
-    else if (value['$respond_to?']('to_a', true)) {
-      var ary = value.$to_a();
-      if (ary === nil) {
-        return [value];
-      }
-      else if (ary.$$is_array) {
-        return ary;
-      }
-      else {
-        $raise(Opal.TypeError, "Can't convert " + value.$$class +
-          " to Array (" + value.$$class + "#to_a gives " + ary.$$class + ")");
-      }
-    }
-    else {
-      return [value];
-    }
-  };
-
   function $apply_blockopts(block, blockopts) {
     if (typeof(blockopts) === 'number') {
       block.$$arity = blockopts;
