fd5703efa4588ad07e6491610f48a23e8a31cb25
hmdne <54514036+hmdne@users.noreply.github.com>
2024-12-22 04:00:23 +0100

    Move typechecking and typecasting functions to a class runtime module

--- opal/runtime/boot.js
+++ opal/runtime/boot.js
@@ -187,49 +187,6 @@
   }
   Opal.return_val = $return_val;
 
-  Opal.coerce_to = function(object, type, method, args) {
-    var body;
-
-    if (method === 'to_int' && type === Opal.Integer && object.$$is_number)
-      return object < 0 ? Math.ceil(object) : Math.floor(object);
-
-    if (method === 'to_str' && type === Opal.String && object.$$is_string)
-      return object;
-
-    if (Opal.is_a(object, type)) return object;
-
-    // Fast path for the most common situation
-    if (object['$respond_to?'].$$pristine && object.$method_missing.$$pristine) {
-      body = object[$jsid(method)];
-      if (body == null || body.$$stub) Opal.type_error(object, type);
-      return body.apply(object, args);
-    }
-
-    if (!object['$respond_to?'](method)) {
-      Opal.type_error(object, type);
-    }
-
-    if (args == null) args = [];
-    return Opal.send(object, method, args);
-  }
-
-  Opal.respond_to = function(obj, jsid, include_all) {
-    if (obj == null || !obj.$$class) return false;
-    include_all = !!include_all;
-    var body = obj[jsid];
-
-    if (obj['$respond_to?'].$$pristine) {
-      if (typeof(body) === "function" && !body.$$stub) {
-        return true;
-      }
-      if (!obj['$respond_to_missing?'].$$pristine) {
-        return Opal.send(obj, obj['$respond_to_missing?'], [jsid.substr(1), include_all]);
-      }
-    } else {
-      return Opal.send(obj, obj['$respond_to?'], [jsid.substr(1), include_all]);
-    }
-  }
-
   // Constants
   // ---------
   //
@@ -563,20 +520,6 @@
 
   // @deprecated
   Opal.find_iter_super_dispatcher = Opal.find_block_super;
-
-  Opal.is_a = function(object, klass) {
-    if (klass != null && object.$$meta === klass || object.$$class === klass) {
-      return true;
-    }
-
-    if (object.$$is_number && klass.$$is_number_class) {
-      return (klass.$$is_integer_class) ? (object % 1) === 0 : true;
-    }
-
-    var ancestors = $ancestors(object.$$is_class ? Opal.get_singleton_class(object) : (object.$$meta || object.$$class));
-
-    return ancestors.indexOf(klass) !== -1;
-  };
 
   function $apply_blockopts(block, blockopts) {
     if (typeof(blockopts) === 'number') {
