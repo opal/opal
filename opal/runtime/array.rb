f4cd20a40d44cb46fcfe485af4cce6a1aa191b0b
hmdne <54514036+hmdne@users.noreply.github.com>
2024-12-22 03:08:45 +0100

    Isolate array functions into their own runtime module

---
+++ opal/runtime/array.rb
@@ -0,0 +1,64 @@
+# backtick_javascript: true
+# use_strict: true
+# opal_runtime_mode: true
+# helpers: raise
+
+module ::Opal
+  # Helpers for implementing multiple assignment
+  # Our code for extracting the values and assigning them only works if the
+  # return value is a JS array.
+  # So if we get an Array subclass, extract the wrapped JS array from it
+
+  # Used for: a, b = something (no splat)
+  def self.to_ary(value = undefined)
+    %x{
+      if (value.$$is_array) {
+        return value;
+      }
+      else if (value['$respond_to?']('to_ary', true)) {
+        var ary = value.$to_ary();
+        if (ary === nil) {
+          return [value];
+        }
+        else if (ary.$$is_array) {
+          return ary;
+        }
+        else {
+          $raise(Opal.TypeError, "Can't convert " + value.$$class +
+            " to Array (" + value.$$class + "#to_ary gives " + ary.$$class + ")");
+        }
+      }
+      else {
+        return [value];
+      }
+    }
+  end
+
+  # Used for: a, b = *something (with splat)
+  def self.to_a(value = undefined)
+    %x{
+      if (value.$$is_array) {
+        // A splatted array must be copied
+        return value.slice();
+      }
+      else if (value['$respond_to?']('to_a', true)) {
+        var ary = value.$to_a();
+        if (ary === nil) {
+          return [value];
+        }
+        else if (ary.$$is_array) {
+          return ary;
+        }
+        else {
+          $raise(Opal.TypeError, "Can't convert " + value.$$class +
+            " to Array (" + value.$$class + "#to_a gives " + ary.$$class + ")");
+        }
+      }
+      else {
+        return [value];
+      }
+    }
+  end
+end
+
+::Opal
