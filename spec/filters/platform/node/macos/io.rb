6885a64f6aebf7ded60a03d00a35dfaf983eccdf
Jan Biedermann <jan@isomorfeus.com>
2026-02-11 13:43:45 +0100

    Node 25 somehow things are different, update spec filters

--- spec/filters/platform/node/macos/io.rb
+++ spec/filters/platform/node/macos/io.rb
@@ -3,4 +3,5 @@ opal_filter "IO" do
   fails "IO#ioctl raises IOError on closed stream" # Expected IOError but got: NotImplementedError (NotImplementedError)
   fails "IO.binwrite accepts a :mode option" # Expected "hello, world!34567890123456789" == "012345678901234567890123456789hello, world!" to be truthy but was false
   fails "IO.write accepts a :mode option" # Expected "hello, world!34567890123456789" == "012345678901234567890123456789hello, world!" to be truthy but was false
+  fails "IO#syswrite on a file does not modify the passed argument" # Expected [198, 32, 25] == [198, 146] to be truthy but was false
 end
