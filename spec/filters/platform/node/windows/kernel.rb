0b2305dc0304d62af74a39c32aa8668fe42b2cd7
Jan Biedermann <jan@isomorfeus.com>
2026-02-11 13:42:48 +0100

    Update spec filters, use ruby 4.0 for actions

--- spec/filters/platform/node/windows/kernel.rb
+++ spec/filters/platform/node/windows/kernel.rb
@@ -1,5 +1,6 @@
 # NOTE: run bin/format-filters after changing this file
 opal_filter "Kernel" do
+  fails "Kernel#` raises an Errno::ENOENT if the command is not executable" # Expected Errno::ENOENT but no exception was raised ("" was returned)
   fails "Kernel#` returns the standard output of the executed sub-process" # Expected  "disc world\r " ==  "disc world " to be truthy but was false
   fails "Kernel#exec runs the specified command, replacing current process" # Expected exit status is 0 but actual is 1 for command ruby_exe("bundle exec opal -Rnode C:/Users/Administrator/workspace/opal/tmp/rubyspec_temp/rubyexe.rb") Output:
   fails "Kernel#system does expand shell variables when given multiples arguments" # Expected (STDOUT): "foo\n"           but got: "\"foo\"\r\n" Backtrace
