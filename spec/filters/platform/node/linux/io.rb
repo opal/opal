ca05f2175b082dac0905f9096d0e701e3647f20f
Jan Biedermann <jan@isomorfeus.com>
2026-02-11 14:19:01 +0100

    More spec and test filters for node 25

--- spec/filters/platform/node/linux/io.rb
+++ spec/filters/platform/node/linux/io.rb
@@ -2,4 +2,5 @@
 opal_filter "IO" do
   fails "IO#ioctl raises IOError on closed stream" # Expected IOError but got: NotImplementedError (NotImplementedError)
   fails "IO#syswrite on a file does not modify the passed argument" # Expected [198, 32, 25] == [198, 146] to be truthy but was false
+  fails "IO#write on a file writes binary data if no encoding is given and multiple arguments passed" # Expected [32, 33, 196, 32, 38] == [135, 196, 133] to be truthy but was false
 end
